481fb93ee1754eb17aaf290e0fc6b107a1a22f0b
Michael Dale <michael.dale@kaltura.com>
2010-06-05 20:07:47 +0000

    bumped version number to avoid cache hits
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@617 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- mwEmbedLoader.js
+++ mwEmbedLoader.js
@@ -6,7 +6,7 @@
 * http://www.kaltura.org/project/HTML5_Video_Media_JavaScript_Library
 */
 
-var kURID = '1.1o';
+var kURID = '1.1n';
 // Static script loader url: 
 var SCRIPT_LOADER_URL = 'http://html5.kaltura.org/jsScriptLoader.php';
 //SCRIPT_LOADER_URL = '../mwEmbed/jsScriptLoader.php';
