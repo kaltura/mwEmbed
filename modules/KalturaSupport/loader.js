751c775a0e45c6429f174429f86f77e7b31f0ad8
Michael Dale <michael.dale@kaltura.com>
2010-08-26 20:35:10 +0000

    restored single request set
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@888 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/KalturaSupport/loader.js
+++ modules/KalturaSupport/loader.js
@@ -36,22 +36,18 @@
 	} );
 	
 	// Set a local variable with the request set so we can append it to embedPlayer
-	var kalturaSupportRequestSet = [
-		[ 
-		  'KalturaClientBase'
-		] ,
-		[ 
-		  'KalturaClient',
-		  'KalturaAccessControlService',
-		  'KalturaAccessControlOrderBy',
-		  'KalturaAccessControl',
-		  'MD5',
-		  'mw.KWidgetSupport'
-		]
+	var kalturaSupportRequestSet = [		
+	  'KalturaClientBase',
+	  'KalturaClient',
+	  'KalturaAccessControlService',
+	  'KalturaAccessControlOrderBy',
+	  'KalturaAccessControl',
+	  'MD5',
+	  'mw.KWidgetSupport'
 	];
 	
 	mw.addModuleLoader( 'KalturaPlaylist', function() {
-		return $j.merge( kalturaSupportRequestSet[ 1 ], 
+		return $j.merge( kalturaSupportRequestSet, 
 				[ 
 				  'mw.PlaylistHandlerKaltura', 
 				  'mw.PlaylistHandlerKalturaRss'
@@ -233,7 +229,7 @@
 		// And add the class to the request set and set the server is per-player specified. 
 		if( mw.getConfig( 'enableKalturaAnalytics' ) 
 			|| $j( playerElement ).attr( 'enableKalturaAnalytics' ) ){
-			kalturaSupportRequestSet[1].push( "mw.KAnalytics" );					
+			kalturaSupportRequestSet.push( "mw.KAnalytics" );					
 		}		
 		
 		// Add kaltura support hook
