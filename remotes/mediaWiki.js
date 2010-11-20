a0a54b8ade15749ffbdc2f6f5d794e0de6e779fa
Michael Dale <michael.dale@kaltura.com>
2010-11-20 18:22:37 +0000

    small subtitle fix version bump
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@1209 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- remotes/mediaWiki.js
+++ remotes/mediaWiki.js
@@ -4,7 +4,7 @@
  */
 var urlparts = getRemoteEmbedPath();
 var mwEmbedHostPath = urlparts[0];
-var mwRemoteVersion = 'r176';
+var mwRemoteVersion = 'r177';
 var mwUseScriptLoader = true;
 
 // Log the mwRemote version makes it easy to debug cache issues
