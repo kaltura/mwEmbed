166dbd55c88f7bfa27d3712ede436ec4e21ddd5d
Michael Dale <michael.dale@kaltura.com>
2010-12-12 13:59:59 +0000

    fixed double loading of faderPlugin
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@1300 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/KalturaSupport/uiConfComponents/faderPlugin.js
+++ modules/KalturaSupport/uiConfComponents/faderPlugin.js
@@ -4,16 +4,17 @@
 * <Plugin id="fader" width="0%" height="0%" includeInLayout="false" target="{controllersVbox}" hoverTarget="{PlayerHolder}" duration="0.5"/>
 */
 $j( mw ).bind( 'newEmbedPlayerEvent', function( event, embedPlayer ){
-	$j( embedPlayer ).bind( 'KalturaSupport.checkUiConf', function( event, $uiConf, callback ){
-	
-		var $fader = $uiConf.find("Plugin#fader");
-		if( $fader.attr('target') === "{controllersVbox}" ){
-			embedPlayer.overlaycontrols = true;
-		} else {
-			embedPlayer.overlaycontrols = false;
-		}
-		
+	$j( embedPlayer ).bind( 'KalturaSupport.checkUiConf', function( event, $uiConf, callback ){	
+		faderPlugin( embedPlayer, $uiConf );		
 		callback();
 		
 	})
 });
+var faderPlugin = function(embedPlayer, $uiConf){
+	var $fader = $uiConf.find("Plugin#fader");
+	if( $fader.attr('target') === "{controllersVbox}" ){
+		embedPlayer.overlaycontrols = true;
+	} else {
+		embedPlayer.overlaycontrols = false;
+	}	
+}
