e5fe88b3db8a1929846ff170fe07e18055172431
Michael Dale <michael.dale@kaltura.com>
2010-12-13 13:27:41 +0000

    clean up comments on ad file
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@1320 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/KalturaAdSupport/loader.js
+++ modules/KalturaAdSupport/loader.js
@@ -6,20 +6,28 @@
 		"mw.KAds" : "mw.KAds.js"
 	});
 	
-	// Bind the KalturaWatermark where the uiconf includes the Kaltura Watermark 
+	// Check for new Embed Player events: 
 	$j( mw ).bind( 'newEmbedPlayerEvent', function( event, embedPlayer ){
+		
+		// Check for KalturaSupport uiConf
 		$j( embedPlayer ).bind( 'KalturaSupport.checkUiConf', function( event, $uiConf, callback ){
-			// Check if the ad plugin is enabled:
+			
+			// Check if the kaltura ad plugin is enabled:
 			if( $uiConf.find('Plugin#vast').length ){
+				
+				// Load the ad plugin components
 				mw.load( ["mw.KAds", "mw.MobileAdTimeline"], function(){
+					
+					// Add the ads to the player: 
 					mw.addKalturaAds( embedPlayer,  $uiConf.find('Plugin#vast'), function(){
+						
 						// Wait until ads are loaded before running callback
 						// ( ie we don't want to display the player until ads are ready )
 						callback();
 					});
 				});
 			} else {
-				// Continue trigger event regardless of if ui-conf is found or not
+				// Continue player build out for players without ads
 				callback();
 			}
 		});
