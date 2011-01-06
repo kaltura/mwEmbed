822185cf6b20a7c7a3137ed52fbd64ae5d63a9c6
Ran Yefet <ran.yefet@kaltura.com>
2011-01-06 18:07:46 +0000

    finished the script
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@1473 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- mwEmbedLoader.php
+++ mwEmbedLoader.php
@@ -8,14 +8,18 @@ require_once( realpath( dirname( __FILE__ ) ) . '/includes/DefaultSettings.php' 
 // Check cache to see if its expired ( file modified time of mwEmbedLoader.js vs cache modified time ) 
 
 // Get resource (  mwEmbedLoader.js )
-$loaderJs = file_get_contents( )
+$loaderJs = file_get_contents( 'mwEmbedLoader.js' );
 
 // Append ResourceLoder path to loader.js
-$loaderJs.= "\n" . "SCRIPT_LOADER_URL = $wgMwEmbedPathUrl . /ResourceLoader.php";
+$path = str_replace("mwEmbedLoader.php", "ResourceLoader.php", $wgMwEmbedPathUrl);
+$loaderJs .= "\n" . "SCRIPT_LOADER_URL = '". $wgKalturaServiceUrl . $path ."';";
 
 // Minify via php_min
 
 // ob_gzhandler automatically checks for browser gzip support in and gzips
 ob_start("ob_gzhandler");
+
+header("Content-type: text/javascript");
+echo $loaderJs;
 
 ?>
