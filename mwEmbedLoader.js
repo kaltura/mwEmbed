915b85d0411c2d689602a955f104a40e17b1f37f
Ran Yefet <ran.yefet@kaltura.com>
2011-02-16 09:06:58 +0000

    Bumped version to 1.3b
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@1633 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- mwEmbedLoader.js
+++ mwEmbedLoader.js
@@ -36,7 +36,7 @@
 *	'EmbedPlayer.EnableIframeApi' : true
 */
 // The version of this script
-KALTURA_LOADER_VERSION = '1.3a';
+KALTURA_LOADER_VERSION = '1.3b';
 
 // Static script loader url: 
 var SCRIPT_LOADER_URL = 'http://www.kaltura.org/apis/html5lib/mwEmbed/ResourceLoader.php';
