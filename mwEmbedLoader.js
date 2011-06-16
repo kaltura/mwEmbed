e7a11ff736bac1291a05891068f94bcbe1c90b31
Michael Dale <michael.dale@kaltura.com>
2011-06-16 18:51:47 +0000

    bumped script version number
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@2189 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- mwEmbedLoader.js
+++ mwEmbedLoader.js
@@ -36,7 +36,7 @@
 *	'EmbedPlayer.EnableIframeApi' : true
 */
 // The version of this script
-KALTURA_LOADER_VERSION = '1.4a5';
+KALTURA_LOADER_VERSION = '1.4a6';
 // Static script loader url: 
 var SCRIPT_LOADER_URL = 'http://www.kaltura.org/apis/html5lib/mwEmbed/ResourceLoader.php';
 var SCRIPT_FORCE_DEBUG = false;
