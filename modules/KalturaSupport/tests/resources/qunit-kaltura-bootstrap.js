effc2433683291da48bc8ae4c1a31429df4ff06b
Michael Dale <michael.dale@kaltura.com>
2011-06-17 19:58:20 +0000

    moved common kaltura player loaded code into kaltura-bootstrap.js
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@2197 b58a29cf-3064-46da-94c6-1c29cc75c8e5

---
+++ modules/KalturaSupport/tests/resources/qunit-kaltura-bootstrap.js
@@ -0,0 +1,25 @@
+//must come after qunit-bootstrap.j
+if( window.QUnit ){
+	QUnit.start();	
+	jsCallbackCalled = false;
+	function jsCallbackReady ( videoId ) {
+		jsCallbackCalled = true;
+		jsKalturaPlayerTest( videoId );
+	}
+	asyncTest( "KalturaSupport::LoadPlayer", function(){
+		var waitCount = 0;
+		var interval = setInterval(function(){
+			// timeout in 20 seconds:
+			if( waitCount == 200 && !jsCallbackCalled ){
+				ok(false, "Player timed out");
+				clearInterval( interval );
+			}
+			if( jsCallbackCalled ){
+				ok(true, "Player loaded");
+				clearInterval( interval );
+				start();
+			}
+			waitCount++;
+		}, 100);
+	});
+}
