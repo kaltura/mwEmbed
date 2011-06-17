9b5fbc972f507f09b75d7dd8d620e8388ebb5857
Tracey Jaquith <tracey@archive.org>
2011-06-17 18:45:27 +0000

    minor bump to new playlist CSS
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@2194 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/InternetArchiveSupport/InternetArchiveVideoPage.js
+++ modules/InternetArchiveSupport/InternetArchiveVideoPage.js
@@ -237,7 +237,6 @@ div#mwplayer_videolist { bottom:0px !important; right:0px !important; }\n\
 div.playlistItem {\n\
   font-family:Lucida Grande;\n\
   margin:0px 5px 0px 5px !important;\n\
-  padding:2px !important;\n\
 }\n\
 div.maudio div.playlistItem { padding:0px !important; }\n\
 div.playlist-title { display:none; }\n\
