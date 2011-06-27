20018960ece3c19c713e68b35dde891b37870923
Michael Dale <michael.dale@kaltura.com>
2011-06-27 21:32:48 +0000

    some tweaks to kaltura qunit added convenience functions  for playerReady
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@2236 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/KalturaSupport/tests/resources/qunit-kaltura-bootstrap.js
+++ modules/KalturaSupport/tests/resources/qunit-kaltura-bootstrap.js
@@ -6,12 +6,14 @@ if( window.QUnit ){
 	function jsCallbackReady ( videoId ) {
 		jsCallbackCalled = true;
 		jsKalturaPlayerTest( videoId );
+
+		document.getElementById( videoId ).addJsListener("entryReady", "kalturaQunitEntryReady");
 	}
 	asyncTest( "KalturaSupport::PlayerLoaded", function(){
 		var waitCount = 0;
 		var interval = setInterval(function(){
 			// timeout in 20 seconds:
-			if( waitCount == 200 && !jsCallbackCalled ){
+			if( waitCount == 2000 && !jsCallbackCalled ){
 				ok(false, "Player timed out");
 				clearInterval( interval );
 			}
@@ -21,6 +23,25 @@ if( window.QUnit ){
 				start();
 			}
 			waitCount++;
-		}, 100);
+		}, 10);
 	});
+	var entryReadyCallbacks = [];
+	var entryReadyAlreadyCalled = false;
+	// Utility function for entry ready testing handler
+	window['kalturaQunitEntryReady'] = function(){
+		// run in async call to ensure non-blocking build out is in dom
+		setTimeout(function(){
+			while( entryReadyCallbacks.length ){
+				entryReadyCallbacks.shift()();
+			}
+			entryReadyAlreadyCalled = true;
+		}, 10 );
+	};
+	window['kalturaQunitWaitForPlayer'] = function( callback ){
+		if( entryReadyAlreadyCalled ){
+			callback();
+			return ;
+		}
+		entryReadyCallbacks.push( callback );
+	};
 }
