96d3718590614d0a2bdf7b07883b8de8f50d5dcc
Ran Yefet <ran.yefet@kaltura.com>
2011-07-19 16:45:13 +0000

    Fixed dependency on KalturaCodeCuePointClientPlugin
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@2346 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/KalturaSupport/kaltura_client_v3/KalturaPlugins/KalturaAnnotationClientPlugin.php
+++ modules/KalturaSupport/kaltura_client_v3/KalturaPlugins/KalturaAnnotationClientPlugin.php
@@ -2,6 +2,7 @@
 require_once(dirname(__FILE__) . "/../KalturaClientBase.php");
 require_once(dirname(__FILE__) . "/../KalturaEnums.php");
 require_once(dirname(__FILE__) . "/../KalturaTypes.php");
+require_once(dirname(__FILE__) . "/KalturaCuePointClientPlugin.php");
 
 class KalturaAnnotationOrderBy
 {
