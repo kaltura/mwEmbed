6d0f0974827af983bba1f6c3399fb4937e16ff3d
Michael Dale <michael.dale@kaltura.com>
2011-08-17 14:38:12 +0000

    check for bumper.plugin for enabling or disabling the bumper
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@2486 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/KalturaSupport/uiConfComponents/bumperPlugin.js
+++ modules/KalturaSupport/uiConfComponents/bumperPlugin.js
@@ -6,14 +6,15 @@ var bumperPlugin = true;
 
 $j( mw ).bind( 'newEmbedPlayerEvent', function( event, embedPlayer ){
 	$j( embedPlayer ).bind( 'KalturaSupport_CheckUiConf', function( event, $uiConf, callback ){
+		
 		// <plugin id="bumper" bumperentryid="1_187nvs4c" clickurl="http://www.nokia.com" lockui="true" playonce="false" presequence="1" width="100%" height="100%"></plugin>
 		var bc = kWidgetSupport.getPluginConfig(
 				embedPlayer,
 				$uiConf, 
 				'bumper', 
-				['bumperEntryID', 'clickUrl', 'lockUi', 'playOnce', 'preSequence', 'width', 'height']
+				['plugin', 'bumperEntryID', 'clickUrl', 'lockUi', 'playOnce', 'preSequence', 'width', 'height']
 		);
-		if( ! bc.bumperEntryID || bc.preSequence == "0" ){
+		if( !bc.plugin || ! bc.bumperEntryID || bc.preSequence == "0" ){
 			callback();
 			return ;
 		}
