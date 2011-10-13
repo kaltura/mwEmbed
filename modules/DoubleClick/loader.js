74bd611c4f4f704ddc7cb134f2c4e746de8975dc
Michael Dale <michael.dale@kaltura.com>
2011-10-13 00:04:09 +0000

    added case to plugin config
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@2918 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/DoubleClick/loader.js
+++ modules/DoubleClick/loader.js
@@ -7,7 +7,7 @@ mw.addModuleLoader( 'DoubleClick', ['AdSupport', 'mw.DoubleClick'] );
 
 $( mw ).bind( 'newEmbedPlayerEvent', function( event, embedPlayer ){
 	$( embedPlayer ).bind( 'KalturaSupport_CheckUiConf', function( event, $uiConf, callback ){
-		if( embedPlayer.getKalturaConfig( 'doubleclick', 'plugin' ) ){
+		if( embedPlayer.getKalturaConfig( 'DoubleClick', 'plugin' ) ){
 			mw.load( 'DoubleClick', function(){
 				new mw.DoubleClick( embedPlayer, callback );
 			});
