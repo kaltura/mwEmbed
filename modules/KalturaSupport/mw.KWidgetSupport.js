8160752127bfba498261ec1aac8637d29cff16c6
Michael Dale <michael.dale@kaltura.com>
2011-11-15 21:35:07 +0000

    fixed fullscreen popup link
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@3246 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/KalturaSupport/mw.KWidgetSupport.js
+++ modules/KalturaSupport/mw.KWidgetSupport.js
@@ -37,7 +37,7 @@ mw.KWidgetSupport.prototype = {
 			// Add Kaltura iframe share support:
 			$( embedPlayer ).bind( 'getShareIframeSrc', function( event, callback ){
 				var iframeUrl = mw.getMwEmbedPath() + 'mwEmbedFrame.php';
-				iframeUrl +='/wid/_' + embedPlayer.kwidgetid +
+				iframeUrl +='/wid/' + embedPlayer.kwidgetid +
 				'/uiconf_id/' + embedPlayer.kuiconfid +
 				'/entry_id/' + embedPlayer.kentryid + '/';
 				// return the iframeUrl via the callback: 
