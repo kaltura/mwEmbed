456b383aba8faaba2fc98e2aa9fe1cf195a8b52a
Ran Yefet <ran.yefet@kaltura.com>
2011-11-28 13:21:56 +0000

    Disable options menu using optionsMenu.visible=false
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@3306 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/KalturaSupport/uiConfComponents/controlbarLayout.js
+++ modules/KalturaSupport/uiConfComponents/controlbarLayout.js
@@ -4,6 +4,12 @@
 		$( embedPlayer ).bind( 'KalturaSupport_CheckUiConf', function( event, $uiConf, callback ){
 
 			var disabled = [];
+
+			// HTML5 Options Menu
+			var optionsMenu = embedPlayer.getKalturaConfig( 'optionsMenu', ['visible', 'includeInLayout'] );
+			if( optionsMenu.visible === false || optionsMenu.includeInLayout === false ) {
+				mw.setConfig('EmbedPlayer.EnableOptionsMenu', false );
+			}
 			
 			// Check if the ui conf layout supports play/pause button
 			// <button id="playBtnControllerScreen" command="play" buttontype="iconButton" focusrectpadding="0" icon="playIcon" overicon="playIcon" downicon="playIcon" disabeledicon="playIcon" selectedupicon="pauseIcon" selectedovericon="pauseIcon" selecteddownicon="pauseIcon" selecteddisabledicon="pauseIcon" tooltip="" uptooltip="Pause" selectedtooltip="Play" k_buttontype="buttonIconControllerArea" color1="14540253" color2="16777215" color3="3355443" color4="10066329" color5="16777215" font="Arial"></button>
@@ -44,7 +50,7 @@
 			// <button id="onVideoPlayBtnStartScreen" command="play" buttontype="onScreenButton" minwidth="60" labelplacement="top" label="Play" stylename="onScreenBtn" upicon="playIcon" overicon="playIcon" downicon="playIcon" disabeledicon="playIcon" selectedupicon="playIcon" selectedovericon="playIcon" selecteddownicon="playIcon" selecteddisabledicon="playIcon" k_buttontype="buttonIconControllerArea" tooltip="Play video" color1="14540253" color2="16777215" color3="3355443" color4="10066329" color5="16777215" font="Arial"></button>
 			if( !$uiConf.find( 'button #onVideoPlayBtnStartScreen' ).length ){
 				disabled.push( 'playButtonLarge' );
-			}			
+			}		
 			
 			controlbarLayout( embedPlayer, disabled );
 			
