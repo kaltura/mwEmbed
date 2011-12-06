37360031cf571c60f0516fe6f5af359608b6428f
Ran Yefet <ran.yefet@kaltura.com>
2011-12-06 14:59:52 +0000

    added support for logging php client in debug mode
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@3383 b58a29cf-3064-46da-94c6-1c29cc75c8e5

---
+++ modules/KalturaSupport/KalturaLogger.php
@@ -0,0 +1,25 @@
+<?php
+
+class KalturaLogger implements IKalturaLogger {
+
+	function __construct() {
+
+	}
+
+	function log( $msg ) {
+		global $wgScriptCacheDirectory;
+
+		$logDir = $wgScriptCacheDirectory . '/logs';
+		$logFile = $logDir . '/' . date("Y-m-d") . '_kalturaClientLog.txt';
+
+		// try to create log dir if not exists
+		if( ! file_exists($logDir) ) {
+			mkdir( $logDir );
+		}
+
+		$msg = $msg . "\n";
+		
+		file_put_contents( $logFile, $msg, FILE_APPEND);
+		
+	}
+}
