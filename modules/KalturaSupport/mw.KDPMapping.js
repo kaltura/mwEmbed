0b8c6f72beb5c8e3ee3f14ec0ff34e39bfeeec12
Ran Yefet <ran.yefet@kaltura.com>
2011-12-28 16:59:25 +0000

    added spaces
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@3674 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/KalturaSupport/mw.KDPMapping.js
+++ modules/KalturaSupport/mw.KDPMapping.js
@@ -33,10 +33,10 @@
 			// Add the hooks to the player manager			
 			$( mw ).bind( 'newEmbedPlayerEvent', function( event, embedPlayer ) {
 				// Add the addJsListener and sendNotification maps
-				embedPlayer.addJsListener = function(listenerString, globalFuncName){
+				embedPlayer.addJsListener = function( listenerString, globalFuncName ){
 					_this.addJsListener( embedPlayer, listenerString, globalFuncName );
 				};
-				embedPlayer.removeJsListener = function(listenerString, callbackName){
+				embedPlayer.removeJsListener = function( listenerString, callbackName ){
 					_this.removeJsListener( embedPlayer, listenerString, callbackName );
 				};
 				
@@ -72,7 +72,7 @@
 			});
 			
 			$( mw ).bind( 'newIframePlayerClientSide', function( event, playerProxy ) {
-				$( playerProxy ).bind( 'jsListenerEvent', function(event, globalFuncName, listenerArgs){
+				$( playerProxy ).bind( 'jsListenerEvent', function( event, globalFuncName, listenerArgs ){
 					// check if globalFuncName has descendant properties
 					if( typeof window[ globalFuncName ] == 'function' ){
 						window[ globalFuncName ].apply( window[ globalFuncName ], listenerArgs );
