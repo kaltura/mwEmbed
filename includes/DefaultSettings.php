9d15929c705d16cd164d309bb29fbc85c81cc049
Michael Dale <michael.dale@kaltura.com>
2012-01-09 16:40:41 +0000

    bumped trunk to 1.6.5
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@3731 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -19,7 +19,7 @@ $wgMwEmbedPathUrl = str_replace(
 );
 
 // The version of the library 
-$wgMwEmbedVersion = '1.6.4';
+$wgMwEmbedVersion = '1.6.5';
 
 // Url to the resource loader php script: 
 $wgResourceLoaderUrl = $wgMwEmbedPathUrl . 'ResourceLoader.php';
