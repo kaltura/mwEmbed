481d345c82c06286b8c72666364ccb7789d8609b
Michael Dale <michael.dale@kaltura.com>
2012-01-17 09:04:15 +0000

    added no-cache headers to sleep test
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@3809 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/KalturaSupport/apiServices/mweApiSleepTest.php
+++ modules/KalturaSupport/apiServices/mweApiSleepTest.php
@@ -11,6 +11,8 @@ class mweApiSleepTest {
 		// Have the script take 10 seconds to run:
 		sleep( 10 );
 		header("Content-type: text/javascript");
+		header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+		header("Pragma: no-cache");
 		echo "document.write('<br> 10 second long script done loading');";
 	}
 }
