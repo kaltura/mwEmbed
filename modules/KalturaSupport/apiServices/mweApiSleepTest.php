8538c4b8d832938762e370bbc257497f718ba90b
Michael Dale <michael.dale@kaltura.com>
2012-01-22 06:18:33 +0000

    renamed id to waitTime ( better describes the role )
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@3846 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/KalturaSupport/apiServices/mweApiSleepTest.php
+++ modules/KalturaSupport/apiServices/mweApiSleepTest.php
@@ -6,13 +6,15 @@ $wgMwEmbedApiServices['sleepTest'] = 'mweApiSleepTest';
 
 class mweApiSleepTest {
 	function run(){
-		$waitTime = intval( $_REQUEST['id'] );
-		// Have the script take 10 seconds to run:
+		$waitTime = intval( $_REQUEST['waitTime'] );
+		if( $waitTime > 30 || $waitTime < 0 ) {
+			$waitTime = 30;
+		}
+		// Have the script take waitTime seconds to run:
 		sleep( $waitTime );
 		header("Content-type: text/javascript");
 		header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 		header("Pragma: no-cache");
-		
 		echo "document.write('<br> 2 second long script done loading id:" . $waitTime . "');";
 	}
 }
