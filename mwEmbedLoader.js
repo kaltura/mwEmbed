15f4f57b6d35339f0fc8c75ab4bae730325a468e
Michael Dale <michael.dale@kaltura.com>
2012-01-25 22:18:40 +0000

    revert r3650 fixes kWidget embed for IE7 and IE8
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@3901 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- mwEmbedLoader.js
+++ mwEmbedLoader.js
@@ -228,7 +228,7 @@ function kOutputFlashObject( replaceTargetId, kEmbedSettings, options ){
 	var elm = document.getElementById( replaceTargetId );
 	// Output a normal flash object tag: 
 	if( elm && elm.parentNode ){
-		
+		var spanTarget = document.createElement("span");
 		var pId =  ( kEmbedSettings.id )? kEmbedSettings.id : elm.id 
 		var swfUrl = mw.getConfig( 'Kaltura.ServiceUrl' ) + '/index.php/kwidget/'+ 
 			'/wid/' + kEmbedSettings.wid + 
@@ -248,26 +248,25 @@ function kOutputFlashObject( replaceTargetId, kEmbedSettings, options ){
 		
 		var flashvarValue = ( kEmbedSettings.flashvars ) ? kFlashVarsToString( kEmbedSettings.flashvars ) : '&';
 
-		var objectTarget = document.createElement("object");
-		objectTarget.id = pId;
-		objectTarget.name = pId;
-		objectTarget.type = "application/x-shockwave-flash";
-		objectTarget.allowFullScreen = 'true';
-		objectTarget.allowNetworking = 'all';
-		objectTarget.allowScriptAccess = 'always';
-		objectTarget.style.cssText = "width: " + width + "; height: " + height;
-		objectTarget.width = width;
-		objectTarget.height = height;
-		objectTarget.resource = swfUrl;
-		objectTarget.data = swfUrl;
-		objectTarget.innerHTML = '<param name="allowFullScreen" value="true" />' +
+		spanTarget.innerHTML = '<object id="' + pId + '" ' + 
+			'name="' + pId + '" ' + 
+			'type="application/x-shockwave-flash" ' + 
+			'allowFullScreen="true" ' +
+			'allowNetworking="all" ' +
+			'allowScriptAccess="always" ' + 
+			'width="' + width +'" ' +
+			'height="' + height + '" ' + 
+			'style="width:' + width + ';height:' + height + ';" ' +
+			'resource="' + swfUrl + '" ' +
+			'data="' + swfUrl + '" >' +
+				'<param name="allowFullScreen" value="true" />' + 
 				'<param name="allowNetworking" value="all" />' +
 				'<param name="allowScriptAccess" value="always" />' +
 				'<param name="bgcolor" value="#000000" />' +
 				'<param name="flashVars" value="' + flashvarValue + '" /> ' +
-				'<param name="movie" value="' + swfUrl + '" />';
-			
-		elm.parentNode.replaceChild( objectTarget, elm );
+				'<param name="movie" value="' + swfUrl + '" />' +
+		'</object>';
+		elm.parentNode.replaceChild( spanTarget, elm );
 	}
 }
 function kIframeWithoutApi( replaceTargetId, kEmbedSettings , options ){
