f4759196f202af8265df448ac07294172e0e569b
Michael Dale <michael.dale@kaltura.com>
2012-01-25 21:09:20 +0000

    bump 1.6.6 library version
    git-svn-id: http://www.kaltura.org/kalorg/html5video/tags/1.6.6/mwEmbed@3898 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -19,7 +19,7 @@ $wgMwEmbedPathUrl = str_replace(
 );
 
 // The version of the library 
-$wgMwEmbedVersion = '1.6.6a3';
+$wgMwEmbedVersion = '1.6.6a4';
 
 // Url to the resource loader php script: 
 $wgResourceLoaderUrl = $wgMwEmbedPathUrl . 'ResourceLoader.php';
