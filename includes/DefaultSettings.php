6ea6b8b83aedfdef99ace735b94a93cf3f5c6dae
Michael Dale <michael.dale@kaltura.com>
2012-02-06 20:45:09 +0000

    merge backup resize fix for iOS resize issue
    git-svn-id: http://www.kaltura.org/kalorg/html5video/tags/1.6.6/mwEmbed@4056 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -19,7 +19,7 @@ $wgMwEmbedPathUrl = str_replace(
 );
 
 // The version of the library 
-$wgMwEmbedVersion = '1.6.6a16';
+$wgMwEmbedVersion = '1.6.6a17';
 
 // Url to the resource loader php script: 
 $wgResourceLoaderUrl = $wgMwEmbedPathUrl . 'ResourceLoader.php';
