269759fd7e434aba40811d1dbe75b2426b077af0
Michael Dale <michael.dale@kaltura.com>
2012-02-07 13:14:36 +0000

    updated dol playhead attribute name
    git-svn-id: http://www.kaltura.org/kalorg/html5video/tags/1.6.7/mwEmbed@4064 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- modules/DolStatistics/mw.DolStatistics.js
+++ modules/DolStatistics/mw.DolStatistics.js
@@ -218,7 +218,7 @@ mw.DolStatistics.prototype = {
 		// KDP Event Data
 		params['KDPDAT_VALUE'] = eventData.toString();
 		// Always include the current time: 
-		params['PLAYHEAD_POSITION'] = embedPlayer.currentTime;
+		params['KDPDAT_PLAYHEAD'] = embedPlayer.currentTime;
 		
 		// Add custom params
 		for( var i =0; i < 10; i++ ){
