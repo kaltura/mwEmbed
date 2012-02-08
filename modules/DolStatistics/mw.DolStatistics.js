kaltura/mwEmbed

commit 19948de5ac8b0b78b9650d9f8c07b2c0ff62c254

* don't increment DolStatisticsCounter on failed entries * KDPPROTO is the protocol for the service url * KSESSIONSEQ lists the sequence number in isolation
git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/mwEmbed@4072 b58a29cf-3064-46da-94c6-1c29cc75c8e5

modules/DolStatistics/mw.DolStatistics.js
@@ -45,10 +45,15 @@ mw.DolStatistics.prototype = {
 		this.eventsList = eventList.split(",");
 		
 		mw.log( 'DolStatistics:: eventList:' + this.eventsList );
-				
+		
+		
 		// Setup player counter, (used global, because on change media we re-initlize the plugin and reset all vars)
-		if( ! $( embedPlayer ).data('DolStatisticsCounter') ) {
-			$( embedPlayer ).data('DolStatisticsCounter', 1 );
+		if( $( embedPlayer ).data('DolStatisticsCounter') === null ) {
+			if( embedPlayer['data-playerError'] ){
+				$( embedPlayer ).data('DolStatisticsCounter', 0 ) 
+			} else {
+				$( embedPlayer ).data('DolStatisticsCounter', 1 );
+			}
 		}
 
 		mw.log('DolStatistics:: Init plugin :: Plugin config: ', this.embedPlayer.getKalturaConfig( 'dolStatistics') );
@@ -68,8 +73,10 @@ mw.DolStatistics.prototype = {
 		this.destroy();
 
 		// On change media remove any existing ads:
-		embedPlayer.bindHelper( 'onChangeMedia' + _this.bindPostFix, function(){
-			$embedPlayer.data('DolStatisticsCounter', $embedPlayer.data('DolStatisticsCounter')+1);
+		embedPlayer.bindHelper( 'onChangeMediaDone' + _this.bindPostFix, function(){
+			if( ! embedPlayer['data-playerError'] ){
+				$embedPlayer.data('DolStatisticsCounter', $embedPlayer.data('DolStatisticsCounter') + 1 );
+			}
 			_this.destroy();
 		});
 
@@ -206,13 +213,15 @@ mw.DolStatistics.prototype = {
 		// Video length
 		params['VIDLEN'] = this.getDuration();
 		// Player protocol
-		params['KDPPROTO'] = this.getConfig('protocol') || location.protocol.substr(0, location.protocol.length-1);
+		params['KDPPROTO'] = mw.parseUri( mw.getConfig( 'Kaltura.ServiceUrl' ) ).protocol;
 		// Kaltura Player ID
 		params['KDPID'] = this.embedPlayer.kuiconfid;
 		// Kaltura Seesion ID
 		params['KSESSIONID'] = this.embedPlayer.evaluate('{configProxy.sessionId}');
 		// Kaltura Playback ID ( kSessionId + playbackCounter )
 		params['KPLAYBACKID'] = this.embedPlayer.evaluate('{configProxy.sessionId}') + $( this.embedPlayer ).data('DolStatisticsCounter');
+		// Kaltura session Seq 
+		params['KSESSIONSEQ'] = $( this.embedPlayer ).data('DolStatisticsCounter');
 		// Kaltura Event name
 		params['KDPEVNT'] = eventName;
 		// KDP Event Data
@@ -227,7 +236,6 @@ mw.DolStatistics.prototype = {
 				params[  _this.getConfig( 'customDataKey' + i )  ] =  _this.getConfig( 'customDataValue' + i );
 			}
 		}
-		
 		
 		mw.log('DolStatistics:: Send Stats Data ' + statsUrl, params);
 		

commit b09941609af2e49b3b360387a9439db5fef6d03d

fixed broken merged in r4072 dol stats fixes
git-svn-id: http://www.kaltura.org/kalorg/html5video/tags/1.6.7/mwEmbed@4078 b58a29cf-3064-46da-94c6-1c29cc75c8e5

modules/DolStatistics/mw.DolStatistics.js
@@ -182,6 +182,7 @@ mw.DolStatistics.prototype = {
 	/* Send stats data using Beacon or jsCallback */
 	sendStatsData: function( eventName, eventData ) {
 		var _this = this;
+		var embedPlayer = this.embedPlayer;
 		// If event name not in our event list, exit
 		if( this.eventsList.indexOf( eventName ) === -1 ) {
 			return ;
@@ -225,7 +226,7 @@ mw.DolStatistics.prototype = {
 		// KDP Event Data
 		params['KDPDAT_VALUE'] = eventData.toString();
 		// Always include the current time: 
-		params['KDPDAT_PLAYHEAD'] = embedPlayer.currentTime;
+		params['KDPDAT_PLAYHEAD'] = this.embedPlayer.currentTime;
 		
 		// Add custom params
 		for( var i =0; i < 10; i++ ){