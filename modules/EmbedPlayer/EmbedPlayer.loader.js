4a21c95ae8579488b696f4655c65f1e6cd5c8893
Michael Dale <mdale@wikimedia.org>
2012-03-06 16:20:14 -0600

    updated embedPlayer module structure for new resource loader

---
+++ modules/EmbedPlayer/EmbedPlayer.loader.js
@@ -0,0 +1,127 @@
+/**
+* EmbedPlayer loader
+*/
+( function( mw, $ ) {
+	alert('runs?');
+	/**
+	* Add a DOM ready check for player tags
+	*
+	* We use 'SetupInterface' binding so other code that depend on the video interface can 
+	* work after the 'IntefacesReady' event
+	*/
+	$( mw ).bind( 'SetupInterface', function( event, callback ){
+		// Check if we have tags to rewrite: 
+		if( $( mw.getConfig( 'EmbedPlayer.RewriteSelector' )  ).length ) {
+			// Rewrite the embedPlayer EmbedPlayer.RewriteSelector and run callback once ready:
+			$( mw.getConfig( 'EmbedPlayer.RewriteSelector' ) )
+				.embedPlayer( callback );
+		} else {
+			callback();
+		}
+	});
+	
+	/**
+	* Add the mwEmbed jQuery loader wrapper 
+	*/
+	$.fn.embedPlayer = function( readyCallback ){
+		var playerSelect;
+		if( this.selector ){
+			playerSelect = this.selector;
+		} else {
+			playerSelect = this;
+		}
+		mw.log( 'jQuery.fn.embedPlayer :: ' + playerSelect );
+		
+		// Hide videonojs class
+		$( '.videonojs' ).hide();
+
+		// Set up the embed video player class request: (include the skin js as well)
+		var dependencySet = [
+			'mw.EmbedPlayer'
+		];
+		
+		// Add PNG fix code needed:
+		if ( $.browser.msie && $.browser.version < 7 ) {
+			$.merge( dependencySet, ['jquery.pngFix'] );
+		}
+		
+		// Guess at playback system for 90+% of users the browser indicates playback mode: 
+		// NOTE: this does not affect a given playback library being loaded on-demand later. 
+		if( ( $.browser.msie && $.browser.version < 9 ) || $.browser.safari ) {			
+			$.merge( dependencySet, ['mw.EmbedPlayerJava'] );
+		}
+		// If video tag is supported add native lib:
+		if( document.createElement('video').canPlayType && !$.browser.safari) {
+			$.merge( dependencySet, ['mw.EmbedPlayerNative'] );
+		}
+		
+		// Check if the iFrame player api is enabled and we have a parent iframe url: 
+		// TODO we might want to move the iframe api to a separate module
+		if ( mw.getConfig( 'EmbedPlayer.EnableIframeApi' ) 
+				&& 
+			mw.getConfig( 'EmbedPlayer.IframeParentUrl' ) 
+		){
+			$.merge( dependencySet, ['mw.EmbedPlayerNative', 'jquery.postMessage','mw.IFramePlayerApiServer'] );
+		}
+		
+		var rewriteElementCount = 0;
+		$( playerSelect).each( function(inx, playerElement){
+			var skinName ='';
+
+			// Assign an the element an ID ( if its missing one )
+			if ( $( playerElement ).attr( "id" ) == '' ) {
+				$( playerElement ).attr( "id", 'v' + ( rewriteElementCount++ ) );
+			}
+			
+			// Add an overlay loader ( firefox has its own native loader )
+			if( !$.browser.mozilla ){
+				$( playerElement )
+					.getAbsoluteOverlaySpinner()
+					.attr('id', 'loadingSpinner_' + $( playerElement ).attr('id') )
+			}
+			// Add core "skin/interface" loader			
+			var skinString = $( playerElement ).attr( 'class' );
+			if( ! skinString 
+					||
+				$.inArray( skinString.toLowerCase(), mw.getConfig('EmbedPlayer.SkinList') ) == -1 
+			){
+				skinName = mw.getConfig( 'EmbedPlayer.DefaultSkin' );
+			} else {
+				skinName = skinString.toLowerCase();
+			}
+			// Add the skin to the request
+			var skinCaseName = skinName.charAt(0).toUpperCase() + skinName.substr(1);
+			$.merge( dependencySet, [ 'mw.PlayerSkin' + skinCaseName ] );
+			
+			// Allow other modules update the dependencies
+			$( mw ).trigger( 'EmbedPlayerUpdateDependencies',
+					[ playerElement, dependencySet ] );
+		});
+		// Remove any duplicates in the dependencySet:
+		dependencySet = $.unique( dependencySet );
+		// Do the request and process the playerElements with updated dependency set
+		mediaWiki.loader.using( dependencySet, function(){
+			mw.processEmbedPlayers( playerSelect, readyCallback );
+		}, function( e ){
+			throw new Error( 'Error loading EmbedPlayer dependency set: ' + e.message  );
+		});
+	};
+	
+	/**
+	 * Utility loader function to grab configuration for passing into an iframe as a hash target
+	 */
+	mw.getIframeHash = function( playerId ){
+		// Append the configuration and request domain to the iframe hash: 
+		var iframeMwConfig =  mw.getNonDefaultConfigObject();
+		// Add the parentUrl to the iframe config: 
+		iframeMwConfig['EmbedPlayer.IframeParentUrl'] = document.URL;
+
+		return '#' + encodeURIComponent(
+			JSON.stringify({
+				'mwConfig' :iframeMwConfig,
+				'playerId' : playerId
+			})
+		);
+	};
+
+} )( mediaWiki, jQuery );
---
+++ modules/EmbedPlayer/EmbedPlayer.php
@@ -0,0 +1,65 @@
+<?php
+	// Register all the EmbedPlayer modules 
+	return array(
+			"mw.MediaElement" => array( 'scripts' => 'resources/mw.MediaElement.js' ),
+			"mw.MediaPlayer" => array( 'scripts' => 'resources/mw.MediaPlayer.js' ),
+			"mw.MediaPlayers" => array( 
+				'scripts' => 'resources/mw.MediaPlayers.js',
+				'dependencies' => 'mw.MediaPlayer'
+			),
+			"mw.MediaSource" => array( 'scripts' => 'resources/mw.MediaSource.js' ),
+			"mw.EmbedTypes" => array( 
+				'scripts' => 'resources/mw.EmbedTypes.js', 
+				'dependencies' =>  array(
+					'mw.MediaPlayers'
+				)
+			),
+
+			"mw.EmbedPlayer" => array( 
+				'scripts' => array( 
+					"resources/mw.EmbedPlayer.js", 
+					"resources/skins/mw.PlayerControlBuilder.js",
+				),
+				'dependencies' => array(
+					// mwEmbed support module 
+					'mediawiki.client',
+					'mediawiki.UtilitiesTime',
+					'mediawiki.Uri',
+					'mediawiki.absoluteUrl',
+				
+					// Sub classes:
+					'mw.MediaElement',
+					'mw.MediaPlayers',
+					'mw.MediaSource',
+					'mw.EmbedTypes',
+				
+					// jQuery dependencies: 
+					'jquery.client',
+					'jquery.hoverIntent',
+					'jquery.cookie',
+					'jquery.ui.mouse',
+					'jquery.menu',
+					'jquery.ui.slider'					
+				),
+				'styles' => "resources/skins/EmbedPlayer.css",
+				'messageFile' => 'EmbedPlayer.i18n.php',		
+			),
+				
+			"mw.EmbedPlayerKplayer"	=> array( 'scripts'=> "resources/mw.EmbedPlayerKplayer.js" ),
+			"mw.EmbedPlayerGeneric"	=> array( 'scripts'=> "resources/mw.EmbedPlayerGeneric.js" ),
+			"mw.EmbedPlayerJava" => array( 'scripts'=> "resources/mw.EmbedPlayerJava.js"),
+			"mw.EmbedPlayerNative"	=> array( 'scripts'=> "resources/mw.EmbedPlayerNative.js" ),
+			"mw.EmbedPlayerImageOverlay" => array( 'scripts'=> "resources/mw.EmbedPlayerImageOverlay.js" ),
+			
+			"mw.EmbedPlayerVlc" => array( 'scripts'=> "resources/mw.EmbedPlayerVlc.js" ),
+			
+			"mw.IFramePlayerApiServer" => array( 'scripts' => "resources/iframeApi/mw.IFramePlayerApiServer.js" ),
+			"mw.IFramePlayerApiClient" => array( 'scripts' => "resources/iframeApi/mw.IFramePlayerApiClient.js" ),
+		
+			"mw.PlayerSkinKskin" => array( 	'scripts' => "resources/skins/kskin/mw.PlayerSkinKskin.js",
+											'styles' => "resources/skins/kskin/PlayerSkinKskin.css"),
+			
+			"mw.PlayerSkinMvpcf" => array( 	'scripts'=> "resources/skins/mvpcf/mw.PlayerSkinMvpcf.js", 
+											'styles'=> "resources/skins/mvpcf/PlayerSkinMvpcf.css"),
+	);
+?>
