b3fec7c9868f2fb10bd5836279c5bd284220b4bc
Michael Dale <mdale@wikimedia.org>
2012-03-09 08:06:31 -0800

    // export the class name for Resource Loader, // TODO can be removed with New Resource Loader

--- modules/KalturaSupport/uiConfComponents/jcarousellite_1.0.1.js
+++ modules/KalturaSupport/uiConfComponents/jcarousellite_1.0.1.js
@@ -201,6 +201,10 @@
  */
 
 (function($) {                                          // Compliant with jquery.noConflict()
+// export the class name for Resource Loader, 
+// TODO can be removed with New Resource Loader
+window.jCarousel = true;
+
 $.fn.jCarouselLite = function(o) {
     o = $.extend({
         btnPrev: null,
