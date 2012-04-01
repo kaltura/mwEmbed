ed2a2854d8e1f69ada6eff93e784bd537751feb9
Ran Yefet <ran@yefet.net>
2012-04-01 14:46:56 +0300

    Bumbed version to 1.6.10.2

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -19,7 +19,7 @@ $wgMwEmbedPathUrl = str_replace(
 );
 
 // The version of the library 
-$wgMwEmbedVersion = '1.6.10rc3';
+$wgMwEmbedVersion = '1.6.10.2';
 
 // Url to the resource loader php script: 
 $wgResourceLoaderUrl = $wgMwEmbedPathUrl . 'ResourceLoader.php';
