9b05ff8fa630f81548734b473025cddea2a2d711
Michael Dale <mdale@wikimedia.org>
2012-05-01 19:13:22 -0400

    restored version tag to 1.6.12alpha

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -19,7 +19,7 @@ $wgMwEmbedPathUrl = str_replace(
 );
 
 // The version of the library 
-$wgMwEmbedVersion = '1.6.11.10';
+$wgMwEmbedVersion = '1.6.12alpha';
 
 // Url to the resource loader php script: 
 $wgResourceLoaderUrl = $wgMwEmbedPathUrl . 'ResourceLoader.php';
