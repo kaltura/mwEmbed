899a210a5135dc5b1a79b7b921371b3e382f6cda
Avi Vaisenberger <avi.vaisenberger@kaltura.com>
2012-05-22 18:32:17 +0300

    Bumped version to 1.6.11.3f_hbp

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -19,7 +19,7 @@ $wgMwEmbedPathUrl = str_replace(
 );
 
 // The version of the library 
-$wgMwEmbedVersion = '1.6.11.3e_hbp';
+$wgMwEmbedVersion = '1.6.11.3f_hbp';
 
 // Url to the resource loader php script: 
 $wgResourceLoaderUrl = $wgMwEmbedPathUrl . 'ResourceLoader.php';
