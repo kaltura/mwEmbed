a6f923174c6d7ffff60b838bc33a0eb63d79d45e
Ran Yefet <ran@yefet.net>
2012-05-31 18:34:33 +0300

    fixed caption button not working

--- modules/TimedText/mw.TimedText.js
+++ modules/TimedText/mw.TimedText.js
@@ -321,18 +321,17 @@ mw.includeAllModuleMessages();
 		showTextMenu: function() {
 			var embedPlayer = this.embedPlayer;
 			var loc = embedPlayer.$interface.find( '.rButton.timed-text' ).offset();
-			mw.log('showTextInterface::' + embedPlayer.id + ' t' + loc.top + ' r' + loc.right);
-
-			var $menu = $( '#timedTextMenu_' + embedPlayer.id );
-			if ( $menu.length != 0 ) {
+			mw.log('showTextInterface::' + embedPlayer.id + ' location: ', loc);
+			// TODO: Fix menu animation
+			var $menu = $( '#textMenuContainer_' + embedPlayer.id );
+			if ( $menu.length ) {
 				// Hide show the menu:
 				if( $menu.is( ':visible' ) ) {
-					$menu.hide( "fast" );
+					$menu.hide().parent().hide(); // Hide menu and parent container
 				}else{
-					// move the menu to proper location
-					$menu.show("fast");
+					$menu.show().parent().show(); // Show menu and parent container
 				}
-			}else{				
+			}else{
 				// Bind the text menu:
 				this.buildMenu( true );
 			}
