e20965687963321b721607d824058aa3829e1f0f
Michael Dale <mdale@wikimedia.org>
2012-06-12 10:29:30 -0700

    bumped revision to 1.6.12.20

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -19,7 +19,7 @@ $wgMwEmbedPathUrl = str_replace(
 );
 
 // The version of the library
-$wgMwEmbedVersion = '1.6.12.16';
+$wgMwEmbedVersion = '1.6.12.20';
 
 // Url to the resource loader php script:
 $wgResourceLoaderUrl = $wgMwEmbedPathUrl . 'ResourceLoader.php';
