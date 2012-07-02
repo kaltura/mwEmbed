8d0fa3cfe851227f0c07eb79011624dd5d95a3c7
Avi Vaisenberger <avi.vaisenberger@kaltura.com>
2012-07-02 18:04:41 +0300

    Added mw.getHexColor utility function to convert color code from HEX notation to HTML

--- modules/MwEmbedSupport/mw.MwEmbedSupport.js
+++ modules/MwEmbedSupport/mw.MwEmbedSupport.js
@@ -314,5 +314,29 @@
 			$( mw ).trigger( 'InterfacesReady' );
 		});
 	});
+	
+	/**
+	 * Convert Hexadecimal string to HTML color code
+	 *
+	 * @param {String} Color code in hexadecimal notation
+	 */
+	mw.getHexColor = function( color ) {
+		if( color.substr(0,2) == "0x" ) {
+			return color.replace('0x', '#');
+		} else {
+			color = parseInt( color );
+			color = color.toString(16);
+			var len = 6 - color.length;
+			if( len > 0 ) {
+				var pre = '';
+				for( var i=0; i<len; i++) {
+					pre += '0';
+				}
+				color = pre + color;
+			}
+			return '#' + color;
+		}
+	};
+
 
 } )( mediaWiki, jQuery );
