560ae8a6247d34bf39edf66f686568d6f5e84399
Michael Dale <mdale@wikimedia.org>
2012-07-10 09:38:30 -0500

    updated title

--- docs/index.php
+++ docs/index.php
@@ -6,7 +6,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Bootstrap, from Twitter</title>
+    <title>Kaltura HTML5 library v<?php echo $wgMwEmbedVersion ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
