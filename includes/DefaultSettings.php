94666489cc1c6206eada49e5e97a4df34b35e6a8
Michael Dale <mdale@wikimedia.org>
2012-07-26 17:33:43 -0700

    white space change to bump changeid

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -3,7 +3,6 @@
  * This file stores default settings for Kaltura html5 client library "mwEmbed".
  * 
  *  DO NOT MODIFY THIS FILE. Instead modify LocalSettings.php in the parent mwEmbd directory. 
- * 
  */
 
 // The default cache directory
@@ -18,7 +17,7 @@ $wgMwEmbedPathUrl = str_replace(
 	$_SERVER['SCRIPT_NAME']
 );
 
-// The version of the library
+// The version of the library 
 $wgMwEmbedVersion = '1.6.12.33';
 
 // Url to the resource loader php script:
