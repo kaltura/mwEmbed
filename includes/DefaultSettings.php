b0716fbaed8e54d9ffb46a6eb05df3c07a1cc50d
Avi Vaisenberger <avi.vaisenberger@kaltura.com>
2012-08-14 18:12:07 +0300

    Bumped version to 1.6.12.27i

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -19,7 +19,7 @@ $wgMwEmbedPathUrl = str_replace(
 );
 
 // The version of the library
-$wgMwEmbedVersion = '1.6.12.27h';
+$wgMwEmbedVersion = '1.6.12.27i';
 
 // Url to the resource loader php script:
 $wgResourceLoaderUrl = $wgMwEmbedPathUrl . 'ResourceLoader.php';
