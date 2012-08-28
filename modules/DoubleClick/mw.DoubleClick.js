be7845c96703cf8f3d35d2136f832230c504c959
Michael Dale <mdale@wikimedia.org>
2012-08-28 12:13:17 -0700

    do force content play for both manual and restore play

--- modules/DoubleClick/mw.DoubleClick.js
+++ modules/DoubleClick/mw.DoubleClick.js
@@ -815,11 +815,6 @@ mw.DoubleClick.prototype = {
 			this.restorePlayerCallback();
 			this.restorePlayerCallback = null;
 			
-			// Do an sync play call ( without events if not on postroll )
-			if( !onContentComplete ){
-				this.forceContentPlay();
-			}
-			
 		} else { // do a manual restore: 
 			// restore player with normal events: 
 			this.embedPlayer.adTimeline.restorePlayer();
@@ -829,6 +824,11 @@ mw.DoubleClick.prototype = {
 			} else {
 				this.embedPlayer.play();
 			}
+		}
+		
+		// Do an sync play call ( without events if not on postroll )
+		if( !onContentComplete ){
+			this.forceContentPlay();
 		}
 	},
 	forceContentPlay: function(){
