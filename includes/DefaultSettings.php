3237571e48e6bcee82d3ea0cf05860758c00db4f
Michael Dale <mdale@wikimedia.org>
2012-08-30 15:50:20 -0700

    bumped version to 1.6.12.44.dol.min

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -18,7 +18,7 @@ $wgMwEmbedPathUrl = str_replace(
 );
 
 // The version of the library: 
-$wgMwEmbedVersion = '1.6.12.43.dol.min';
+$wgMwEmbedVersion = '1.6.12.44.dol.min';
 
 
 // Default HTTP protocol
