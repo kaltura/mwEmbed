b034189232df9b5fdd4475fcb55c992655b849f9
Michael Dale <mdale@wikimedia.org>
2012-09-11 15:32:11 -0700

    iOS 5+ correctly responds to canPlayType vnd.apple.mpegurl
    remove hard coded user agent check ( breaks iPad user agent spoofing in
    chrome )

--- modules/EmbedPlayer/resources/mw.EmbedTypes.js
+++ modules/EmbedPlayer/resources/mw.EmbedTypes.js
@@ -170,8 +170,8 @@ mw.EmbedTypes = {
 					// Test for h264:
 					if ( dummyvid.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"' ) ) {
 						this.mediaPlayers.addPlayer( h264NativePlayer );
-						// Check for iOS for vdn player support ( apple adaptive ) or vdn canPlayType != '' ( ie maybe/probably )
-						if( mw.isIOS() || dummyvid.canPlayType('application/vnd.apple.mpegurl; codecs="avc1.42E01E"' ) ){
+						// Check for vdn player support ( apple adaptive ) or vdn canPlayType != '' ( ie maybe/probably )
+						if( dummyvid.canPlayType('application/vnd.apple.mpegurl; codecs="avc1.42E01E"' ) ){
 							// Android 3x lies about HLS support ( only add if not Android 3.x )
 							if( navigator.userAgent.indexOf( 'Android 3.') == -1 ){
 								this.mediaPlayers.addPlayer( appleVdnPlayer );
