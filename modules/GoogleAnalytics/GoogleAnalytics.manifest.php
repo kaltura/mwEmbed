3181fb3b9a69e15c4a884044713f0456cb040574
Michael Dale <mdale@wikimedia.org>
2012-09-11 11:23:08 -0700

    include custom event in google analytics example

--- modules/GoogleAnalytics/GoogleAnalytics.manifest.php
+++ modules/GoogleAnalytics/GoogleAnalytics.manifest.php
@@ -4,17 +4,31 @@
  */
 return array(
 	'googleAnalytics' => array(
-		'description' => "Google Analytics, supports sending player analytic event to google",
+		'description' => "Google Analytics, supports sending player analytic event to google.<br> For full implementation guide see <a href=\"http://knowledge.kaltura.com/google-analytics-kaltura-player\">google analytics</a> in the knowledge center",
 		'attributes'=> array(
 			'urchinCode' => array(
 				'doc' => 'The google urchin code i.e UA-30149691-1',
 				'type' => 'string',
-				'edit' => true,
 			),
 			'trackEventMonitor'=> array(
 				'doc' => 'Function called on parent page for every event',
 				'type' => 'string',
-				'edit' => true,
+			),
+			'customEvent' => array(
+				'doc' => "Comma seperated list of events you want to track",
+				'type' => 'string',
+			),
+			'doPlayCategory' => array(
+				'doc' => "Category sent to google analytics for prefixed event",
+				'type' => 'string'
+			),
+			'doPlayAction' => array(
+				'doc' => "Action sent to google analytics for prefixed event",
+				'type' => 'string'
+			),
+			'doPlayValue' => array(
+				'doc' => "Value sent to google analytics for prefixed event",
+				'type' => 'string'
 			)
 		)
 	)
