1a7118629b0c43ed60a23c584ba6f0cdcad4f989
Michael Dale <mdale@wikimedia.org>
2012-09-12 11:57:04 -0700

    removed fixed position display ( breaks iframe display in android )

--- modules/KalturaSupport/kalturaIframe.php
+++ modules/KalturaSupport/kalturaIframe.php
@@ -497,7 +497,7 @@ class kalturaIframe {
 			html { margin: 0; padding: 0; width: 100%; height: 100%; }
 			body {
 				margin:0;
-				position:fixed;
+				/*position:fixed; // breaks android iframe display */
 				top:0px;
 				left:0px;
 				bottom:0px;
