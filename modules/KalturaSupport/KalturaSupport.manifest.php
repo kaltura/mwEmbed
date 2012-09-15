1ddedea2dfbb8efad1be9358c5c70fcfa33102d4
Nu Butthajit <nbutthajit@gmail.com>
2012-09-14 18:03:33 -0700

    create description box on page plugin.

--- modules/KalturaSupport/KalturaSupport.manifest.php
+++ modules/KalturaSupport/KalturaSupport.manifest.php
@@ -40,7 +40,19 @@ $kgDefaultCaptionAttr = array(
 	)
 );
 return array (
-
+	'descriptionBox' => array(
+	 	'description' => 'Appends description box below player.',
+		'attributes' => array(
+				'boxHeight' => array(
+					'doc' => 'Height of the description box',
+					'type' => 'number'
+				),
+				'descriptionLabel' => array(
+					'doc' => 'Description label, entry title if undefined.',
+					'type' => 'string'
+				),
+		)
+	),
 	/*Captions */
 	'closedCaptionsOverPlayer' => array(
 		'description' => 'Display captions ontop of the player',
