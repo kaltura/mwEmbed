b57a4e221012583fccc2d2b2605699ab972346ab
Avi Vaisenberger <avi.vaisenberger@kaltura.com>
2012-09-27 13:40:22 +0200

    Added a check to prevent double JS loading

--- modules/AkamaiAnalytics/mw.AkamaiAnalytics.js
+++ modules/AkamaiAnalytics/mw.AkamaiAnalytics.js
@@ -21,22 +21,33 @@
 			window.AKAMAI_MEDIA_ANALYTICS_CONFIG_FILE_PATH = this.getConfig( 'configPath' ) || 'http://ma193-r.analytics.edgesuite.net/config/beacon-3431.xml';
 			window.parent.AKAMAI_MEDIA_ANALYTICS_CONFIG_FILE_PATH = window.AKAMAI_MEDIA_ANALYTICS_CONFIG_FILE_PATH;
 			
-			$.getScript( 'http://79423.analytics.edgesuite.net/html5/akamaihtml5-min.js', function() {
-				setAkamaiMediaAnalyticsData( 'category', embedPlayer.evaluate( '{mediaProxy.entry.categories}' ) );
-				setAkamaiMediaAnalyticsData( 'publisherId', embedPlayer.kpartnerid );
-				setAkamaiMediaAnalyticsData( 'title', embedPlayer.evaluate( '{mediaProxy.entry.name}' ) );
-				setAkamaiMediaAnalyticsData( 'entryId', embedPlayer.kentryid );
-				var flavorURL = embedPlayer.getSource().src;
-				var startIndex = flavorURL.indexOf( '/flavorId/' ) + 10;
-				var flavorId = flavorURL.substr( startIndex, flavorURL.indexOf( '/format/' ) - startIndex );
-				setAkamaiMediaAnalyticsData( 'flavorId', flavorId );
-				setAkamaiMediaAnalyticsData( 'contentLength', embedPlayer.evaluate( '{mediaProxy.entry.duration}' ) );
-				setAkamaiMediaAnalyticsData( 'contentType', _this.getMediaTypeName() );
-				setAkamaiMediaAnalyticsData( 'device', navigator.platform );
-				setAkamaiMediaAnalyticsData( 'playerId', embedPlayer.kuiconfid );
+			if ( typeof setAkamaiMediaAnalyticsData == 'function' ) {
+				// Akamai HTML5 JS is already loaded, don't reload
+				_this.setData( embedPlayer );
 				callback();
-			} );
-        },
+			}
+			else {
+				$.getScript( 'http://79423.analytics.edgesuite.net/html5/akamaihtml5-min.js', function() {
+					_this.setData( embedPlayer );
+					callback();
+				} );
+			}
+		},
+		
+		setData: function( embedPlayer ) {
+			setAkamaiMediaAnalyticsData( 'category', embedPlayer.evaluate( '{mediaProxy.entry.categories}' ) );
+			setAkamaiMediaAnalyticsData( 'publisherId', embedPlayer.kpartnerid );
+			setAkamaiMediaAnalyticsData( 'title', embedPlayer.evaluate( '{mediaProxy.entry.name}' ) );
+			setAkamaiMediaAnalyticsData( 'entryId', embedPlayer.kentryid );
+			var flavorURL = embedPlayer.getSource().src;
+			var startIndex = flavorURL.indexOf( '/flavorId/' ) + 10;
+			var flavorId = flavorURL.substr( startIndex, flavorURL.indexOf( '/format/' ) - startIndex );
+			setAkamaiMediaAnalyticsData( 'flavorId', flavorId );
+			setAkamaiMediaAnalyticsData( 'contentLength', embedPlayer.evaluate( '{mediaProxy.entry.duration}' ) );
+			setAkamaiMediaAnalyticsData( 'contentType', this.getMediaTypeName() );
+			setAkamaiMediaAnalyticsData( 'device', navigator.platform );
+			setAkamaiMediaAnalyticsData( 'playerId', embedPlayer.kuiconfid );
+		},
 		
 		getConfig: function( attr )  {
 			return this.embedPlayer.getKalturaConfig( 'akamaiAnalytics', attr );
