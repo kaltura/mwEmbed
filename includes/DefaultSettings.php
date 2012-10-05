eb0457e0650d07699c940e3ce16340946974c288
Michael Dale <mdale@wikimedia.org>
2012-10-05 15:17:48 -0700

    bumped version to 1.6.12.49

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -18,7 +18,7 @@ $wgMwEmbedPathUrl = str_replace(
 );
 
 // The version of the library: 
-$wgMwEmbedVersion = '1.6.12.48';
+$wgMwEmbedVersion = '1.6.12.49';
 
 // Default HTTP protocol
 $wgHTTPProtocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? "https" : "http";
