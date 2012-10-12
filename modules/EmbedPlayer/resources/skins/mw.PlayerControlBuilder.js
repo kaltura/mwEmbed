56d665c87967e1ea2aee1e16c58bf8129e38432e
Michael Dale <mdale@wikimedia.org>
2012-10-12 11:14:11 -0700

    white space cleanup player control builder

--- modules/EmbedPlayer/resources/skins/mw.PlayerControlBuilder.js
+++ modules/EmbedPlayer/resources/skins/mw.PlayerControlBuilder.js
@@ -1710,115 +1710,115 @@ mw.PlayerControlBuilder.prototype = {
 		return false; // onclick action return false
 	},
 
-    /**
-    * Close an alert
-    */
-    closeAlert: function( keepOverlay ) {
+	/**
+	* Close an alert
+	*/
+	closeAlert: function( keepOverlay ) {
 		var embedPlayer = this.embedPlayer;
-        var $alert = $( this ).find( '.alert-container' );
-
-        mw.log( 'mw.PlayerControlBuilder::closeAlert' );
-        if ( !keepOverlay || ( mw.isIpad() && this.inFullScreen ) ) {
+	    var $alert = $( this ).find( '.alert-container' );
+	
+	    mw.log( 'mw.PlayerControlBuilder::closeAlert' );
+	    if ( !keepOverlay || ( mw.isIpad() && this.inFullScreen ) ) {
 			embedPlayer.controlBuilder.closeMenuOverlay();
 			// not sure why this was here, breaks playback on iPad :( 
 			/*if ( mw.isIpad() ) {
 				embedPlayer.disablePlayControls();
 			}*/
 		}
-        $alert.remove();
-        return false; // onclick action return false;
-    },
+		$alert.remove();
+		return false; // onclick action return false;
+	},
 
-    /**
-    * Generic function to display custom alert overlay on video.
-    *
-    * @param (Object) Object which includes:
-    *   title Alert Title
-    *   body Alert body
-    *   buttonSet[label,callback] Array of buttons
-    *   style CSS object
-    */
-    displayAlert: function( alertObj ) {
+	/**
+	* Generic function to display custom alert overlay on video.
+	*
+	* @param (Object) Object which includes:
+	*   title Alert Title
+	*   body Alert body
+	*   buttonSet[label,callback] Array of buttons
+	*   style CSS object
+	*/
+	displayAlert: function( alertObj ) {
 		var embedPlayer = this.embedPlayer;
-        var callback;
+		var callback;
 		mw.log( 'PlayerControlBuilder::displayAlert:: ' + alertObj.title );
-        // Check if callback is external or internal (Internal by default)
-
+		// Check if callback is external or internal (Internal by default)
+		
 		// Check if overlay window is already present:
 		if ( embedPlayer.getInterface().find( '.overlay-win' ).length != 0 ) {
-            return;
-        }
-        if( typeof alertObj.callbackFunction == 'string' ) {
-            if ( alertObj.isExternal ) {
-            	try{
-            		callback = window.parent[ alertObj.callbackFunction ];
-            	} catch ( e ){
-            		// could not call parent method
-            	}
-            } else {
-                callback = window[ alertObj.callbackFunction ];
-            }
-        } else if( typeof alertObj.callbackFunction == 'function' ) {
-            // Make life easier for internal usage of the listener mapping by supporting
-            // passing a callback by function ref
-            callback = alertObj.callbackFunction;
-        } else {
-            mw.log( "PlayerControlBuilder :: displayAlert :: Error: bad callback type" );
-            callback = function() {};
-        }
-
-        var $container = $( '<div />' ).addClass( 'alert-container' );
-        var $title = $( '<div />' ).text( alertObj.title ).addClass( 'alert-title alert-text' );
-        if ( alertObj.props && alertObj.props.titleTextColor ) {
-            $title.removeClass( 'alert-text' );
-            $title.css( 'color', mw.getHexColor( alertObj.props.titleTextColor ) );
-        }
-        var $message = $( '<div />' ).text( alertObj.message ).addClass( 'alert-message alert-text' );
+			return;
+		}
+		if( typeof alertObj.callbackFunction == 'string' ) {
+		if ( alertObj.isExternal ) {
+			try{
+				callback = window.parent[ alertObj.callbackFunction ];
+			} catch ( e ){
+				// could not call parent method
+				}
+			} else {
+				callback = window[ alertObj.callbackFunction ];
+			}
+		} else if( typeof alertObj.callbackFunction == 'function' ) {
+		// Make life easier for internal usage of the listener mapping by supporting
+		// passing a callback by function ref
+		    callback = alertObj.callbackFunction;
+		} else {
+			// mw.log( "PlayerControlBuilder :: displayAlert :: Error: bad callback type" );
+			callback = function() {};
+		}
+		
+		var $container = $( '<div />' ).addClass( 'alert-container' );
+		var $title = $( '<div />' ).text( alertObj.title ).addClass( 'alert-title alert-text' );
+		if ( alertObj.props && alertObj.props.titleTextColor ) {
+			$title.removeClass( 'alert-text' );
+			$title.css( 'color', mw.getHexColor( alertObj.props.titleTextColor ) );
+		}
+		var $message = $( '<div />' ).text( alertObj.message ).addClass( 'alert-message alert-text' );
 		if ( alertObj.isError ) {
 			$message.addClass( 'error' );
 		}
-        if ( alertObj.props && alertObj.props.textColor ) {
-            $message.removeClass( 'alert-text' );
-            $message.css( 'color', mw.getHexColor( alertObj.props.textColor ) );
-        }
-        var $buttonsContainer = $( '<div />' ).addClass( 'alert-buttons-container' );
-        if ( alertObj.props && alertObj.props.buttonRowSpacing ) {
-            $buttonsContainer.css( 'margin-top', alertObj.props.buttonRowSpacing );
-        }
-        var $buttonSet = alertObj.buttons || [];
-
-        // If no button was passed display just OK button
-        var buttonsNum = $buttonSet.length;
-        if ( buttonsNum == 0 && !alertObj.noButtons ) {
-            $buttonSet = ["OK"];
-            buttonsNum++;
-        }
-
-        $.each( $buttonSet, function(i) {
-            var label = this.toString();
-            var $currentButton = $( '<button />' )
-                .addClass( 'alert-button' )
-                .text( label )
-                .click( function( eventObject ) {
-                    callback( eventObject );
-                    embedPlayer.controlBuilder.closeAlert( alertObj.keepOverlay );
-                } );
-            if ( alertObj.props && alertObj.props.buttonHeight ) {
-                $currentButton.css( 'height', alertObj.props.buttonHeight );
-            }
-            // Apply buttons spacing only when more than one is present
-            if (buttonsNum > 1) {
-                if (i < buttonsNum-1) {
-                    if ( alertObj.props && alertObj.props.buttonSpacing ) {
-                        $currentButton.css( 'margin-right', alertObj.props.buttonSpacing );
-                    }
-                }
-            }
-            $buttonsContainer.append( $currentButton );
-        } )
-        $container.append( $title, $message, $buttonsContainer );
-        return embedPlayer.controlBuilder.displayMenuOverlay( $container, false, true );
-    },
+		if ( alertObj.props && alertObj.props.textColor ) {
+			$message.removeClass( 'alert-text' );
+			$message.css( 'color', mw.getHexColor( alertObj.props.textColor ) );
+		}
+		var $buttonsContainer = $( '<div />' ).addClass( 'alert-buttons-container' );
+		if ( alertObj.props && alertObj.props.buttonRowSpacing ) {
+			$buttonsContainer.css( 'margin-top', alertObj.props.buttonRowSpacing );
+		}
+		var $buttonSet = alertObj.buttons || [];
+		
+		// If no button was passed display just OK button
+		var buttonsNum = $buttonSet.length;
+		if ( buttonsNum == 0 && !alertObj.noButtons ) {
+			$buttonSet = ["OK"];
+			buttonsNum++;
+		}
+		
+		$.each( $buttonSet, function(i) {
+			var label = this.toString();
+			var $currentButton = $( '<button />' )
+			.addClass( 'alert-button' )
+				.text( label )
+				.click( function( eventObject ) {
+					callback( eventObject );
+					embedPlayer.controlBuilder.closeAlert( alertObj.keepOverlay );
+				} );
+			if ( alertObj.props && alertObj.props.buttonHeight ) {
+				currentButton.css( 'height', alertObj.props.buttonHeight );
+			}
+			// Apply buttons spacing only when more than one is present
+			if (buttonsNum > 1) {
+				if (i < buttonsNum-1) {
+					if ( alertObj.props && alertObj.props.buttonSpacing ) {
+						$currentButton.css( 'margin-right', alertObj.props.buttonSpacing );
+					}
+				}
+			}
+			$buttonsContainer.append( $currentButton );
+		} );
+		$container.append( $title, $message, $buttonsContainer );
+		return embedPlayer.controlBuilder.displayMenuOverlay( $container, false, true );
+	},
 
 	aboutPlayerLibrary: function(){
 		return $( '<div />' )
