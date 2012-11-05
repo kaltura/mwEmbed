3b6a4098785ae7f6ada014312ac069263f84defd
Avi Vaisenberger <avi.vaisenberger@kaltura.com>
2012-11-05 14:29:55 +0200

    Disabled google analytics quartiles events for entries with no duration (i.e Live streaming)

--- modules/GoogleAnalytics/resources/mw.GoogleAnalytics.js
+++ modules/GoogleAnalytics/resources/mw.GoogleAnalytics.js
@@ -199,6 +199,10 @@ uiConf Examples:
 			} );
 			$.each( _this.eventTrackList, function() {
 				var eventName = this;
+				// Disable quartiles for entries with no duration, i.e live streaming
+				if ( eventName == 'quartiles' && !_this.embedPlayer.duration ) {
+					return ;
+				}
 				var eventNameBinding = _this.getEventNameBinding( eventName );
 				_this.embedPlayer.addJsListener( eventNameBinding + _this.bindPostFix, function( data ) {
 					_this.playerEvent( eventName, data );
