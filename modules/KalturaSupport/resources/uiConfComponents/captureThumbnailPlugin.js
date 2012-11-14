74a073a6f21a4456957d70f676907927fc69dca6
Michael Dale <mdale@wikimedia.org>
2012-11-14 14:51:53 -0600

    fixed white space tabs

--- modules/KalturaSupport/resources/uiConfComponents/captureThumbnailPlugin.js
+++ modules/KalturaSupport/resources/uiConfComponents/captureThumbnailPlugin.js
@@ -1,9 +1,9 @@
 /* captureThumbnail Plugin:
 <Plugin id="captureThumbnail"
-    width="0%"
-    height="0%"
-    includeInLayout="false"
-    loadingPolicy="noWait"/>
+	width="0%"
+	height="0%"
+	includeInLayout="false"
+	loadingPolicy="noWait"/>
 
 <Button id="captureThumbBtnControllerScreen"
 	kClick="sendNotification( 'captureThumbnail' )"
@@ -27,117 +27,117 @@
 
 			bindPostFix : '.captureThumbnail',
 
-	    init: function( embedPlayer ) {
-	        this.embedPlayer = embedPlayer;
-	        this.addPlayerBindings();
-	        this.addCaptureButton();
-	    },
+		init: function( embedPlayer ) {
+			this.embedPlayer = embedPlayer;
+			this.addPlayerBindings();
+			this.addCaptureButton();
+		},
 
 		addPlayerBindings: function() {
 			var _this = this;
-	        var embedPlayer = this.embedPlayer;
+			var embedPlayer = this.embedPlayer;
 
 			// Unbind previously binded events by namespace
 			embedPlayer.unbindHelper( _this.bindPostFix );
-	        embedPlayer.bindHelper( 'captureThumbnail', function() {
-	            _this.captureThumbnail();
-	        } );
-	        embedPlayer.bindHelper( 'captureThumbnailFinished', function( e, isPlaying ) {
-	            _this.drawModal( isPlaying );
-	        } );
-	        embedPlayer.bindHelper( 'captureThumbnailError', function( e, isPlaying ) {
-	            _this.drawModal( isPlaying, true );
-	        } );
+			embedPlayer.bindHelper( 'captureThumbnail', function() {
+				_this.captureThumbnail();
+			} );
+			embedPlayer.bindHelper( 'captureThumbnailFinished', function( e, isPlaying ) {
+				_this.drawModal( isPlaying );
+			} );
+			embedPlayer.bindHelper( 'captureThumbnailError', function( e, isPlaying ) {
+				_this.drawModal( isPlaying, true );
+			} );
 		},
 
-	    addCaptureButton: function() {
+		addCaptureButton: function() {
 			var embedPlayer = this.embedPlayer;
-	        // TODO: We should have better support for kClick attribute [ sendNotification( 'flagForReview' ) ]
-	        // var captureButtonClick = embedPlayer.getKalturaConfig( 'captureThumbnail', 'kClick' );
+			// TODO: We should have better support for kClick attribute [ sendNotification( 'flagForReview' ) ]
+			// var captureButtonClick = embedPlayer.getKalturaConfig( 'captureThumbnail', 'kClick' );
+			
+			mw.log( 'captureThumbnailPlugin :: add button' );
+			embedPlayer.bindHelper( 'addControlBarComponent', function(event, controlBar ) {
+				var $captureButton = {
+					'w': 28,
+					'o': function( ctrlObj ) {
+						var $textButton = $( '<div />' )
+						.attr( 'title', embedPlayer.getKalturaConfig( 'captureThumbBtnControllerScreen', 'tooltip' ) )
+						.addClass( "ui-state-default ui-corner-all ui-icon-image ui-icon_link rButton" )
+						.append( $( '<span />' ).addClass( "ui-icon ui-icon-image" ) )
+						.buttonHover()
+						.click(function() {
+							embedPlayer.triggerHelper( 'captureThumbnail' );
+						} );
+						return $textButton;
+					}
+				};
+				
+				// Add the button to control bar
+				controlBar.supportedComponents['captureButton'] = true;
+				controlBar.components['captureButton'] = $captureButton;
+			} );
+		},
 
-	        mw.log( 'captureThumbnailPlugin :: add button' );
-	        embedPlayer.bindHelper( 'addControlBarComponent', function(event, controlBar ) {
-	            var $captureButton = {
-	                'w': 28,
-	                'o': function( ctrlObj ) {
-	                    var $textButton = $( '<div />' )
-	                        .attr( 'title', embedPlayer.getKalturaConfig( 'captureThumbBtnControllerScreen', 'tooltip' ) )
-	                        .addClass( "ui-state-default ui-corner-all ui-icon-image ui-icon_link rButton" )
-	                        .append( $( '<span />' ).addClass( "ui-icon ui-icon-image" ) )
-	                        .buttonHover()
-	                        .click(function() {
-	                            embedPlayer.triggerHelper( 'captureThumbnail' );
-	                        } );
-	                    return $textButton;
-	                }
-	            };
-
-	            // Add the button to control bar
-	            controlBar.supportedComponents['captureButton'] = true;
-	            controlBar.components['captureButton'] = $captureButton;
-	        } );
-	    },
-
-	    captureThumbnail: function() {
-	        var _this = this;
-	        var embedPlayer = this.embedPlayer;
-	        var isPlaying = embedPlayer.isPlaying();
-	        embedPlayer.pause();
-	        embedPlayer.addPlayerSpinner();
-	        var roundedTime = ( parseFloat( embedPlayer.currentTime ) ).toFixed( 3 );
-	        this.getKalturaClient().doRequest( {
+		captureThumbnail: function() {
+			var _this = this;
+			var embedPlayer = this.embedPlayer;
+			var isPlaying = embedPlayer.isPlaying();
+			embedPlayer.pause();
+			embedPlayer.addPlayerSpinner();
+			var roundedTime = ( parseFloat( embedPlayer.currentTime ) ).toFixed( 3 );
+			this.getKalturaClient().doRequest( {
 				'service' : 'thumbasset',
 				'action' : 'generate',
-	            'entryId' : embedPlayer.kentryid,
-	            'thumbParams:quality': 75,
-	            'thumbParams:videoOffset': roundedTime,
-	            'thumbParams:objectType': 'KalturaThumbParams',
-	            'thumbParams:requiredPermissions:-': ''
+				'entryId' : embedPlayer.kentryid,
+				'thumbParams:quality': 75,
+				'thumbParams:videoOffset': roundedTime,
+				'thumbParams:objectType': 'KalturaThumbParams',
+				'thumbParams:requiredPermissions:-': ''
 			}, function( data ) {
 				// In case of error, print an error message
 				if ( data.message && data.message.indexOf( "Error" ) != -1 ) {
 					embedPlayer.triggerHelper( 'captureThumbnailError', isPlaying );
 					return false;
 				}
-	            var thumbId = data.id;
-	            if ( thumbId ) {
-	                _this.kClient.doRequest( {
-	                    'service' : 'thumbasset',
-	                    'action' : 'setAsDefault',
-	                    'thumbAssetId' : thumbId
-	                }, function() {
-	                    embedPlayer.triggerHelper( 'captureThumbnailFinished', isPlaying );
-	                } );
-	            }
+				var thumbId = data.id;
+				if ( thumbId ) {
+					_this.kClient.doRequest( {
+						'service' : 'thumbasset',
+						'action' : 'setAsDefault',
+						'thumbAssetId' : thumbId
+					}, function() {
+						embedPlayer.triggerHelper( 'captureThumbnailFinished', isPlaying );
+					} );
+				}
 				return true;
-	        } );
-	    },
+			} );
+		},
 
 		drawModal: function( isPlaying, isError ) {
 			var embedPlayer = this.embedPlayer;
-	        var alertObj = {
-	            'title': 'Capture Thumbnail',
-	            'message': 'New thumbnail has been set',
-	            'buttons': [],
-	            'callbackFunction': function() {
-	                if ( isPlaying ) {
-	                    embedPlayer.play();
-	                }
-	            },
-	            'isExternal': false, // KDP defaults to false
-	            'isModal': true,
-	            'props': {
-	                'buttonRowSpacing': '5px'
-	            }
+			var alertObj = {
+				'title': 'Capture Thumbnail',
+				'message': 'New thumbnail has been set',
+				'buttons': [],
+				'callbackFunction': function() {
+					if ( isPlaying ) {
+						embedPlayer.play();
+					}
+				},
+				'isExternal': false, // KDP defaults to false
+				'isModal': true,
+				'props': {
+					'buttonRowSpacing': '5px'
+				}
 			};
 			if ( isError ) {
 				alertObj.message = 'An error occurred while trying to capture thumbnail'
 			}
-	        embedPlayer.hideSpinnerAndPlayBtn();
-	        embedPlayer.controlBuilder.displayAlert( alertObj );
+			embedPlayer.hideSpinnerAndPlayBtn();
+			embedPlayer.controlBuilder.displayAlert( alertObj );
 		},
 
-	    getKalturaClient: function() {
+		getKalturaClient: function() {
 			if( ! this.kClient ) {
 				this.kClient = mw.kApiGetPartnerClient( this.embedPlayer.kwidgetid );
 			}
