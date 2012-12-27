a6ab22fa9458d6582e3d53a6e5058059f3ed4d17
Michael Dale <mdale@wikimedia.org>
2012-12-27 13:46:00 -0600

    Chapters: updated desc for horz box width

--- kWidget/onPagePlugins/onPagePlugins.manifest.php
+++ kWidget/onPagePlugins/onPagePlugins.manifest.php
@@ -127,7 +127,7 @@ return array(
 				'type' => 'number',
 			),
 			'horizontalChapterBoxWidth'=> array(
-				'doc' => "The total width of the chapter box for thumbnail + description for horizontal layout, in pixels ( default 220 )",
+				'doc' => "The total width of the chapter box for horizontal layout, in pixels ( default 220 )",
 				'type' => 'nubmer',
 			),
 			'thumbnailRotator' => array(
