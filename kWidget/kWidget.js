10ee32660027c41ed5d1d764ee411871cbdbea9b
Michael Dale <mdale@wikimedia.org>
2012-12-28 00:25:31 -0600

    ThumbEmbed: fix support for thumb embed

--- kWidget/kWidget.js
+++ kWidget/kWidget.js
@@ -397,7 +397,7 @@ var kWidget = {
 			'<div class="kWidgetCentered kWidgetPlayBtn" ' +
 				'id="' + targetId + '_playBtn"' +
 			'></div></div>';
-		// Add a click binding to do the realy embed:
+		// Add a click binding to do the really embed:
 		document.getElementById( targetId + '_playBtn' ).addEventListener( 'click', function(){
 			// Check for the ready callback:
 			if( settings.readyCallback ){
@@ -754,7 +754,7 @@ var kWidget = {
 		
 		// Check if we need to capture a play event ( iOS sync embed call )
 		if( settings.captureClickEventForiOS && this.isIOS() ){
-			this.captureClickWrapedIframeUpdate(  targetId, settings, iframe );
+			this.captureClickWrapedIframeUpdate( targetId, settings, iframe );
 			return ;
 		}
 		// get the callback name:
@@ -818,17 +818,21 @@ var kWidget = {
 		newDoc.write( '<html>' +
 			'<head></head>' +
 			'<body>' +
-				'<video class="persistentNativePlayer" ' +
-					'id="' + targetId + '" ' +
-					'kwidgetid="' + settings.wid + '" '+
-					'kentryid="' + settings.entry_id + '" ' +
-					'kuiconfid="' + settings.uiconf_id + '" ' +
-					//'poster="' + _this.getKalturaThumbUrl( settings ) + '" ' +
-					// Only applies to iOS, and only to caputre the play event,
-					// so we only include a low bitrate mp4
-					'src="' + vidSrc + '" ' +
-					'style="width:100%;height:100%" ' +
-				'</video>' +
+				'<div class="mwPlayerContainer"  style="width: 100%; height: 100%">' +
+					'<div class="videoHolder">' +
+						'<video class="persistentNativePlayer" ' +
+							'id="' + targetId + '" ' +
+							'kwidgetid="' + settings.wid + '" '+
+							'kentryid="' + settings.entry_id + '" ' +
+							'kuiconfid="' + settings.uiconf_id + '" ' +
+							//'poster="' + _this.getKalturaThumbUrl( settings ) + '" ' +
+							// Only applies to iOS, and only to caputre the play event,
+							// so we only include a low bitrate mp4
+							'src="' + vidSrc + '" ' +
+							'style="width:100%;height:100%" ' +
+						'</video>' +
+					'</div>' +
+				'</div>' +
 				// issue play on the silent black video ( to capture iOS gesture )
 				'<script>document.getElementById(\'' + targetId + '\').play();</script>' +
 				'<div id="scriptsHolder"></div>' +
@@ -865,6 +869,7 @@ var kWidget = {
 			}
 
 			var scripts = [];
+			var headElm = head.childNodes;
 			//var ret = iframeElm.contentDocument.body.childNodes;
 			var ret = iframeElm.contentDocument.getElementById("scriptsHolder").childNodes;
 			for ( var i = 0; ret[i]; i++ ) {
