b0e19f4e0c4aad9a2ddf33813f1674cadc189fd2
oferico <ofer.luft@kaltura.com>
2013-01-09 19:01:09 -0500

    Update kWidget/onPagePlugins/chapters/chaptersEdit.js

--- kWidget/onPagePlugins/chapters/chaptersEdit.js
+++ kWidget/onPagePlugins/chapters/chaptersEdit.js
@@ -258,7 +258,7 @@ kWidget.addReadyCallback( function( playerId ){
 			// have a max of 10 time listings across the width
 			var listingWidth = this.$timeline.width() / numOfTimeIncludes;
 			
-			var docstext = "Click anywhere within the timeline area to add a new chapter";
+			var docstext = "Click anywhere!!! within the timeline area to add a new chapter";
 			
 			console.log("cuePoints: "+this.cuePoints);
 			if (this.cuePoints.length > 0) {
