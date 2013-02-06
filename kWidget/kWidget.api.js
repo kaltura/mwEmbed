1f55bdbae550014e2c8af5f114f645c7ea2d6ab8
ranyefet <ran@yefet.net>
2013-02-06 11:37:22 +0200

    changed char variable to currentChar

--- kWidget/kWidget.api.js
+++ kWidget/kWidget.api.js
@@ -222,8 +222,8 @@ kWidget.api.prototype = {
 		var hash = 0;
 		if (str.length == 0) return hash;
 		for (var i = 0; i < str.length; i++) {
-			var char = str.charCodeAt(i);
-			hash = ((hash<<5)-hash)+char;
+			var currentChar = str.charCodeAt(i);
+			hash = ((hash<<5)-hash)+currentChar;
 			hash = hash & hash; // Convert to 32bit integer
 		}
 		return hash;
