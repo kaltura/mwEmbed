4f3ddcd6d12539087b098a9a1944281b15e23be8
Michael Dale <mdale@wikimedia.org>
2013-03-06 09:00:35 -0600

    IpadNative Controls demo page fixes

---
+++ modules/EmbedPlayer/EmbedPlayer.manifest.php
@@ -0,0 +1,11 @@
+<?php 
+return array(
+	'EmbedPlayer.OverlayControls' => array(
+		'type' => 'boolean',
+		'doc' => "If the player controls should be overlaid on top of the video. Can also be set to false per embed player via overlayControls attribute"
+	),
+	'EmbedPlayer.EnableIpadHTMLControls' => array(
+		'type' => 'boolean',
+		'doc' => "Set to false to use native device controls on iPad. By default this is true, and we use HTML controls on iPad"
+	),
+);
