3bf633e2afcc2a459b36406513191ef1b88f0d00
Itay Kinnrot <itayk@Itays-MacBook-Air.local>
2013-03-12 11:42:44 +0200

    bump version 1.7.4

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,7 +12,7 @@ $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
 
-$wgMwEmbedVersion = '1.7.3';
+$wgMwEmbedVersion = '1.7.4';
 
 
 // Default HTTP protocol from GET or SERVER parameters
