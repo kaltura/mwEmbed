b56e1b3901bd200c19b24760169891f4834e1a6d
Zohar Babin <zohar.babin@kaltura.com>
2013-03-14 20:19:24 -0400

    made some things configureable. looking gooood :)

--- kWidget/onPagePlugins/limeSurveyCuePointForms/limeSurveyCuePointForms.js
+++ kWidget/onPagePlugins/limeSurveyCuePointForms/limeSurveyCuePointForms.js
@@ -54,13 +54,19 @@
 			
 			// Add a form to the player:
 			var pos = $(this.kdp).position();
+            
+            //Get background color values:
+            var bgalpha = this.getConfig('backgroundAlpha');
+            var bgcolorRGB = this.hexToRgb(this.getConfig('backgroundHexColor'));
+                
 			// remove any old form:
 			$( '#' + this.getFormContainerId() ).remove();
 			$(this.kdp).after(
 				$('<div>')
 				.attr('id', this.getFormContainerId() )
 				.css({
-					'background-color': 'rgba(255, 255, 255, .9)',
+					'background-color': 'rgba('+bgcolorRGB.r+', '+bgcolorRGB.g+', '+bgcolorRGB.b+', '+bgalpha+')',
+                    'border' : 'none',
 					'position': 'absolute',
 					'top' : pos.top,
 					'left': pos.left,
@@ -69,23 +75,25 @@
 				}).append(
 					$('<iframe>')
 					.css({
+                        'border' : 'none',
 						'width':'100%',
 						'height': '100%'
 					})
-					.attr('src', _this.currentFormData.limeSurveyURL + "?orgin=" + document.domain )
+					.attr('src', _this.currentFormData.limeSurveyURL + "?orgin=" + document.domain + "&playerId=" + _this.playerId )
 				)
 			)
 			window.addEventListener("message", function(event){
-				var hreflimeUrl = $('<a>').prop('href', _this.currentFormData.limeSurveyURL);
+                var hreflimeUrl = $('<a>').prop('href', _this.currentFormData.limeSurveyURL);
                 var limeHostname = hreflimeUrl.prop('protocol') + '//' + hreflimeUrl.prop('hostname');
                 if (event.origin !== limeHostname){
 					return;
 				}
-				if( event.data == 'ok' ){
+                var messagePayload = JSON.parse(event.data);
+				if( messagePayload.status == 'ok' && messagePayload.playerId == _this.playerId){
 					setTimeout(function(){
 						$( '#' + _this.getFormContainerId() ).fadeOut('fast');
 						_this.kdp.sendNotification('doPlay');
-					}, 3000 );
+					},  _this.currentFormData.fadeOutTimeMs);
 				}
 			}, false);
 			
@@ -141,6 +149,22 @@
 		// get any of this plugin configuration values
 		getConfig : function( attr ) {
 			return this.getAttr(this.pluginName + '.' + attr);
-		}
+		},
+        
+        //calculate RGB components from hex color
+        hexToRgb : function(hex) {
+            // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+            var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+            hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+                return r + r + g + g + b + b;
+            });
+        
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            return result ? {
+                r: parseInt(result[1], 16),
+                g: parseInt(result[2], 16),
+                b: parseInt(result[3], 16)
+            } : null;
+        }
 	}
 })();
