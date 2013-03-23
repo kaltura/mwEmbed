1965cc21c50f5649adf630f30cb4c9922dc36e3d
Hadar Weiss <whadar@gmail.com>
2013-03-23 02:08:56 +0200

    overlayUI fit Kaltura player; changed vid;
    and also cleaned up some old code

--- modules/Peer5/resources/mw.Peer5.js
+++ modules/Peer5/resources/mw.Peer5.js
@@ -1,38 +1,42 @@
-( function( mw, $ ) { "use strict";
+(function (mw, $) {
+    "use strict";
 
-mw.Peer5 = function( embedPlayer, callback ){
-	return this.init( embedPlayer, callback );
-};
+    mw.Peer5 = function (embedPlayer, callback) {
+        return this.init(embedPlayer, callback);
+    };
 
-mw.Peer5.prototype = {
-	bindPostfix: '.Peer5',
-	
-	init: function(  embedPlayer, callback){
-		var _this = this;
-		this.embedPlayer = embedPlayer;
-		// Load Peer5 
-		kWidget.appendScriptUrl( _this.getConfig('peer5libUrl'), function(){
-			// bind player
-			_this.bindPlayer();
-			// continue player build out:
-			callback();
-		}, document );
-	},
-	bindPlayer: function( event, embedPlayer ) {
-		// checkPlayerSourcesEvent ( add peer5 mediaStream source )
-		
-		$( this.embedPlayer ).bind( 'playerReady', function( event, callback ) {
-			var vid = document.querySelector('video')
-			peer5.create(vid, 'http://wpc.a09f.edgecastcdn.net/80A09F/test/pilots/metacafe/1_dash.mp4', 'video/mp4; codecs="avc1.64001f,mp4a.40.2"');
-			peer5.setLogLevel(2);		
-			this.mediaElement.selectedSource.src = vid.src;	
-			$(this.getInterface()).find( '.ui-widget.source-switch' ).text('Peer5 Demo for Chrome');
-		});		
-	},
-	getConfig: function( propId ){
-		// return the attribute value
-		return this.embedPlayer.getKalturaConfig( 'peer5', propId );
-	}
-}
+    mw.Peer5.prototype = {
+        bindPostfix:'.Peer5',
 
-} )( window.mw, window.jQuery );
+        init:function (embedPlayer, callback) {
+            var _this = this;
+            this.embedPlayer = embedPlayer;
+            // Load Peer5
+            kWidget.appendScriptUrl(_this.getConfig('peer5libUrl'), function () {
+                // bind player
+                _this.bindPlayer();
+                // continue player build out:
+                callback();
+            }, document);
+        },
+        bindPlayer:function (event, embedPlayer) {
+            // checkPlayerSourcesEvent ( add peer5 mediaStream source )
+
+            $(this.embedPlayer).bind('playerReady', function (event, callback) {
+                var vid = document.querySelector('video');
+                peer5.create(vid, 'http://commondatastorage.googleapis.com/peer5_vod/wind2.mp4', 'video/mp4; codecs="avc1.64001f,mp4a.40.2"',
+                    {overlayUI:{
+                        chunks_area_style:'position: absolute; top: 408px;left: 40px;width: 563px;'
+                    }});
+
+                this.mediaElement.selectedSource.src = vid.src;
+                $(this.getInterface()).find('.ui-widget.source-switch').text('Peer5 Demo for Chrome');
+            });
+        },
+        getConfig:function (propId) {
+            // return the attribute value
+            return this.embedPlayer.getKalturaConfig('peer5', propId);
+        }
+    }
+
+})(window.mw, window.jQuery);
