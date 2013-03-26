7220c3e1c492486246a3e66e176a61b98fd66499
Michael Dale <mdale@wikimedia.org>
2013-03-26 09:02:25 -0600

    Peer5: minimize loader, improve description

--- modules/Peer5/Peer5.loader.js
+++ modules/Peer5/Peer5.loader.js
@@ -1,43 +1,16 @@
 ( function( mw, $ ) { "use strict";
-
 	mw.addKalturaPlugin( ['mw.Peer5'], 'peer5', function( embedPlayer, callback){
-		// do user agent checks {
-
-		var getMajorVersionNumber=function(fullVersion){
-          var arr = fullVersion.split('.');
-          var number = parseInt(arr[0]);
-          return number;
-        };
-
-		var detectBrowser=function () {
-            var N = navigator.appName, ua = navigator.userAgent, tem;
-            var M = ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
-            if (M && (tem = ua.match(/version\/([\.\d]+)/i)) != null) M[2] = tem[1];
-            M = M ? [M[1], M[2]] : [N, navigator.appVersion, '-?'];
-            return M;
-        };
-
-		var getMajorVersionNumber=function(fullVersion){
-          var arr = fullVersion.split('.');
-          var number = parseInt(arr[0]);
-          return number;
-        };
-
-		var browserDetails = detectBrowser();
-        var browserName = browserDetails[0].toLowerCase();
-        var browserVersion = getMajorVersionNumber(browserDetails[1]);
-
-		if( browserName == 'chrome' && browserVersion > 25 ){
-			embedPlayer.peer5 = new mw.Peer5( embedPlayer, callback );
-			return ;
+		var ua = navigator.userAgent;
+		if( /chrome/i.test(ua)) {
+			var uaArray = ua.split(' ');
+			// check chrome browser version: 
+			if( parseInt( uaArray[uaArray.length - 2].substr(7).split('.')[0] ) > 25 ){
+				embedPlayer.peer5 = new mw.Peer5( embedPlayer, callback );
+				return ;
+			}
 		}
-
-        //if not HTML5 supported, should not get in!
-
+		//if not HTML5 supported, should not get in!
 		//be sure to issue callback if not running plugin:
-        callback();
-		
+		callback();
 	});
-
-
 })( window.mw, jQuery );
--- modules/Peer5/Peer5.manifest.php
+++ modules/Peer5/Peer5.manifest.php
@@ -6,7 +6,9 @@
 return array (
 	/** Playlist */
 	'peer5' => array(
-		'description' => 'The Peer5 P2P distrabution <a title="Peer5" href="http://peer5.com" target="_new">learn more</a>',
+		'description' => 'The Peer5 P2P distrabution <a title="Peer5" href="http://peer5.com" target="_new">learn more</a>. <br>
+		Note you must use google chrome version 25 or greater to use the p2p transport. Other browsers
+		will just use http progressive streaming.',
 		'attributes' => array(
 			'peer5libUrl' => array(
 				'doc' => "The URL to Peer5 SaaS",
