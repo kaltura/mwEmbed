baa1e44d1de851986865be07139c37f90fb31199
Hadar Weiss <whadar@gmail.com>
2013-03-28 16:07:02 +0200

    Added tests and externalized params kWidget.embed

--- modules/Peer5/resources/mw.Peer5.js
+++ modules/Peer5/resources/mw.Peer5.js
@@ -7,11 +7,17 @@
 
     mw.Peer5.prototype = {
         bindPostfix:'.Peer5',
+        peer5_vid:null,
+        peer5_type:null,
+        peer5_options:null,
 
         init:function (embedPlayer, callback) {
             var _this = this;
             this.embedPlayer = embedPlayer;
             // Load Peer5
+            this.peer5_vid = this.getConfig('url');
+            this.peer5_type = this.getConfig('type');
+            this.peer5_overlayUI = this.getConfig('overlayUI');
             kWidget.appendScriptUrl(_this.getConfig('peer5libUrl'), function () {
                 // bind player
                 _this.bindPlayer();
@@ -22,16 +28,35 @@
         bindPlayer:function (event, embedPlayer) {
             // checkPlayerSourcesEvent ( add peer5 mediaStream source )
 
+            var _this = this;
             $(this.embedPlayer).bind('playerReady', function (event, callback) {
-                var vid = document.querySelector('video');
-                peer5.create(vid, 'http://commondatastorage.googleapis.com/peer5_vod/wind2.mp4', 'video/mp4; codecs="avc1.64001f,mp4a.40.2"',
-                    {overlayUI:{
+                    var vid = this.getPlayerElement();
+
+                    var url = _this.peer5_vid || vid.src;
+                    var type = (this.getSource().mimeType == 'mp4')?'video/mp4; codecs="avc1.64001f,mp4a.40.2"':null;
+
+                    var options = {};
+                    var overlay = {
+                        chunks_area_style:'position: absolute; top: 399px;left: 40px;width: 563px;'
+                    };
+
+                    if (_this.peer5_overlayUI) {
+                        options.overlayUI = overlay;
+                    }
+
+                    peer5.create(vid, url, type, {overlayUI:{
                         chunks_area_style:'position: absolute; top: 399px;left: 40px;width: 563px;'
                     }});
-
-                this.mediaElement.selectedSource.src = vid.src;
-                $(this.getInterface()).find('.ui-widget.source-switch').text('Peer5 Demo for Chrome');
-            });
+//
+//                    peer5.create(vid, 'http://commondatastorage.googleapis.com/peer5_vod/wind2.mp4', 'video/mp4; codecs="avc1.64001f,mp4a.40.2"',
+//                        {overlayUI:{
+//                            chunks_area_style:'position: absolute; top: 399px;left: 40px;width: 563px;'
+//                        }});
+                    this.mediaElement.selectedSource.src = vid.src;
+                    $(this.getInterface()).find('.ui-widget.source-switch').text('Peer5 Demo for Chrome');
+                }
+            )
+            ;
         },
         getConfig:function (propId) {
             // return the attribute value
@@ -39,4 +64,5 @@
         }
     }
 
-})(window.mw, window.jQuery);
+})
+    (window.mw, window.jQuery);
