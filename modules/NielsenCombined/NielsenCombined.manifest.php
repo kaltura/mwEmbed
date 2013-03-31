43c4da1e4b22a32f2c3a4449494b93b3013236eb
Michael Dale <mdale@wikimedia.org>
2013-03-31 09:15:20 -0600

    Docs: updated Nielsen combined desc

--- modules/NielsenCombined/NielsenCombined.manifest.php
+++ modules/NielsenCombined/NielsenCombined.manifest.php
@@ -6,7 +6,12 @@
 
 return array(
 	'nielsenCombined' => array(
-		'description' => "Nielsen Combined",
+		'description' => "Nielsen Combined enables, media owners and advertisers to track video playback 
+		as well as measure accurately 
+		the size and demographic composition (age, gender, etc.) of audiences viewing video online, 
+		across the entire web and for specific sites. Lean more at 
+		<a target=\"new\" href=\"http://www.nielsen.com/us/en/nielsen-solutions.html\">Nielsen.com</a>
+		The Kaltura Integration supports the full Combined becoan event flow.",
 		'attributes'=> array(
 			'clientId' => array(
 				'doc' => "The client id",
