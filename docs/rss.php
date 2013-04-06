c0a208af76f6b4e8f591a0efeded79579485a8b0
Michael Dale <mdale@wikimedia.org>
2013-04-05 21:48:09 -0600

    Docs: updated encoding to UTF-8

--- docs/rss.php
+++ docs/rss.php
@@ -1,4 +1,4 @@
-<?php echo '<?xml version="1.0" encoding="ISO-8859-1" ?>' . "\n";
+<?php echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
 // Path to local git binnary
 $kgGitBinPath = '/usr/bin/git';
 
