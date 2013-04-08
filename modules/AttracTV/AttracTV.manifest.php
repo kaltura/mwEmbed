e0c99ca2b01a7142a4fba2e739859e211f8de65f
Omer Paran <omer.paran@gmail.com>
2013-04-08 17:34:23 +0300

    AttracTV Initial Commit

---
+++ modules/AttracTV/AttracTV.manifest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Tremor plugin manifest 
+*/
+
+return array (
+	/** Playlist */
+	'AttracTV' => array(
+		'description' => 'The AttracTV p2p distrabution learn more',
+		'attributes' => array(
+			'AttracTVlibUrl' => array(
+				'doc' => "The URL to your peer 5 library",
+				'type' => 'string'
+			),
+		)
+	)
+);
