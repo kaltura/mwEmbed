440cb6ba93875459c8f6222c4093793a69df40ad
Michael Dale <mdale@wikimedia.org>
2013-04-15 12:12:08 -0500

    Docs: add missing comma

--- modules/Peer5/Peer5.manifest.php
+++ modules/Peer5/Peer5.manifest.php
@@ -8,7 +8,7 @@ return array (
 	'peer5' => array(
 		'description' => 'The Peer5 P2P distribution <a title="Peer5" href="http://peer5.com" target="_new">learn more</a>. <br>        
 		Requirements: Google Chrome version (M26+) Other browsers will just use http progressive streaming. <br>
-		The highest bitrate available will be used, unless specified otherwise using mediaProxy.preferedFlavorBR param <br>'
+		The highest bitrate available will be used, unless specified otherwise using mediaProxy.preferedFlavorBR param <br>',
 		'attributes' => array(
 			'peer5libUrl' => array(
 				'doc' => "The URL to Peer5 SaaS",
