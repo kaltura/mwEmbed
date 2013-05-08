df50f74e499e5907de28200eb9069d6f78b237d1
Itay Kinnrot <itay.kinnrot@kaltura.com>
2013-05-08 09:48:25 +0300

    SUP-46 #fix #comment add .tt sufix to the caption - low risk file #time 2h

--- modules/TimedText/resources/mw.TextSource.js
+++ modules/TimedText/resources/mw.TextSource.js
@@ -161,7 +161,7 @@
 					break;
 			}
 			// check for other indicators ( where the caption is missing metadata )
-			if( this.src && this.src.substr( -4 ) == 'ttml' ){
+			if( this.src && (this.src.substr( -4 ) == 'ttml' || this.src.substr( -2 ) == "tt") ){
 				return this.getCaptionsFromTMML( data );
 			}
 			if( this.src && this.src.substr( -3 ) == 'srt' ){
