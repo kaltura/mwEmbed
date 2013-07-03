2bbdedd52f50b9053da1d7dbfc8f2f506f835e3c
ranyefet <ran@yefet.net>
2013-07-03 11:40:38 +0300

    Added mapping file for kaltura modules

---
+++ modules/KalturaSupport/KalturaModulesMap.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * mwEmbed Modules to Kaltura Plugins mapping 
+ **/
+
+return array(
+	
+	'AdSupport'          => array( 'vast', 'bumper' ),
+	'AkamaiAnalytics'    => 'akamaiMediaAnalytics',
+	'AttracTV'           => 'AttracTV',
+	'Comscore'           => 'comscore',
+	'Conviva'            => 'Conviva',
+	'DoubleClick'        => array( 'DoubleClick', 'doubleClick' ),
+	'FreeWheel'          => array( 'FreeWheel', 'freeWheel' ),
+	'GoogleAnaytics'     => 'googleAnalytics',
+	'NielsenCombined'    => 'nielsenCombined',
+	'NielsenVideoCensus' => 'nielsenVideoCensus',
+	'Omniture'           => array( 'omniture', 'siteCatalyst15' ),
+	'Peer5'              => 'peer5',
+	'Playlist'           => 'playlistAPI',
+	'Plymedia'           => 'plymedia',
+	'TimedText'          => array( 'closedCaptions', 'closedCaptionsUnderPlayer', 'closedCaptionsOverPlayer', 'closedCaptionsFlexible' ),
+	'Tremor'             => 'tremor',
+);
