765af00399a7d40557c285b8d3d0e8c55e5d0b6b
ranyefet <ran@yefet.net>
2013-07-08 23:06:34 +0300

    Added autoInit parameter to PluginFactory::register

--- modules/MwEmbedSupport/mw.PluginFactory.js
+++ modules/MwEmbedSupport/mw.PluginFactory.js
@@ -1,46 +1,56 @@
 ( function( mw, $ ) {
-	// exit if already defined
-	if( mw.PluginFactory ) return ;
 
-	mw.PluginFactory = {
+// exit if already defined
+if( mw.PluginFactory ) return ;
 
-		// Holds our plugins classes
-		registerdPlugins: {},
+mw.PluginFactory = {
 
-		// Holds our initialise plugins
-		initialisePlugins: {},
+	// Holds our plugins classes
+	registerdPlugins: {},
 
-		// Register a new Plugin
-		register: function( pluginName, pluginClass ){
-			if( !this.registerdPlugins[ pluginName ] ) {
-				this.registerdPlugins[ pluginName ] = pluginClass;
-			}
-			return this;
-		},
-		getClass: function( pluginName ) {
-			if( !this.registerdPlugins[ pluginName ] ) {
-				mw.log('PluginFactory::init: Plugin "' + pluginName + '" not registered.');
-				return;
-			}
-			return this.registerdPlugins[ pluginName ];
-		},
-		init: function( pluginName ){
+	// Holds our initialise plugins
+	initialisePlugins: {},
 
-			if( !this.registerdPlugins[ pluginName ] ) {
-				mw.log('PluginFactory::init: Plugin "' + pluginName + '" not registered.');
-				return;
-			}
-			if( this.initialisePlugins[ pluginName ] ) {
-				mw.log('PluginFactory::init: Plugin "' + pluginName + '" already initialised.');
-				return;
-			}
-			var _this = this;
-			mw.addKalturaPlugin( pluginName, function( embedPlayer, callback ){
-				var pluginClass = _this.getClass( pluginName );
-				_this.initialisePlugins[ pluginName ] = new pluginClass( embedPlayer, callback, pluginName );
-			});
-			return this;
+	// Register a new Plugin
+	register: function( pluginName, pluginClass, autoInit ){
+		if( this.registerdPlugins[ pluginName ] ) {
+			mw.log('PluginFactory::register: Plugin "' + pluginName + '" already registered.');
+			return;
 		}
-	};
+
+		this.registerdPlugins[ pluginName ] = pluginClass;
+
+		// By default we automaticaly init plugin on registration
+		if( autoInit === undefined ) {
+			autoInit = true;
+		}
+		if( autoInit ) {
+			this.init( pluginName );
+		}
+	},
+	getClass: function( pluginName ) {
+		if( !this.registerdPlugins[ pluginName ] ) {
+			mw.log('PluginFactory::getClass: Plugin "' + pluginName + '" not registered.');
+			return;
+		}
+		return this.registerdPlugins[ pluginName ];
+	},
+	init: function( pluginName ){
+		if( !this.registerdPlugins[ pluginName ] ) {
+			mw.log('PluginFactory::init: Plugin "' + pluginName + '" not registered.');
+			return;
+		}
+		if( this.initialisePlugins[ pluginName ] ) {
+			mw.log('PluginFactory::init: Plugin "' + pluginName + '" already initialised.');
+			return;
+		}
+		var _this = this;
+		mw.addKalturaPlugin( pluginName, function( embedPlayer, callback ){
+			var pluginClass = _this.getClass( pluginName );
+			_this.initialisePlugins[ pluginName ] = new pluginClass( embedPlayer, callback, pluginName );
+		});
+		return this;
+	}
+};
 
 } )( mediaWiki, jQuery );
