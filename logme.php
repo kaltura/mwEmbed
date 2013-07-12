36c9320afc20cee0a1c8705db7f57b9c54e93bce
Itay Kinnrot <itay.kinnrot@kaltura.com>
2013-07-12 15:15:22 +0300

    add external log support

--- logme.php
+++ logme.php
@@ -2,6 +2,13 @@
 
 $data = '';
 $myDir = dirname( __FILE__ );
+$fileName = $myDir . '/logClient.txt';
+if (isset($_GET['delete']))
+{
+	file_put_contents($fileName, ' '	);
+	exit(0);
+
+} 
 $userAgent = $_SERVER['HTTP_USER_AGENT'];
 
 if (isset($_GET['logname']))
@@ -22,8 +29,8 @@ if (isset($_REQUEST['logdata']))
 }
 
 
-file_put_contents($myDir . '/logClient.txt', $data,FILE_APPEND	);
+file_put_contents($fileName, $data,FILE_APPEND	);
 //mail('itay.kinnrot@kaltura.com', 'My Subject', $data);
 
 ?>
-dfdsfdsf
+VVV
