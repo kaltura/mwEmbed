d3ab5e76b9fbc9bcd4df4fc5a399c07232feaef9
ranyefet <ran@yefet.net>
2013-07-23 18:08:34 +0300

    playHead.js cleanup

--- modules/KalturaSupport/components/playHead.js
+++ modules/KalturaSupport/components/playHead.js
@@ -53,24 +53,13 @@
 				// we want less than monitor rate for smoth animation
 				animate: mw.getConfig( 'EmbedPlayer.MonitorRate' ) - ( mw.getConfig( 'EmbedPlayer.MonitorRate' ) / 30 ) ,
 				start: function( event, ui ) {
-					var id = ( embedPlayer.pc != null ) ? embedPlayer.pc.pp.id:embedPlayer.id;
 					embedPlayer.userSlide = true;
-					// If playlist always start at 0
-					embedPlayer.startTimeSec = ( embedPlayer.instanceOf == 'mvPlayList' ) ? 0:
-									mw.npt2seconds( embedPlayer.getTimeRange().split( '/' )[0] );
 				},
 				slide: function( event, ui ) {
 					var perc = ui.value / 1000;
 					// always update the title 
 					$( this ).find('.ui-slider-handle').attr('data-title', mw.seconds2npt( perc * embedPlayer.getDuration() ) );
 					
-					embedPlayer.jumpTime = mw.seconds2npt( parseFloat( parseFloat( embedPlayer.getDuration() ) * perc ) + embedPlayer.startTimeSec );
-					// mw.log('perc:' + perc + ' * ' + embedPlayer.getDuration() + ' jt:'+ this.jumpTime);
-					if ( _this.longTimeDisp ) {
-						ctrlObj.setStatus( gM( 'mwe-embedplayer-seek_to', embedPlayer.jumpTime ) );
-					} else {
-						ctrlObj.setStatus( embedPlayer.jumpTime );
-					}
 					// Update the thumbnail / frame
 					if ( embedPlayer.isPlaying == false ) {
 						embedPlayer.updateThumbPerc( perc );
@@ -86,10 +75,6 @@
 						embedPlayer.userSlide = false;
 						embedPlayer.seeking = true;
 
-						// set seek time (in case we have to do a url seek)
-						embedPlayer.seekTimeSec = mw.npt2seconds( embedPlayer.jumpTime, true );
-						mw.log( 'PlayerLayoutBuilder:: seek to: ' + embedPlayer.jumpTime + ' perc:' + perc + ' sts:' + embedPlayer.seekTimeSec );
-						ctrlObj.setStatus( gM( 'mwe-embedplayer-seeking' ) );
 						if( embedPlayer.isStopped() ){
 							embedPlayer.play();
 						}
