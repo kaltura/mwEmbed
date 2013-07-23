dfb29122e4024cfcaf1f3fc06f14744105823ea9
Itay Kinnrot <itay.kinnrot@kaltura.com>
2013-07-23 08:50:09 +0300

    1.9.0.rc1

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,7 +12,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '1.8.9';
+$wgMwEmbedVersion = '1.9.0.rc1';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
