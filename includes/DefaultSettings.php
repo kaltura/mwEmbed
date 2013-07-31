fe58877c99836ea0065dad7163756d15e90c1271
Eliza Sapir <eliza.sapir@kaltura.com>
2013-07-31 15:24:50 +0300

    the version was updated to 1.8.9.6

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,7 +12,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '1.8.9.3';
+$wgMwEmbedVersion = '1.8.9.6';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
