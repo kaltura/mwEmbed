1f80875f88ea0778e7eabf956d32046cf607ac6a
Itay Kinnrot <itay.kinnrot@kaltura.com>
2013-08-05 21:45:32 +0300

    v1.9.0.rc4 -fix share plugin in html5

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,7 +12,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '1.9.0.rc3';
+$wgMwEmbedVersion = '1.9.0.rc4';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
