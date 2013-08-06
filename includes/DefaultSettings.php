1a02a1f24fc2dc1bc197872504c7c7cc7e05efd6
MichalRadwantzor <michal.radwantzor@kaltura.com>
2013-08-06 14:10:24 +0300

    version 1.9.0.rc5

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,7 +12,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '1.9.0.rc4';
+$wgMwEmbedVersion = '1.9.0.rc5';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
