4349fda49875796555b41b2686488d822bfcf45d
Nu Butthajit <nbutthajit@gmail.com>
2013-08-15 00:04:32 -0700

    forEach is not available in IE8 and broke the Mutliplaylist multiRequest.  Changed it up to a friendlier version.

--- kWidget/kWidget.js
+++ kWidget/kWidget.js
@@ -1770,22 +1770,23 @@ var kWidget = {
 	 },
 	 // similar to jQuery.extend 
 	 extend: function( obj ){
-		 Array.prototype.slice.call(arguments, 1).forEach(function(source) {
-			if (source) {
-				for (var prop in source) {
-					if (source[prop].constructor === Object) {
-						if (!obj[prop] || obj[prop].constructor === Object) {
-							obj[prop] = obj[prop] || {};
-							extend(obj[prop], source[prop]);
+			var slicer	= Array.prototype.slice.call(arguments, 1);
+			$.each(slicer, function(index,source){
+				if (source) {
+					for (var prop in source) {
+						if (source[prop].constructor === Object) {
+							if (!obj[prop] || obj[prop].constructor === Object) {
+								obj[prop] = obj[prop] || {};
+								extend(obj[prop], source[prop]);
+							} else {
+								obj[prop] = source[prop];
+							}
 						} else {
 							obj[prop] = source[prop];
 						}
-					} else {
-						obj[prop] = source[prop];
 					}
 				}
-			}
-		});
+			});
 		return obj;
 	},
 	// similar to parm
