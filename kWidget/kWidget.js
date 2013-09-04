157617d905475c878465d67673a97e9cab825259
ranyefet <ran@yefet.net>
2013-09-04 15:28:51 +0300

    Use Videotag for IE9

--- kWidget/kWidget.js
+++ kWidget/kWidget.js
@@ -1248,11 +1248,6 @@ var kWidget = {
 			return false;
 		}
 		var dummyvid = document.createElement( "video" );
-		// IE9 is grade B HTML5 support only invoke it if forceMobileHTML5 is true,
-		// but for normal tests we categorize it as ~not~ supporting html5 video.
-		if( navigator.userAgent.indexOf( 'MSIE 9.' ) != -1 ){
-			return false;
-		}
 		if( dummyvid.canPlayType ) {
 			return true;
 		}
