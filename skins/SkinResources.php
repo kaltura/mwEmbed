bc296c80948786f24808df13c3002af105b711d0
ranyefet <ran@yefet.net>
2013-10-02 12:40:28 +0300

    Added mw.EmbedPlayer as dependency for our skin

--- skins/SkinResources.php
+++ skins/SkinResources.php
@@ -3,6 +3,11 @@
 // go over the folder list 
 return array(
 	'kdark' => array(
+		/** 
+		  * We need to have mw.EmbedPlayer dependency for our skin
+		  * So that the Core CSS will load before Skin CSS
+		 **/
+		'dependencies' => 'mw.EmbedPlayer',
 		'styles' => array(
 			'skins/kdark/css/layout.css',
 			'skins/kdark/css/icons.css',
