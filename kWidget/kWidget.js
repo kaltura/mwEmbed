a30df82572fa52c7ddb6937530ab61df4d100b34
MichalRadwantzor <michal.radwantzor@kaltura.com>
2013-10-23 12:14:58 +0300

    fix js error in "extend"

--- kWidget/kWidget.js
+++ kWidget/kWidget.js
@@ -1770,7 +1770,7 @@ var kWidget = {
 						if (source[prop].constructor === Object) {
 							if (!obj[prop] || obj[prop].constructor === Object) {
 								obj[prop] = obj[prop] || {};
-								extend(obj[prop], source[prop]);
+								this.extend(obj[prop], source[prop]);
 							} else {
 								obj[prop] = source[prop];
 							}
