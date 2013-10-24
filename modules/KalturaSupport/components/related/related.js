4e8df33bc4c6f522ec0c7d48bb0089f8ef5e9a51
ranyefet <ran@yefet.net>
2013-10-24 19:03:50 +0300

    Support for data-click and data-notification attributes within templates

--- modules/KalturaSupport/components/related/related.js
+++ modules/KalturaSupport/components/related/related.js
@@ -1,204 +1,238 @@
 ( function( mw, $ ) {"use strict";
 
-	mw.PluginManager.add( 'related', mw.KBaseComponent.extend({
+mw.PluginManager.add( 'related', mw.KBaseComponent.extend({
 
-		defaultConfig: {
-			parent: "topBarContainer",
-			order: 4,
-			align: "right",
-			//visible: false,
-			itemsLimit: 12,
-			displayOnPlaybackDone: true,
-			autoContinueTime: null,
-			templatePath: 'components/related/related.tmpl.html',
-			playlistId: null,
-		},
-		$screen: null,
-		setup: function(){
-			var _this = this;
+	defaultConfig: {
+		parent: "topBarContainer",
+		order: 4,
+		align: "right",
+		//visible: false,
+		itemsLimit: 12,
+		displayOnPlaybackDone: true,
+		autoContinueTime: null,
+		templatePath: 'components/related/related.tmpl.html',
+		playlistId: null,
+	},
+	$screen: null,
+	setup: function(){
+		var _this = this;
 
-			this.bind('playerReady', function(){
-				// Set remaining time to auto continue time
-				_this.setConfig('timeRemaining', _this.getConfig('autoContinueTime'));
+		this.bind('playerReady', function(){
+			// Stop timer
+			_this.stopTimer();
 
-				// Reset our items data
-				_this.templateData = null;
+			// Reset our items data
+			_this.templateData = null;
 
-				// Load items data
-				_this.getItemsData(function(){
-					if( _this.$screen ){
-						_this.$screen.remove();
-						_this.$screen = null;
-					}
-				});
-			});
-
-			this.bind('onplay', function(){
-				_this.hide();
-			});
-
-			if( this.getConfig('displayOnPlaybackDone') ){
-				this.bind('onEndedDone', function(){
-					_this.show();
-					if( _this.getConfig('autoContinueTime') ){
-						_this.setupTimer();
-					}
-				});				
-			}
-		},
-		setupTimer: function(){
-			var _this = this;
-			this.autoContinueInterval = setInterval(function(){
-				var ct = _this.getConfig('timeRemaining');
-				if( ct > 0 ){
-					_this.setConfig('timeRemaining', --ct);
-				} else {
-					clearInterval(_this.autoContinueInterval);
-					_this.changeMedia( _this.templateData.nextItem.id );
+			// Load items data
+			_this.getItemsData(function(){
+				if( _this.$screen ){
+					_this.$screen.remove();
+					_this.$screen = null;
 				}
-			}, 1000);
-		},
-		stopTimer: function(){
-
-		},
-		getItemsData: function( callback ){
-			if( !this.templateData ){
-				var _this = this;
-
-				// Allow other plugins to inject data
-				this.getPlayer().triggerQueueCallback( 'relatedData', function( args ){
-					// Get data from event
-					if( args ){
-						_this.templateData = args[0];
-						callback();
-						return ;
-					}
-					_this.getDataFromApi( callback );
-				});					
-				return;
-			}
-			callback();
-			return;
-		},
-		getDataFromApi: function( callback ){
-			var _this = this;
-			this.getKalturaClient().doRequest( {
-				'service' : 'playlist',
-				'action' : 'execute',
-				'id' : this.getConfig( 'playlistId' ),
-				'filter:objectType': 'KalturaMediaEntryFilterForPlaylist',
-				'filter:idNotIn': this.getPlayer().kentryid,
-				'filter:limit': this.getConfig('itemsLimit')
-			}, function( data ){
-				// Check if we got error
-				if( data.code && data.message ){
-					_this.log('Error getting related items: ' + data.message);
-					_this.getBtn().hide();
-				}
-				var nextItem = data.splice(0,1);
-				_this.templateData = {
-					nextItem: nextItem[0],
-					moreItems: data
-				};
-				callback();
 			});
-		},
-		getItems: function(){
-			return this.getTemplateHTML(this.templateData);
-		},
-		selectItem: function( $item ){
-			if( !$item.find('.title').is(':visible') ){
-				this.getScreen().find('.item').removeClass('hover');
-				$item.addClass('hover');
-				return false;
-			}
+		});
 
-			this.changeMedia( $item.data('entryid') );
-		},
-		changeMedia: function( entryId ){
-			var _this = this;
-			this.getPlayer().sendNotification('relatedVideoSelect', {entryId: entryId});
-			this.getPlayer().sendNotification('changeMedia', {entryId: entryId});
-			this.bind('onChangeMediaDone', function(){
-				_this.getPlayer().play();
-				_this.unbind('onChangeMediaDone');
-			})
-			this.hide();
-		},
-		hide: function(){
-			this.opened = false;
-			this.getScreen().hide();
-			if( this.wasPlaying ){
-				this.getPlayer().play();
-				this.wasPlaying = false;
-			}
-			this.getPlayer().restoreComponentsHover();
-		},
-		show: function(){
-			this.opened = true;
-			this.wasPlaying = this.getPlayer().isPlaying();
-			if( this.wasPlaying ) {
-				this.getPlayer().pause();
-			}
-			this.getPlayer().disableComponentsHover();
-			this.getScreen().show();
-		},
-		toggle: function(){
-			if( this.opened ){
-				this.hide();
-			} else {
-				this.show();
-			}
-		},
-		onConfigChange: function( property, value ){
-			if( property == 'timeRemaining' ){
-				var timeFormat = mw.KDPMapping.prototype.formatFunctions.timeFormat;
-				this.getScreen().find('.remaining').html(timeFormat(value));
-			}
-			this._super( property, value );
-		},
-		getScreen: function(){
-			if( ! this.$screen ){
-				var _this = this;
-				this.$screen = $('<div />')
-									.addClass( 'screen ' + this.pluginName )
-									.append( 
-										$('<div class="screen-content" /> ').append(
-											_this.getItems()
-										)
-									)
-									.hide();
+		this.bind('onplay', function(){
+			_this.hide();
+		});
 
-				var $items = this.$screen.find('.item').click(function(){
-					_this.selectItem( $(this) );
-				});
-
-				// Add hover events only if device support mouse events
-				if( mw.hasMouseEvents() ){
-					$items.hover(function(){
-						$( this ).addClass('hover');
-					},function(){
-						$( this ).removeClass('hover');
-					});
+		if( this.getConfig('displayOnPlaybackDone') ){
+			this.bind('onEndedDone', function(){
+				_this.show();
+				if( _this.getConfig('autoContinueTime') ){
+					_this.startTimer();
 				}
-
-				this.getPlayer().getVideoHolder().append( this.$screen );
-			}
-			return this.$screen;
-		},
-		getComponent: function() {
-			if( !this.$el ) {	
-				var _this = this;
-				this.$el = $( '<button />' )
-							.attr( 'title', 'Related' )
-							.addClass( "btn icon-related" + this.getCssClass() )
-							.click( function(){
-								_this.toggle();
-							});
-				
-			}
-			return this.$el;
+			});				
 		}
-	}));
+
+		this.bind('replayEvent', function(){
+			_this.stopTimer();
+		});
+	},
+	startTimer: function(){
+		var _this = this;
+		var updateTimeRemaining = function(){
+			var ct = _this.getConfig('timeRemaining');
+			if( ct > 0 ){
+				_this.setConfig('timeRemaining', --ct);
+				_this.timeRemainingMonitor = setTimeout(updateTimeRemaining, 1000);
+			} else {
+				_this.stopTimer();
+				_this.changeMedia( null, _this.templateData.nextItem.id );
+			}
+		};
+		setTimeout(updateTimeRemaining, 1000);
+	},
+	stopTimer: function(){
+		// Clear our interval
+		clearTimeout(this.timeRemainingMonitor);
+		this.timeRemainingMonitor = null;
+		// Set remaining time to auto continue time
+		this.setConfig('timeRemaining', this.getConfig('autoContinueTime'));
+	},
+	getItemsData: function( callback ){
+		if( !this.templateData ){
+			var _this = this;
+
+			// Allow other plugins to inject data
+			this.getPlayer().triggerQueueCallback( 'relatedData', function( args ){
+				// Get data from event
+				if( args ){
+					_this.templateData = args[0];
+					callback();
+					return ;
+				}
+				_this.getDataFromApi( callback );
+			});					
+			return;
+		}
+		callback();
+		return;
+	},
+	getDataFromApi: function( callback ){
+		var _this = this;
+		this.getKalturaClient().doRequest( {
+			'service' : 'playlist',
+			'action' : 'execute',
+			'id' : this.getConfig( 'playlistId' ),
+			'filter:objectType': 'KalturaMediaEntryFilterForPlaylist',
+			'filter:idNotIn': this.getPlayer().kentryid,
+			'filter:limit': this.getConfig('itemsLimit')
+		}, function( data ){
+			// Check if we got error
+			if( data.code && data.message ){
+				_this.log('Error getting related items: ' + data.message);
+				_this.getBtn().hide();
+			}
+			var nextItem = data.splice(0,1);
+			_this.templateData = {
+				nextItem: nextItem[0],
+				moreItems: data
+			};
+			callback();
+		});
+	},
+	getItems: function(){
+		return this.getTemplateHTML(this.templateData);
+	},
+	changeMedia: function( $el, entryId ){
+		this.stopTimer();
+		// Show title on first click
+		if( $el && $el.hasClass('item') && !$el.find('.title').is(':visible') ){
+			this.getScreen().find('.item').removeClass('hover');
+			$el.addClass('hover');
+			return false;
+		}
+
+		if( !entryId ){
+			entryId = $el.data('entryid');
+		}
+
+		var _this = this;
+		this.getPlayer().sendNotification('relatedVideoSelect', {entryId: entryId});
+		this.getPlayer().sendNotification('changeMedia', {entryId: entryId});
+		this.bind('onChangeMediaDone', function(){
+			_this.getPlayer().play();
+			_this.unbind('onChangeMediaDone');
+		})
+		this.hide();
+	},
+	hide: function(){
+		this.opened = false;
+		this.getScreen().hide();
+		if( this.wasPlaying ){
+			this.getPlayer().play();
+			this.wasPlaying = false;
+		}
+		this.getPlayer().restoreComponentsHover();
+	},
+	show: function(){
+		this.opened = true;
+		this.wasPlaying = this.getPlayer().isPlaying();
+		if( this.wasPlaying ) {
+			this.getPlayer().pause();
+		}
+		this.getPlayer().disableComponentsHover();
+		this.getScreen().show();
+	},
+	toggle: function(){
+		if( this.opened ){
+			this.hide();
+		} else {
+			this.show();
+		}
+	},
+	onConfigChange: function( property, value ){
+		if( property == 'timeRemaining' ){
+			var timeFormat = mw.KDPMapping.prototype.formatFunctions.timeFormat;
+			this.getScreen().find('.remaining').html(timeFormat(value));
+		}
+		this._super( property, value );
+	},
+	getScreen: function(){
+		if( ! this.$screen ){
+			var _this = this;
+			this.$screen = $('<div />')
+								.addClass( 'screen ' + this.pluginName )
+								.append( 
+									$('<div class="screen-content" /> ').append(
+										_this.getItems()
+									)
+								)
+								.hide();
+
+			// Should be moved into KBasePlugin
+			var $items = this.$screen.find('[data-click],[data-notification]').click(function(e){
+				return _this.handleClick( e, $(this) );
+			});
+
+			// Add hover events only if device support mouse events
+			if( mw.hasMouseEvents() ){
+				$items.hover(function(){
+					$( this ).addClass('hover');
+				},function(){
+					$( this ).removeClass('hover');
+				});
+			}
+
+			this.getPlayer().getVideoHolder().append( this.$screen );
+		}
+		return this.$screen;
+	},
+	handleClick: function( e, $el ){
+
+		// TODO: should be moved into KBasePlugin
+		e.preventDefault();
+
+		// Always stop timer
+		this.stopTimer();
+
+		var data = $el.data();
+		// Trigger local callback for data-click property
+		if( data.click && $.isFunction(this[data.click]) ){
+			this[data.click]( $el );
+		}
+
+		if( data.notification ){
+			this.getPlayer().sendNotification( data.notification );
+		}
+
+		return false;
+	},
+	getComponent: function() {
+		if( !this.$el ) {	
+			var _this = this;
+			this.$el = $( '<button />' )
+						.attr( 'title', 'Related' )
+						.addClass( "btn icon-related" + this.getCssClass() )
+						.click( function(){
+							_this.toggle();
+						});
+			
+		}
+		return this.$el;
+	}
+}));
 
 } )( window.mw, window.jQuery );
