44070ec8d2be062f3a92385852073ba3588b24d3
ranyefet <ran@yefet.net>
2013-11-03 19:18:50 +0200

    Share plugin now uses KBaseScreen

--- modules/KalturaSupport/components/share/share.js
+++ modules/KalturaSupport/components/share/share.js
@@ -1,23 +1,23 @@
 ( function( mw, $ ) {"use strict";
 
-mw.PluginManager.add( 'share', mw.KBaseComponent.extend({
+mw.PluginManager.add( 'share', mw.KBaseScreen.extend({
 
 	defaultConfig: {
 		parent: "topBarContainer",
 		order: 5,
 		align: "right",
-		socialShareEnabled: true,
+		tooltip: 'Share',
+		usePreviewPlayer: true,
+		previewPlayerEnabled: true,
+		socialShareEnabled: true,		
 		socialShareURL: 'smart', // 'parent' / 'http://custom.url/entry/{mediaProxy.entry.id}'
 		socialNetworks: 'facebook,twitter,googleplus',
 		shareOffset: true,
 		templatePath: 'components/share/share.tmpl.html'
 	},
-	$screen: null,
-	disablePreviewPlayer: false,
+	iconBtnClass: "icon-share",
 	setup: function(){
 		this.setupPlayerURL();
-		this.setNetworksData();
-		this.addBindings();
 	},
 	setupPlayerURL: function(){
 		var shareURL = null;
@@ -55,8 +55,8 @@ mw.PluginManager.add( 'share', mw.KBaseComponent.extend({
 		}
 		return shareURL;
 	},
-	setNetworksData: function(){
-		this.templateData = {
+	getTemplateData: function(){
+		return {
 			networks: [
 				{
 					id: 'facebook', 
@@ -79,118 +79,13 @@ mw.PluginManager.add( 'share', mw.KBaseComponent.extend({
 			]
 		};
 	},
-	addBindings: function(){
-		var _this = this;
-		this.bind('playerReady', function(){
-			_this.remove();
-		});
-		this.bind('onplay', function(){
-			if( _this.getScreen().is(':visible') ){
-				if( _this.hasPreviewPlayer() ){
-					_this.resizePlayer();
-				} else {
-					_this.hide();
-				}
-			}
-		});
-		this.bind('playerSizeChanged', function(e, size){
-			if( size == 'tiny' ){
-				_this.disablePreviewPlayer = true;
-			} else {
-				_this.disablePreviewPlayer = false;
-			}
-		});
-	},
-	resizePlayer: function(){
-		this.getPlayer().getInterface().addClass('previewPlayer');
-	},
-	restorePlayer: function(){
-		this.getPlayer().getInterface().removeClass('previewPlayer');
-	},
-	remove: function(){
-		if( this.$screen ){
-			this.$screen.remove();
-		}
-	},
-	hide: function(){
-		// Resume playing if we don't have preview player
-		if( this.wasPlaying && !this.hasPreviewPlayer() ) {
-			this.getPlayer().play();
-		}			
-		this.restorePlayer();
-		this.getPlayer().restoreComponentsHover();		
-		this.getScreen().fadeOut(400);
-	},
-	show: function(){
-		this.wasPlaying = this.getPlayer().isPlaying();
-		if( this.wasPlaying && !this.hasPreviewPlayer() ) {
-			this.getPlayer().pause();
-		}
-		if( this.hasPreviewPlayer() ){
-			this.resizePlayer();
-		}		
-		this.getPlayer().disableComponentsHover();
-		this.getScreen().fadeIn(400);
-	},
-	toggle: function(){
-		if( this.getScreen().is(':visible') ){
-			this.hide();
-		} else {
-			this.show();
-		}
-	},
 	openPopup: function( e ){
-		var url = $(e.target).attr('href');
+		var url = $(e.target).parents('a').attr('href');
 		window.open( 
 			url + encodeURIComponent( this.getConfig('socialShareURL')), 
 			'share-dialog', 
 			'width=626,height=436'
 		);
-	},
-	hasPreviewPlayer: function(){
-		return this.getScreen().find('.videoPreview').length && !this.disablePreviewPlayer;
-	},
-	getScreen: function(){
-		if( ! this.$screen ){
-			var _this = this;
-			this.$screen = $('<div />')
-								.addClass( 'screen ' + this.pluginName )
-								.append( 
-									$('<div class="screen-content" /> ').append(
-										this.getTemplateHTML(this.templateData)
-									)
-								);
-
-			// Create expand button
-			var $expandBtn = $( '<i />' )
-							.addClass( 'expandPlayerBtn icon-expand2' )
-							.click(function(){
-								_this.hide();
-							});
-
-			this.$screen.find('.popup').click(function(e){
-				e.preventDefault();
-				_this.openPopup(e);
-				return false;
-			});2
-
-			this.getPlayer().getVideoHolder().append( this.$screen );
-			this.getPlayer().getVideoDisplay().append( $expandBtn );
-		}
-		return this.$screen;
-	},
-	getComponent: function() {
-		if( !this.$el ) {	
-			var _this = this;
-			this.$el = $( '<button />' )
-						.attr( 'title', 'Share' )
-						.addClass( "btn icon-share" + this.getCssClass() )
-						.click( function(){
-							_this.toggle();
-						});
-			
-		}
-		return this.$el;
 	}
 }));
 
