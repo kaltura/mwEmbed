197f777d3c20ae887af8e75b8b8fac879b18bdab
ranyefet <ran@yefet.net>
2013-11-04 14:22:52 +0200

    Bumped version to v2.0.0.rc11

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,7 +12,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.0.0.rc10';
+$wgMwEmbedVersion = '2.0.0.rc11';
 
 
 // Default HTTP protocol from GET or SERVER parameters
