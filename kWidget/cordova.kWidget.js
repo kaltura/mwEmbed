780dff953114b1fa16967d7f09ea33f12d8b9e46
Eliza Sapir <eliza.sapir@kaltura.com>
2013-11-06 17:07:47 +0200

    supporting only one cordova.KWidget.js

--- kWidget/cordova.kWidget.js
+++ kWidget/cordova.kWidget.js
@@ -1,5 +1,9 @@
 /**
  * Created with JetBrains WebStorm.
+ * User: elizasapir
+ * Date: 9/10/13
+ * Time: 5:58 PM
+ * To change this template use File | Settings | File Templates.
  */
 
 /**
@@ -10,29 +14,31 @@
 		return ;
 	}
 	var init = function(){
-		var executeCordova;
 
-		cordova.define("cordova/plugin/NativeComponentPlugin",
-			function(require, exports, module) {
-				executeCordova = require("cordova/exec");
-				executeCordova( null, null, "NativeComponentPlugin", "cordovaInitialized", [] );
-			});
-		//This is mandatory for supporting cordova plugins
-		if (!window.plugins) {
-			window.plugins = {};
+		if ( kWidget.isAndroid() ){
+			var executeCordova;
+			cordova.define("cordova/plugin/NativeComponentPlugin",
+				function(require, exports, module) {
+					executeCordova = require("cordova/exec");
+					executeCordova( null, null, "NativeComponentPlugin", "cordovaInitialized", [] );
+				});
+			//This is mandatory for supporting cordova plugins
+			if (!window.plugins) {
+				window.plugins = {};
+			}
+			if (!window.plugins.NativeComponentPlugin) {
+				window.plugins.NativeComponentPlugin = cordova.require("cordova/plugin/NativeComponentPlugin");
+			}
 		}
-		if (!window.plugins.NativeComponentPlugin) {
-			window.plugins.NativeComponentPlugin = cordova.require("cordova/plugin/NativeComponentPlugin");
-		}
-
 		cordova.kWidget = {
-
 			// This element is populated by cordova
 			proxyElement: null,
 			// callbacks to auth object events go here:
 			embed : function( targetId, settings ){
 				this.target = document.getElementById( targetId );
-				this.iframeUrl = kWidget.getIframeUrl() + '?' + kWidget.getIframeRequest( targetId, settings );
+				this.target.style.cssText = "background-color:transparent;";
+				//kWidget.getIframeRequest( targetId, settings ) - we get it encoded so we decode before encoding whole url again
+				this.iframeUrl = kWidget.getIframeUrl() + '?' + decodeURIComponent(kWidget.getIframeRequest( targetId, settings ));
 				this.iframeUrl += '#' + JSON.stringify( window.preMwEmbedConfig );
 				this.addApi( this.target );
 				this.drawPlayer( this.target );
@@ -43,7 +49,6 @@
 					setTimeout( function() {
 						_this.drawPlayer( _this.target );
 					}, 250 );
-
 				});
 			},
 			addApi: function( target ){
@@ -56,29 +61,29 @@
 				if( args == undefined || !args ){
 					args = [];
 				}
-				executeCordova(null,null,"NativeComponentPlugin", command, args);
+				if ( kWidget.isAndroid() ){
+					cordova.exec = executeCordova;
+				}
+				cordova.exec(null,null,"NativeComponentPlugin", command, args);
 			},
 			evaluate:function(){
-				this.exec("evaluate", ['']);
+				this.exec( "evaluate", [ '' ] );
 			},
 			sendNotification:function( notificationName, notificationData ){
-				this.exec( "sendNotification", [ notificationName, JSON.stringify( notificationData ) ]);
+				this.exec( "sendNotification", [ notificationName, JSON.stringify( notificationData ) ] );
 			},
-			addJsListener:function(){
-
+			addJsListener: function(){
 			},
-			drawPlayer:function( target ){
+			drawPlayer: function( target ){
 				// get target size + position
 				var videoElementRect = target.getBoundingClientRect();
-
 				var x = videoElementRect.left;
-				var y = videoElementRect.top;
+				var y = videoElementRect.top + document.body.scrollTop;
 				var w = videoElementRect.right - videoElementRect.left;
 				var h = videoElementRect.bottom - videoElementRect.top;
-				this.exec( "drawVideoNativeComponent", [x, y, w, h] );
+				this.exec( "drawVideoNativeComponent", [ x, y, w, h ] );
 			}
 		};
 	}
-	document.addEventListener("deviceready", init, false);
-
+	document.addEventListener( "deviceready", init, false );
 })( window.kWidget );
