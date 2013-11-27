2c7c23801a976afd826a1f1e2df259e8de6cdce7
Michael Dale <mdale@wikimedia.org>
2013-11-27 08:22:10 -0600

    updated chroemecast description

--- modules/Chromecast/Chromecast.manifest.php
+++ modules/Chromecast/Chromecast.manifest.php
@@ -4,7 +4,8 @@
  */
 return array(
 	'chromecast' => array(
-		'description' => "The Chromecast extension enables sending your video to Chromecast device, where the API is available",
+		'description' => "The Chromecast extension enables sending your video to Chromecast device,
+			where the <a target=\"_new\" href=\"https://chrome.google.com/webstore/detail/google-cast/\">chromecast extension</a> is installed",
 		'attributes'=> array(
 				
 		)
