d05ebdd17098618b6692ea5460caf818f8aa08af
Michael Dale <mdale@wikimedia.org>
2013-12-02 07:38:48 -0600

    check that source[prop] is defined before checking constructor prop
    * This was throwing an error, in some of the player studio test api
    calls.
    * Also fixed some wp

--- kWidget/kWidget.js
+++ kWidget/kWidget.js
@@ -1761,24 +1761,24 @@ var kWidget = {
 	 },
 	 // similar to jQuery.extend 
 	 extend: function( obj ){
-		 var argSet	= Array.prototype.slice.call(arguments, 1);
-			for(var i=0;i< argSet.length;i++){
-				var source	= argSet[i];
-				if (source) {
-					for (var prop in source) {
-						if (source[prop].constructor === Object) {
-							if (!obj[prop] || obj[prop].constructor === Object) {
-								obj[prop] = obj[prop] || {};
-								this.extend(obj[prop], source[prop]);
-							} else {
-								obj[prop] = source[prop];
-							}
+		var argSet	= Array.prototype.slice.call(arguments, 1);
+		for(var i=0;i< argSet.length;i++){
+			var source	= argSet[i];
+			if (source) {
+				for (var prop in source) {
+					if (source[prop] && source[prop].constructor === Object) {
+						if (!obj[prop] || obj[prop].constructor === Object) {
+							obj[prop] = obj[prop] || {};
+							this.extend(obj[prop], source[prop]);
 						} else {
 							obj[prop] = source[prop];
 						}
+					} else {
+						obj[prop] = source[prop];
 					}
 				}
-			};
+			}
+		};
 		return obj;
 	},
 	// similar to parm
