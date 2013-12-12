30a30fc391a8cadb64ccfaebb7d1ac7b70d68975
Michael Dale <mdale@wikimedia.org>
2013-12-12 16:10:14 -0500

    socket push test ( not yet working )

--- modules/MediaSessionService/includes/BaseStreamService.php
+++ modules/MediaSessionService/includes/BaseStreamService.php
@@ -2,6 +2,8 @@
 require_once( dirname( __FILE__ ) . '/../../KalturaSupport/KalturaCommon.php' );
 
 class BaseStreamService {
+	// stores the socket connection for realtime events
+	var $socket = null;
 	
 	function __construct(){
 		global $container;
@@ -12,6 +14,44 @@ class BaseStreamService {
 	}
 	function setStreamUrl( $url ){
 		$this->streamUrl = $url;
+	}
+	/* for now this is mostly for debugging */
+	function sendSocketMessage( $message ){
+		echo 'sendSocketMessage' ."\n";
+		// This is our new stuff
+		$address = "127.0.0.1";
+ 		$service_port = '8080';
+
+		error_reporting(E_ALL);
+		
+		/* Create a TCP/IP socket. */
+		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		if ($socket === false) {
+			echo "socket_create() failed: reason: " . 
+			socket_strerror(socket_last_error()) . "\n";
+		}
+		
+		echo "Attempting to connect to '$address' on port '$service_port'...";
+		$result = socket_connect($socket, $address, $service_port);
+		if ($result === false) {
+		   echo "socket_connect() failed.\nReason: ($result) " . 
+			  socket_strerror(socket_last_error($socket)) . "\n";
+		}
+		
+		$in = "HEAD / HTTP/1.1\r\n";
+		$in .= "Host: www.google.com\r\n";
+		$in .= "Connection: Close\r\n\r\n";
+		$out = '';
+		
+		echo "Sending HTTP HEAD request...";
+		if(!socket_write($socket, $in, strlen($in)))
+		{
+			$errorcode = socket_last_error();
+			$errormsg = socket_strerror($errorcode);
+		
+			die("Could not send message on socket : [$errorcode] $errormsg \n");
+		}
+		echo "OK.\n";
 	}
 	function getStreamHandler(){
 		// Grab and parse the base content m3u8
