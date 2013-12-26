4c9bd392af503264335db7147dec8bf5aa0329d2
amirch1 <amir.chervinsky@kaltura.com>
2013-12-26 12:51:47 +0200

    update version to rc8

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,7 +12,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.1.rc7';
+$wgMwEmbedVersion = '2.1.rc8';
 
 
 // Default HTTP protocol from GET or SERVER parameters
