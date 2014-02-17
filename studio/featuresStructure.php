505ff093601f3d40be1aba0f1ae0c50240fa6ddf
amirch1 <amir.chervinsky@kaltura.com>
2014-02-17 13:40:36 +0200

    removed chapterView for now

--- studio/featuresStructure.php
+++ studio/featuresStructure.php
@@ -125,7 +125,7 @@ return array(
 		"type" => "menu",
 		"model" => "plugins",
 		"children"=> array(
-			"chaptersView" => "",
+			/*"chaptersView" => "",*/
 			"playbackRateSelector" => "",
 			"restrictUserAgent" => ""
 		)
