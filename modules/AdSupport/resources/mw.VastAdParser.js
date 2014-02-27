1bde47112998462ed193c31e8dba5472c2e70187
MichalRadwantzor <michal.radwantzor@kaltura.com>
2014-02-27 14:31:09 +0200

    #FEC-1059 #support vast 2.0.1 video clicks events

--- modules/AdSupport/resources/mw.VastAdParser.js
+++ modules/AdSupport/resources/mw.VastAdParser.js
@@ -16,12 +16,16 @@ mw.VastAdParser = {
 		var adConf = {};
 		var $vast = $( xmlObject );
 
-		// Check for Vast Wrapper response
-		if( $vast.find('Wrapper').length && $vast.find('VASTAdTagURI').length) {
-			var adUrl = $vast.find('VASTAdTagURI').text();
+		var addVideoClicksIfExist = function() {
 			if ( $vast.find('VideoClicks ClickTracking').length > 0 )  {
 				_this.videoClickTrackingUrl =  $vast.find('VideoClicks ClickTracking').text();
 			}
+		}
+
+		// Check for Vast Wrapper response
+		if( $vast.find('Wrapper').length && $vast.find('VASTAdTagURI').length) {
+			var adUrl = $vast.find('VASTAdTagURI').text();
+			addVideoClicksIfExist();
 			mw.log('VastAdParser:: Found vast wrapper, load ad: ' + adUrl);
 			mw.AdLoader.load( adUrl, callback, true );
 			return ;
@@ -178,8 +182,9 @@ mw.VastAdParser = {
 				curIcon.viewTracking = _this.getURLFromNode ( $( icon ).find('IconViewTracking') );
 				curIcon.html = $('<div />').html( curIcon.$html ).html();
 				currentAd.icons.push(curIcon);
-				
+
 			});
+			addVideoClicksIfExist();
 			if (( currentAd.videoFiles && currentAd.videoFiles.length > 0 ) || currentAd.vpaid) {
 				adConf.ads.push( currentAd );
 			}
