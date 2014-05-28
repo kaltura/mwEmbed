6175dcba2f784853cdf998b91dbff819bb6b3e31
Michael Dale <mdale@wikimedia.org>
2014-05-28 14:54:38 +0300

    Removed client example

--- docs/marketing_customersamples.php
+++ docs/marketing_customersamples.php
@@ -7,10 +7,6 @@
 <p><a href="http://www.blogher.com/video"><img src="images/blogher.jpg" alt="" title="Blogher Popup Player" width="540" height="287" class="alignnone size-large wp-image-281" /></a>
 </div>
 <div class="blurb-full">
-<h2>ABC News</h2>
-<p><a href="http://abcnews.go.com/video"><img src="images/abc.news.jpg" alt="" title="ABC News Player" width="540" height="338" class="alignnone size-large wp-image-284" /></a>
-</div>
-<div class="blurb-full">
 <h2>TMZ</h2>
 <p><a href="http://tmz.com/videos/"><img src="images/tmz.jpg" alt="" title="TMZ Playlist Player" width="540" height="406" class="alignnone size-large wp-image-293" /></a>
 </div>
