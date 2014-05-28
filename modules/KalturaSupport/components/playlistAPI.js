dd55a336c3a52d42e2b37eef90a4a0d07ccf3dda
amirch1 <amir.chervinsky@kaltura.com>
2014-05-28 10:03:43 +0300

    refactored playlist

--- modules/KalturaSupport/components/playlistAPI.js
+++ modules/KalturaSupport/components/playlistAPI.js
@@ -26,21 +26,6 @@
 			// add bindings
 		},
 		loadPlaylists: function(){
-			// find all playlists that were defined in the config
-			for (var playlistIndex=0; playlistIndex < 1000; playlistIndex++){
-				if (this.getConfig('kpl' + playlistIndex +'Id')){
-					this.loadPlaylistByID(this.getConfig('kpl' + playlistIndex +'Id'));
-				}else if (this.getConfig('kpl' + playlistIndex +'Url')){
-					this.loadPlaylistByRss(this.getConfig('kpl' + playlistIndex +'Url'));
-				}else{
-					break;
-				}
-			}
-			if (this.playlistSet.length > 0){
-				console.log("----------- render playlist")
-			};
-		},
-		loadPlaylistByID: function(playlistID){
 			var embedPlayer = this.embedPlayer;
 			// Populate playlist set with kalturaPlaylistData
 			for (var playlistId in embedPlayer.kalturaPlaylistData ) {
@@ -48,10 +33,12 @@
 					this.playlistSet.push( embedPlayer.kalturaPlaylistData[ playlistId ] );
 				}
 			}
-		},
-		loadPlaylistByRss: function(playlistRss){
-			console.log ("load by rss (currently not supported): "+playlistRss);
+
+			if (this.playlistSet.length > 0){
+				console.log("----------- render playlist")
+			};
 		}
+
 
 	})
 
