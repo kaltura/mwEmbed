c8ec6c0b4b89f7c003ea24984efe67788ef4ffc9
amirch1 <amir.chervinsky@kaltura.com>
2014-06-08 16:17:07 +0300

    updated documentation

--- docs/content_api.php
+++ docs/content_api.php
@@ -56,6 +56,66 @@
 					'docPath' => 'thumb'
 				)
 			)
+		),
+		'sendNotification' => array(
+			'desc'=>'Call a KDP notification (perform actions using this API, for example: play, pause, changeMedia, etc.)',
+			'params' => array(
+				'notificationName' => array(
+					'type' => 'String',
+					'desc' => 'The name of notification to call',
+				),
+				'notificationData'=> array(
+					'type' => 'Object',
+					'optional' => true,
+					'desc' => 'The custom data to pass with the notification.'
+				)
+			)
+		),
+		'addJsListener' => array(
+			'desc'=>'Register a javascript handler function for a KDP notification',
+			'params' => array(
+				'listenerString' => array(
+					'type' => 'String',
+					'desc' => 'The name of the notification to listen to',
+				),
+				'jsFunctionName'=> array(
+					'type' => 'String',
+					'desc' => 'The name of the JavaScript handler function'
+				)
+			)
+		),
+		'evaluate' => array(
+			'desc'=>"Retrieve the value of a KDP model property or component's property using the standard OOP dot notation inside curly brackets",
+			'params' => array(
+				'object.property.properties' => array(
+					'type' => 'String',
+					'desc' => 'the reference to the component object with data that you want to extract. Enclose the reference in curly braces within single or double quotation marks.',
+				)
+			)
+		),
+		'setKDPAttribute' => array(
+			'desc'=>"Change a value of a KDP model property or component's property using the standard OOP dot notation.",
+			'params' => array(
+				'object' => array(
+					'type' => 'String',
+					'desc' => 'A string that represents the object you want to modify. Use standard dot notation to specify sub-objects, for example, configProxy.flashvars'
+				),'property' => array(
+					'type' => 'String',
+					'desc' => 'The player property that you want to modify.'
+				),'value' => array(
+					'type' => 'String',
+					'desc' => 'The new value that you want to set for the player property.'
+				)
+			)
+		),
+		'jsCallbackReady' => array(
+			'desc'=>"A javascript function on the hosting web page that will be called by KDP when setup of externalInterface APIs is completed.",
+			'params' => array(
+				'objectId' => array(
+					'type' => 'String',
+					'desc' => 'Represents the identifier of the player that is embedded.'
+				)
+			)
 		)
 	);
 	$objectDefinitions = array(
@@ -203,6 +263,60 @@
         )
     );
 
+	$methods = array(
+        'doPause' => array(
+            'body' => 'None',
+            'desc' => 'Command the player to pause.'
+        ),
+        'doPlay' => array(
+            'body' => 'None',
+            'desc' => 'Command the player to play.'
+        ),
+        'doStop' => array(
+            'body' => 'None',
+            'desc' => 'Do stop command to the player. Pause and move the playhead to 0.'
+        ),
+        'doSeek' => array(
+            'body' => 'Position to seek to',
+            'desc' => 'Do seek command to the player.'
+        ),
+        'doSwitch' => array(
+            'body' => 'New stream bitrate',
+            'desc' => 'Do switch command for manual switching between streams within the resource.'
+        ),
+        'cleanMedia' => array(
+            'body' => 'None',
+            'desc' => 'cleans the media from the player.'
+        )
+    );
+
+	$listeners = array(
+        'startUp' => array(
+            'body' => 'Root of the application',
+            'desc' => 'The first command that register the main proxys and main view mediator.'
+        ),
+        'initiatApp' => array(
+            'body' => 'None',
+            'desc' => 'Start the init macro commands.'
+        ),
+        'skinLoaded' => array(
+            'body' => 'None',
+            'desc' => 'Dispatched when the skin is loaded.'
+        ),
+        'skinLoadFailed' => array(
+            'body' => 'None',
+            'desc' => 'Dispatched when skin load failed.'
+        ),
+        'sourceReady' => array(
+            'body' => 'None',
+            'desc' => 'When the source is ready we can set the media element to the media player.'
+        ),
+        'kdpReady' => array(
+            'body' => 'None',
+            'desc' => 'Notify that the application is ready to be used and events can be listened to and that the loaded entry is ready to be played.'
+        )
+    );
+
     function getTableContent($headers, $param){
         $paramArrayObject = new ArrayObject($param);
         $paramArrayObject->ksort();
@@ -297,52 +411,100 @@ Once embed the following API is available:
 <p>To simplify the management of many of the player features, Kaltura has implemented the “UIVars” to override and configure player features.</p>
 <p>Kaltura UIVars are an incredibly powerful feature of the Kaltura Players which allow publishers to pre-set or override the value of any FlashVar (object level parameters), show, hide and disable existing UI element, add new plugins and UI elements to an existing player, and modify attributes of all the player's elements.</p>
 <p>FlashVars are configuration variables that are set to the Kaltura Player in the HTML embed code and work for “regular” static embed, server-generated embed or JavaScript-generated embed code.  Below is a list of all the Kaltura Player FlashVars.</p>
+<br><br>
 <div class="docblock">
-
-<?php echo getTableContent( array( 'Ui Var', 'Type', 'Description' ), $uiVars ) ?>
-
+	<?php echo getTableContent( array( 'Ui Var', 'Type', 'Description' ), $uiVars ) ?>
 </div><br><br>
+Code sample:<br>
+<pre class="prettyprint linenums">
+kWidget.embed({
+  "targetId": "kaltura_player_1402219661",
+  "wid": "_1645161",
+  "uiconf_id": 24231962,
+  "flashvars": {
+		'autoMute': true,
+		'autoPlay': false,
+		'adsOnReplay': true,
+		'imageDefaultDuration': 5,
+		'mediaProxy.preferedFlavorBR': 1400
+  },
+  "cache_st": 1402219661,
+  "entry_id": "1_a3njcsia"
+});
+</pre>
 
+<br><br>
+<a name="kdpAPI"></a>
 
-<!--br><br><br>
-Notifications: Notifications are both actions and events; (e.g. play and pause). To know when a notification was dispatched add a listener to the notification name using the addJsListener (e.g. addJsListener('play');) and to make Kdp perform an action dispatch the notification using sendNotification (e.g. sendNotification('play')); For a list of notifications see the Notifications leaf in this tree
-Attributes: At run-time any public KDP parameter or object properties in Kdp can be retrieved or changed via JavaScript; use evaluate method to get the values of various attributes or object properties and use the setKDPAttribute method to change it's value
-<h4>Legacy KDP API</h4>
- jsCallbackReady
-A JavaScript function on the hosting web page that will be called by KDP when setup of externalInterface APIs is completed. When setting sourceType=entryId, it is advised to use kdpReady instead of this event. Value should be the name of a function to be used as the handler in the hosting page
-Default: 
-undefined
- addJsListener(listenerString:String, jsFunctionName:String)
-Register a javascript handler function for a KDP notification
-Parameters: 
-listenerString: The name of the notification to listen to
-jsFunctionName: The name of the JavaScript handler function
- removeJsListener(listenerString:String, jsFunctionName:String)
-Remove a listener to KDP notofication
-Parameters: 
-listenerString: The name of the notification to listen to
-jsFunctionName: The name of the JavaScript handler function
- sendNotification(notificationName:String, notificationData:Object)
-Call a KDP notification (perform actions using this API, for example: play, pause, changeMedia, etc.)
-Parameters: 
-notificationName: The name of notification to call
-notificationData: the custom data to pass with the notification
-<h3>Native SDK API</h3>
+<h2>Player API</h2>
+<p>The JavaScript API is a two-way communication channel that lets the player tell you what it is doing and lets you tell the player to do things.
+<br>For more information: <a href="http://knowledge.kaltura.com/javascript-api-kaltura-media-players#UnderstandingtheJavaScriptAPIWorkflow" target="_blank">JavaScript API for Kaltura Media Players</a></p>
 
-<h3>Evaluatble Properties</h3>
+<h3>Receiving Notification that the Player API Is Ready </h3>
+<p>Before you can use the JavaScript API's base methods, the player has to reach the point in its internal loading sequence when it is ready to interact with your code. The player lets you know that it is ready by calling the <b>jsCallbackReady</b> JavaScript function on the page.</p>
+<p>jsCallbackReady is the player's first callback. The player passes jsCallbackReady an objectId parameter that represents the identifier of the player that is embedded on the page.</p>
+<?php echo getDocs( array( 'jsCallbackReady' ) ) ?>
+<br><br>Code sample:<br>
+<pre class="prettyprint linenums">
+function jsCallbackReady(objectId) {
+    window.kdp = document.getElementById(objectId);
+}
+</pre>
+<p>Kaltura recommends that you place jsCallbackReady in the global scope. This allows easily finding this critical function in the JavaScript code.</p><br><br>
 
-<h3>On Page / App, Public Notifications</h3>
-This is the public api
-<h4>Player Buildout</h4>
-<h4>Player States</h4>
-<h4>Playback Actions</h4>
-<h4>Add related</h4>
+<h3>Calling a player method from JavaScript</h3>
+<p>Use the <b>sendNotification</b> method to create custom notifications that tell the player to do something, such as play, seek, or pause.</p>
+<?php echo getDocs( array( 'sendNotification' ) ) ?>
+<br><br><p>Available Notifications:</p>
+<?php echo getTableContent( array( 'Notification', 'Body', 'Description' ), $methods ) ?>
+<br><br>Code sample:<br>
+<pre class="prettyprint linenums">
+//TBD
+</pre>
 
-<h3>Iframe in player plugins, Public Events</h3>
-This API should be used when building player plugins that run inside the iframe
-<h4>Player Buildout</h4>
-<h4>Player States</h4>
-<h4>Playback Actions</h4>
-<h4>Add related</h4>
+<h3>Registering to a player event</h3>
+<p>Use the <b>addJsListener</b> method to listen for a specific notification that something happened in the player, such as the video is playing or is paused.</p>
+<?php echo getDocs( array( 'addJsListener' ) ) ?>
+<br><br><p>Available Listeners:</p>
+<?php echo getTableContent( array( 'Notification', 'Body', 'Description' ), $listeners ) ?>
+<br><br>Code sample:<br>
+<pre class="prettyprint linenums">
+kdp.addJsListener(“playerUpdatePlayhead”, “playerUpdatePlayheadHandler”)
+function playerUpdatePlayheadHandler(data, id) {
+    // data = the player's progress time in seconds
+    // id = the ID of the player that fired the notification
+}
+</pre>
 
-<h3>All plugins:</h3-->
+<h3>Un-registering a player event</h3>
+<p>Use the <b>removeJsListener</b> method to remove a listener that is no longer needed.</p>
+<h5>Why Remove a JsListener?</h5>
+KDP3 accumulates JsListeners. If you add a JsListener for a notification and then add another JsListener for the same notification, the new JsListener does not override the previous one. Both JsListeners are executed in the order in which they are added. To prevent unexpected behavior in your application, Kaltura recommends that you remove unnecessary JsListeners.
+When you remove a listener, you must specify the associated function name.
+<br><br>Code sample:<br>
+<pre class="prettyprint linenums">
+removeJsListener("event", "functionName")
+</pre>
+
+<h3>Retrieving a player property</h3>
+<p>Use the <b>evaluate</b> method to find out something about a player by extracting data from player components.</p>
+<?php echo getDocs( array( 'evaluate' ) ) ?>
+<br><br><p>Available Properties:</p>
+
+<br><br>Code sample:<br>
+<pre class="prettyprint linenums">
+function getName() {
+var entry_name = kdp.evaluate('{mediaProxy.entry.name}');
+    alert('Entry name: '+entry_name);
+}
+</pre>
+
+<h3>Setting a player attribute</h3>
+<p>Use the <b>setKDPAttribute</b> method to change something about a player by setting player attribute values.</p>
+<?php echo getDocs( array( 'setKDPAttribute' ) ) ?>
+<br><br><p>Available Attributes:</p>
+
+<br><br>Code sample:<br>
+<pre class="prettyprint linenums">
+kdp.setKDPAttribute("configProxy.flashvars","autoPlay","true")
+</pre>
