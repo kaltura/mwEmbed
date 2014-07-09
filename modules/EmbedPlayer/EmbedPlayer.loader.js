4756a78adf9dbf4c9a346ddcfe58d6f005d61644
paladox2015 <paladox2015@users.noreply.github.com>
2014-07-09 14:59:19 +0100

    Update EmbedPlayer.loader.js

--- modules/EmbedPlayer/EmbedPlayer.loader.js
+++ modules/EmbedPlayer/EmbedPlayer.loader.js
@@ -10,9 +10,9 @@
 	*/
 	$( mw ).bind( 'SetupInterface', function( event, callback ){
 		// Check if we have tags to rewrite:
-		if( $( mw.getConfig( 'EmbedPlayer.RewriteSelector' )  ).length ) {
+		if( $( mw.config.get( 'EmbedPlayer.RewriteSelector' )  ).length ) {
 			// Rewrite the embedPlayer EmbedPlayer.RewriteSelector and run callback once ready:
-			$( mw.getConfig( 'EmbedPlayer.RewriteSelector' ) )
+			$( mw.config.get( 'EmbedPlayer.RewriteSelector' ) )
 				.embedPlayer( callback );
 		} else {
 			callback();
