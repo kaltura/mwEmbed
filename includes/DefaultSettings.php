7e836452d3b97ccbb2316f3e649c1bb025935c3d
amirch1 <amir.chervinsky@kaltura.com>
2014-09-10 08:40:18 +0300

    updated version

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,6 +12,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
+
 $wgMwEmbedVersion = '2.18.1';
 
 // Default HTTP protocol from GET or SERVER parameters
