0f6d6208557ced65c66b04901d9bfc4b318f288d
MichalRadwantzor <michal.radwantzor@kaltura.com>
2014-09-11 15:11:52 +0300

    #FEC-1691 #commet support nativeCallout in Android

--- modules/KalturaSupport/components/nativeCallout.js
+++ modules/KalturaSupport/components/nativeCallout.js
@@ -2,13 +2,26 @@
 
 	mw.PluginManager.add( 'nativeCallout', mw.KBasePlugin.extend({
 		defaultConfig: {
-			"storeUrl": "http://itunes.apple.com/app/id698657294",
-			"mimeName": "kalturaPlayerToolkit://",
+			"storeUrl": null,
+			"mimeName": null,
 			"iframeUrl": null
 		},
+
+		IOS_STORE_URL: "http://itunes.apple.com/app/id698657294",
+		ANDROID_STORE_URL: "market://details?=com.kaltura.kalturaplayertoolkit",
+		IOS_MIME_NAME: "kalturaPlayerToolkit://",
+		ANDROID_MIME_NAME: "http://kalturaplayertoolkit.com",
+
 		setup: function(){
 			// Bind player
 			this.addBindings();
+			if( !this.getConfig( "storeUrl" ) ) {
+				this.setConfig( "storeUrl", mw.isAndroid() ? this.ANDROID_STORE_URL : this.IOS_STORE_URL );
+			}
+
+			if( !this.getConfig( "mimeName" ) ) {
+				this.setConfig( "mimeName", mw.isAndroid() ? this.ANDROID_MIME_NAME : this.IOS_MIME_NAME );
+			}
 
 			if( !this.getConfig( "iframeUrl" ) ) {
 				var chromecastPluginFlashvar = "&flashvars[chromecast.plugin]=true";
@@ -16,7 +29,7 @@
 			}
 		},
 		isSafeEnviornment: function(){
-			return mw.isIOS() === true;
+			return mw.isMobileDevice() === true;
 		},
 		addBindings: function() {
 			var _this = this;
@@ -32,23 +45,28 @@
 
 		// New "doPlay" implementation when nativeCallout plugin exist on mobile devices
 		calloutNativePlayer: function() {
-
 			var _this = this;
 			var timeout;
-			$('<iframe />')
-				.attr('src', _this.getConfig( "mimeName" ) + "?iframeUrl:=" + _this.getConfig( "iframeUrl" ))
-				.attr('style', 'display:none;')
-				.appendTo('body');
-
-			timeout = setTimeout(function() {
-				document.location = _this.getConfig( "storeUrl" );
-			}, 500);
-			window.addEventListener( 'pagehide', preventPopup );
 
 			function preventPopup() {
 				clearTimeout(timeout);
 				timeout = null;
 				window.removeEventListener( 'pagehide', preventPopup );
+			}
+
+			var url =  _this.getConfig( "mimeName" ) + "?iframeUrl:=" + _this.getConfig( "iframeUrl" );
+			if ( mw.isAndroid() ) {
+				window.open( url, "_system" );
+			} else {
+				$('<iframe />')
+					.attr('src', url)
+					.attr('style', 'display:none;')
+					.appendTo('body');
+
+				timeout = setTimeout(function() {
+					document.location = _this.getConfig( "storeUrl" );
+				}, 500);
+				window.addEventListener( 'pagehide', preventPopup );
 			}
 		}
 	}));
