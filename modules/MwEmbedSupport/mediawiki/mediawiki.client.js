67351cd9c553a8351a264ae8cde2853d8439dfad
Itay Kinnrot <itay.kinnrot@kaltura.com>
2014-09-18 15:52:30 +0300

    fix conflict

--- modules/MwEmbedSupport/mediawiki/mediawiki.client.js
+++ modules/MwEmbedSupport/mediawiki/mediawiki.client.js
@@ -108,6 +108,10 @@
 		return /OS 8_/.test( userAgent ) && mw.isIOS();
 	};
 
+	mw.isIOS80 = function(){
+		return /Version\/8/.test( navigator.userAgent ) && mw.isIOS();
+	};
+
 	// Does the client has native touch bindings?
 	mw.hasNativeTouchBindings = function(){
 		return (mw.isAndroid41() || mw.isAndroid42() || ( mw.isAndroid() && mw.isFirefox() ));
