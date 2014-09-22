3a6033aa3a19b897426b39d33b79b5b14dc72682
amirch1 <amir.chervinsky@kaltura.com>
2014-09-22 11:47:19 +0300

    #FEC-1901 #comment fix fullscreen on vertical playlist

--- modules/KalturaSupport/resources/mw.KBaseMediaList.js
+++ modules/KalturaSupport/resources/mw.KBaseMediaList.js
@@ -47,12 +47,22 @@
 				if (_this.getConfig( 'parent')){
 					_this.renderMediaList();
 					_this.setSelectedMedia(_this.selectedMediaItemIndex);
+				}else if ( _this.getLayout() === "vertical"){
+					$(".videoHolder").width("100%");
+					if (_this.getConfig("containerPosition") === "left"){
+						$(".mwPlayerContainer").css("margin-left", 0 + "px");
+					}
 				}
 			});
 			// handle fullscreen entering resize
 			$( this.embedPlayer ).bind('onOpenFullScreen', function() {
 				if ( !_this.getConfig( 'parent') ){
 					$(".medialistContainer").hide();
+				}else if ( _this.getLayout() === "vertical"){
+					$(".videoHolder").width(_this.videoWidth+"px");
+					if (_this.getConfig("containerPosition") === "left"){
+						$(".mwPlayerContainer").css("margin-left", _this.getMedialistComponent().width() + "px");
+					}
 				}
 			});
 
