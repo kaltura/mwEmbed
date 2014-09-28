76d9e04298d6a077512fb02ac24e5fa6f4c74d7f
amirch1 <amir.chervinsky@kaltura.com>
2014-09-28 10:26:55 +0300

    added playlist events to documentation

--- docs/api_listeners.php
+++ docs/api_listeners.php
@@ -308,4 +308,31 @@
 			'availability' => 'kdp'
 		)
 	);
+
+	$playlists = array(
+        'playlistReady' => array(
+            'props' => 'None',
+            'desc' => 'The playlist layout is ready.'
+        ),
+        'playlistPlayNext' => array(
+            'props' => 'None',
+            'desc' => 'The next clip was requested.'
+        ),
+        'playlistPlayPrevious' => array(
+            'props' => 'None',
+            'desc' => 'The previous clip was requested.'
+        ),
+        'playlistFirstEntry' => array(
+            'props' => 'None',
+            'desc' => 'The first clip in the playlist was loaded.'
+        ),
+        'playlistMiddleEntry' => array(
+            'props' => 'None',
+            'desc' => 'A clip that is not the first or the last clip in the playlist was loaded.'
+        ),
+        'playlistLastEntry' => array(
+            'props' => 'None',
+            'desc' => 'The last clip in the playlist was loaded.'
+        )
+    );
 ?>
