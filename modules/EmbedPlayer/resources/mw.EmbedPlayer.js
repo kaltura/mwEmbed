bd22bf89d78109f7395fe6cc92e06f92f9d958dc
Ran Yefet <ran.yefet@kaltura.com>
2014-09-30 15:32:36 +0300

    Remove deprecated method

--- modules/EmbedPlayer/resources/mw.EmbedPlayer.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayer.js
@@ -2938,10 +2938,6 @@
 			}
 		},
 
-		switchAudioTrack: function ( trackIndex ) {
-			mw.log('Error player does not multiple audio tracks' );
-		},
-
 		bufferStart: function() {
 			if ( !this.isInSequence() && !this.buffering ) {
 				var _this = this;
