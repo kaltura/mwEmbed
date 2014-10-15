cdcc6d3bb3d40262236f4640efaa773fca1806ca
MichalRadwantzor <michal.radwantzor@kaltura.com>
2014-10-15 10:28:26 +0300

    #FEC-2132 #comment fix hover controls behaviour on touch devices

--- modules/DoubleClick/resources/mw.DoubleClick.js
+++ modules/DoubleClick/resources/mw.DoubleClick.js
@@ -333,7 +333,7 @@
 		},
 		getAdContainer: function(){
 			if( !$('#' + this.getAdContainerId() ).length ){
-				this.embedPlayer.getInterface().append(
+				this.embedPlayer.getVideoHolder().after(
 					$('<div />')
 						.attr( 'id',  this.getAdContainerId() )
 						.css({
