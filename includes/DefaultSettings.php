4f30a8d51f0f4bda64816275a5a04ad0fc7390ca
amirch1 <amir.chervinsky@kaltura.com>
2014-10-26 14:06:06 +0200

    bump v2.20.rc12

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,7 +12,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.20.rc11';
+$wgMwEmbedVersion = '2.20.rc12';
 
 
 // Default HTTP protocol from GET or SERVER parameters
