4fce36485994ef1459f42a6634a981160dce8163
MichalRadwantzor <michal.radwantzor@kaltura.com>
2014-11-02 18:24:28 +0200

    fix content duration after allAdsCompleted

--- modules/DoubleClick/resources/mw.DoubleClick.js
+++ modules/DoubleClick/resources/mw.DoubleClick.js
@@ -1107,6 +1107,7 @@
 			} else { // do a manual restore:
 				// restore player with normal events:
 				this.embedPlayer.adTimeline.restorePlayer( null, adPlayed);
+				this.embedPlayer.setDuration(this.embedPlayer.duration);
 				// managed complete ... call clip done if content complete.
 				if( onContentComplete ){
 					if (_this.postRollCallback){
@@ -1120,7 +1121,7 @@
 						_this.embedPlayer.setCurrentTime(_this.timeToReturn);
 						_this.timeToReturn = null;
 					}
-					this.embedPlayer.setDuration(this.embedPlayer.duration);
+
 					this.embedPlayer.play();
 				}
 			}
