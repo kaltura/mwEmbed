4cf44228770954b1eef2367772e4a62c72995479
amirch1 <amir.chervinsky@kaltura.com>
2014-11-12 13:38:00 +0200

    #FEC-2025 #time 2h #comment use regular expression instead of trim as trim is not supported by IE8

--- modules/TimedText/resources/mw.TextSource.js
+++ modules/TimedText/resources/mw.TextSource.js
@@ -394,7 +394,7 @@
 			for (var i = 0; i < caplist.length; i++) {
 		 		var captionText = "";
 				var caption = false;
-				captionText = caplist[i].trim();
+				captionText = caplist[i].replace(/^\s+|\s+$/g, '');
 				var s = captionText.split(/\n/);
 				if (s.length < 2) {
 					// file format error or comment lines
