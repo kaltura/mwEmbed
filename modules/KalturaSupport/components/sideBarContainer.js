59638a7daa2ebdf342d8dbda084bc058bce993f6
orenme <oren.melamed@kaltura.com>
2014-11-25 15:22:05 +0200

    FEC-1990 #comment Sidebar responsive layout
    Show/Hide sidebar when height/width or fullscreen dimensions are met.

--- modules/KalturaSupport/components/sideBarContainer.js
+++ modules/KalturaSupport/components/sideBarContainer.js
@@ -5,7 +5,6 @@
 		defaultConfig: {
 			'hover': true,
 			'clickToClose': false,
-			'position': 'left'
 			'closeTimeout': 1000,
 			'position': 'left',
 			'fullScreenDisplayOnly': false,
@@ -13,7 +12,10 @@
 			'minDisplayHeight': 0
 		},
 		enabled: true,
+		render: true,
+		screenShown: false,
 		keepOnScreen: false,
+		openAfterDisable: false,
 
 		setup: function(){
 			// Bind player
@@ -40,6 +42,31 @@
 					} );
 				}
 			});
+			this.bind( 'preShowScreen onDisableInterfaceComponents', function( event, excludedComponents ){
+				if( event.type == "preShowScreen" || $.inArray( _this.pluginName, excludedComponents ) == -1) {
+					_this.enabled = false;
+					_this.hide();
+				}
+			});
+			this.bind( 'preHideScreen onEnableInterfaceComponents', function( event, excludedComponents ){
+				if( event.type == "preHideScreen" || $.inArray( _this.pluginName, excludedComponents ) == -1) {
+					_this.enabled = true;
+					_this.show();
+				}
+			});
+			this.bind('updateLayout', function(){
+				if (_this.getPlayer().layoutBuilder.isInFullScreen() ||
+					(!_this.getConfig("fullScreenDisplayOnly") &&
+					_this.getConfig("minDisplayWidth") <= _this.getPlayer().getWidth() &&
+					_this.getConfig("minDisplayHeight") <= _this.getPlayer().getHeight())){
+					_this.render = true;
+					_this.getComponent().css('height', _this.getPlayer().getVideoHolder().height());
+					_this.show();
+				} else {
+					_this.render = false;
+					_this.hide();
+				}
+			});
 
 			// If have no components, hide
 			this.bind('layoutBuildDone', function(){
@@ -47,21 +74,7 @@
 					_this.destroy();
 				}
 			});
-
 			this.bind( 'layoutBuildDone ended', function(){
-				_this.show();
-			});
-
-			this.bind('updateLayout', function(){
-				_this.getComponent().css('height', _this.getPlayer().getVideoHolder().height());
-			});
-
-			this.bind("disableSideBar", function(){
-				_this.enabled = false;
-				_this.hide();
-			});
-			this.bind("enableSideBar", function(){
-				_this.enabled = true;
 				_this.show();
 			});
 
@@ -84,16 +97,23 @@
 			}
 		},
 		show: function(){
-			if (this.enabled) {
+			if (this.enabled && this.render) {
 				this.getComponentReminder().addClass( 'open' );
 				// Trigger the screen overlay with layout info:
 				this.getPlayer().triggerHelper( 'onShowSidelBar', {
 					'top': this.getComponentReminder().height() + 15
 				} );
+				if ( this.openAfterDisable ) {
+					this.openAfterDisable = false;
+					this.getComponentReminder().trigger( "click" );
+				}
 			}
 		},
 		hide: function(){
-			if( this.enabled && (this.keepOnScreen || (this.getConfig('clickToClose') && this.getConfig( 'isSideBarOpen')))) return;
+			if( this.enabled && this.render && (this.keepOnScreen || (this.getConfig('clickToClose') && this.getConfig( 'isSideBarOpen')))) return;
+			if ( this.getConfig( 'isSideBarOpen' ) ) {
+				this.openAfterDisable = true;
+			}
 			this.setConfig( 'isSideBarOpen', 'false' );
 			this.getComponentReminder().removeClass( 'open shifted' );
 			this.getComponent().removeClass( 'openBtn' );
