1233a992c81a8c4376b276c551977d400d47593a
amirch1 <amir.chervinsky@kaltura.com>
2014-11-26 10:08:28 +0200

    #PS-1936 #comment verify target div has height

--- kWidget/kWidget.js
+++ kWidget/kWidget.js
@@ -894,7 +894,7 @@ var kWidget = {
 		// fix for iOS8 iframe overflow issue
 		var userAgent = navigator.userAgent;
 		var isIOS8 = ( /OS 8_/.test(userAgent) || /Version\/8/.test(userAgent) ) && ( userAgent.indexOf('iPad') != -1 || userAgent.indexOf('iPhone') != -1 );
-		if (isIOS8) {
+		if (isIOS8 && parseInt(widgetElm.style.height) > 0) {
 			iframe.style.height = widgetElm.style.height;
 		}
 
