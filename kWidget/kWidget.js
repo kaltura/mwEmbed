297095f0b321f2d345f5bbbaa0e1825b52bf396f
amirch1 <amir.chervinsky@kaltura.com>
2014-11-26 09:39:05 +0200

    #PS-1936 #time 2h #comment set a fixed height for the iFrame due to known iOS8 issue where iframe with height 100% wrapped by div with fixed size gets the entire content height instead of the div height (https://www.google.co.il/search?q=iOS+iframe+height+issue&oq=iOS+iframe+height+issue&aqs=chrome..69i57j0j69i61l2.237j0j4&sourceid=chrome&es_sm=91&ie=UTF-8)

--- kWidget/kWidget.js
+++ kWidget/kWidget.js
@@ -891,6 +891,13 @@ var kWidget = {
 		// copy the target element css to the iframe proxy style:
 		iframe.style.cssText = iframeCssText;
 
+		// fix for iOS8 iframe overflow issue
+		var userAgent = navigator.userAgent;
+		var isIOS8 = ( /OS 8_/.test(userAgent) || /Version\/8/.test(userAgent) ) && ( userAgent.indexOf('iPad') != -1 || userAgent.indexOf('iPhone') != -1 );
+		if (isIOS8) {
+			iframe.style.height = widgetElm.style.height;
+		}
+
 		// Create the iframe proxy that wraps the actual iframe
 		// and will be converted into an "iframe" player via jQuery.fn.iFramePlayer call
 		var iframeProxy = document.createElement("div");
