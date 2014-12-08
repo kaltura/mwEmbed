9b7e46775668e92a36b0b841fdcf31e0a44c97f9
amirch1 <amir.chervinsky@kaltura.com>
2014-12-08 10:03:32 +0200

    PS-1912 #time 1h #comment added onAllAdsCompleted event triggering and updated test page

--- modules/DoubleClick/resources/mw.DoubleClick.js
+++ modules/DoubleClick/resources/mw.DoubleClick.js
@@ -841,6 +841,7 @@
 				} else {
 					_this.hideAdContainer();
 				}
+				_this.embedPlayer.triggerHelper( 'onAllAdsCompleted' );
 			});
 		},
 		bindChromelessEvents: function(){
@@ -910,6 +911,7 @@
 					_this.embedPlayer.triggerHelper( 'AdSupport_AdUpdateDuration', _this.entryDuration );
 					_this.embedPlayer.triggerHelper( 'timeupdate', 0);
 				}
+				_this.embedPlayer.triggerHelper( 'onAllAdsCompleted' );
 			},'allAdsCompleted', true);
 
 			this.embedPlayer.getPlayerElement().subscribe(function(adInfo){
