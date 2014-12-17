5e0715a25df577fe85cb00a82f28b408f601e6c9
vadim <vadim@panda-os.com>
2014-12-17 11:26:31 +0200

    1. Added entry metadata for Share URL 2. Added embed code, using metadata 3. Added new field for embed code

--- modules/KalturaSupport/components/share/share.js
+++ modules/KalturaSupport/components/share/share.js
@@ -18,58 +18,89 @@ mw.PluginManager.add( 'share', mw.KBaseScreen.extend({
 		templatePath: 'components/share/share.tmpl.html'
 	},
 	iconBtnClass: "icon-share",
-	setup: function(){
-		this.setupPlayerURL();
+    setup: function(){
 		this.addBindings();
 	},
 	setupPlayerURL: function(){
 		var shareURL = null;
-		switch( this.getConfig('socialShareURL') ){
+        switch( this.getConfig('socialShareURL') ){
             case 'smart':
                 shareURL = this.getSmartURL();
-            break;
+                break;
             case 'parent':
                 shareURL = this.getParentURL();
-            break;
+                break;
             default:
                 shareURL = this.getConfig("socialShareURL");
-		}
-		if( shareURL ) {
-			this.setConfig('shareURL', shareURL);
-		}
+        }
+        if( shareURL ) {
+            this.setConfig('shareURL', shareURL);
+        }
 	},
 	addBindings: function() {
 		var _this = this;
 		this.bind('playerReady', function( ){
-			_this.setupPlayerURL();
+            _this.setupPlayerURL();
+            _this.setupPlayerEmbedCode();
 		});
 	},
+
+    setupPlayerEmbedCode: function() {
+        var embedCode = '';
+        var landingPage = this.getPlayer().kalturaEntryMetaData[ this.getConfig('metadataKey') ];
+        if(typeof landingPage !== 'undefined') {
+            var serviceUrl = mw.getConfig('Kaltura.ServiceUrl');
+            var playerHeight = this.getPlayer().height;
+            var playerWidth = this.getPlayer().width;
+            var embedUrl = serviceUrl + landingPage + '/embed';
+
+            embedCode = "<iframe src=" + "'" + embedUrl + "'" + ' width=' + "'" + playerWidth + "'" +  ' height=' + "'" + playerHeight + "'" + ' frameborder=' + "'0'></iframe>";
+        }
+        else {
+            mw.log('setupPlayerEmbedCode: Error -  no landing page metadata exist for entry: ' + this.getPlayer().kentryid);
+            mw.log('setupPlayerEmbedCode: No embed code created');
+        }
+
+        this.setConfig('embedCode', embedCode);
+    },
 	getParentURL: function(){
 		return ( mw.getConfig( 'EmbedPlayer.IframeParentUrl') ) ?
 				mw.getConfig( 'EmbedPlayer.IframeParentUrl') : document.URL;
 	},
 	getKalturaShareURL: function(){
-		return mw.getConfig('Kaltura.ServiceUrl') + '/index.php/extwidget/preview' +
-				'/partner_id/' + this.getPlayer().kpartnerid +
-				'/uiconf_id/' + this.getPlayer().kuiconfid +
-				'/entry_id/' + this.getPlayer().kentryid + '/embed/dynamic';
+        var serviceUrl = mw.getConfig('Kaltura.ServiceUrl');
+        var kalturaShareUrl;
+        var landingPage = this.getPlayer().kalturaEntryMetaData[ this.getConfig('metadataKey') ];
+        if(typeof landingPage !== 'undefined') {
+            kalturaShareUrl = serviceUrl + landingPage;
+        }
+        else {
+            mw.log('getKalturaShareURL: Error -  no landing page metadata exist for entry: ' + this.getPlayer().kentryid);
+            mw.log("getKalturaShareURL: Creating default share url");
+            kalturaShareUrl =  serviceUrl + 'index.php/extwidget/preview' +
+                '/partner_id/' + this.getPlayer().kpartnerid +
+                '/uiconf_id/' + this.getPlayer().kuiconfid +
+                '/entry_id/' + this.getPlayer().kentryid + '/embed/dynamic';
+        }
+
+        return kalturaShareUrl;
 	},
 	getSmartURL: function(){
-		var shareURL = this.getKalturaShareURL();
-		if(  mw.getConfig('EmbedPlayer.IsFriendlyIframe') ){
-			try {
-				var $parentDoc = $( window['parent'].document );
-				var hasOpenGraphTags = $parentDoc.find('meta[property="og:video"]').length;
-				var hasTwitterCardsTags = $parentDoc.find('meta[name="twitter:player"]').length;
-				if( hasOpenGraphTags || hasTwitterCardsTags ){
-					shareURL = this.getParentURL();
-				}
-			} catch (e) {}
-		}
-		return shareURL;
+        var shareURL = this.getKalturaShareURL();
+        if( mw.getConfig('EmbedPlayer.IsFriendlyIframe') ){
+            try {
+                var $parentDoc = $( window['parent'].document );
+                var hasOpenGraphTags = $parentDoc.find('meta[property="og:video"]').length;
+                var hasTwitterCardsTags = $parentDoc.find('meta[name="twitter:player"]').length;
+                if( hasOpenGraphTags || hasTwitterCardsTags ){
+                    shareURL = this.getParentURL();
+                }
+            } catch (e) {}
+        }
+
+        return shareURL;
 	},
 	getTemplateData: function(){
-
 		var networks = [];
 		var socialNetworks = this.getConfig("socialNetworks");
 
@@ -109,7 +140,7 @@ mw.PluginManager.add( 'share', mw.KBaseScreen.extend({
 			'_blank',
 			'width=626,height=436'
 		);
-	}
+    }
 }));
 
 } )( window.mw, window.jQuery );
