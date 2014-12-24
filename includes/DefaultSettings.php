c7a08b94779feb0d33d832bb01ac08be59976bac
MichalRadwantzor <michal.radwantzor@kaltura.com>
2014-12-24 11:45:39 +0200

    bump 2.24.rc10

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,7 +12,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.24.rc9';
+$wgMwEmbedVersion = '2.24.rc10';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
