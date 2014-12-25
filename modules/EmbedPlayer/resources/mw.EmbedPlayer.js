0fe65d44d9967f025a3892a0df7947f1aee34a0c
orenme <oren.melamed@kaltura.com>
2014-12-25 10:10:57 +0200

    Fix merge deletion

--- modules/EmbedPlayer/resources/mw.EmbedPlayer.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayer.js
@@ -1651,7 +1651,9 @@
 				};
 
 				if ($.isFunction(_this.changeMediaCallback)) {
-					_this.changeMediaCallback(changeMediaDoneCallback);
+					setTimeout(function () {
+						_this.changeMediaCallback(changeMediaDoneCallback);
+					}, 250);
 				} else {
 					changeMediaDoneCallback();
 				}
