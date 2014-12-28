a32803f7e679b0c9015248cd1579dfd855c1c1f8
Guy Livni <guy.livni@kaltura.com>
2014-12-28 16:14:30 +0200

    additional check empty string

--- modules/KalturaSupport/resources/mw.ClosedCaptions.js
+++ modules/KalturaSupport/resources/mw.ClosedCaptions.js
@@ -320,7 +320,7 @@
 		},
 		getTextSourceFromDB: function( dbTextSource ) {
 			var _this = this;
-			if( dbTextSource.fileExt == null ){
+			if( dbTextSource.fileExt == null || dbTextSource.fileExt == ""){
 				// TODO other format mappings?
 				switch( dbTextSource.format ){
 					case '1':
