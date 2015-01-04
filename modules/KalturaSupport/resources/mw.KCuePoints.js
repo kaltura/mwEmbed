b617a96dae16d366ab905dd9216f2658b44254db
orenme <oren.melamed@kaltura.com>
2015-01-04 13:46:11 +0200

    Output logs for failed cue point API fetching

--- modules/KalturaSupport/resources/mw.KCuePoints.js
+++ modules/KalturaSupport/resources/mw.KCuePoints.js
@@ -204,10 +204,11 @@
 		},
 		isValidResult: function (data) {
 			// Check if we got error
-			if (!data
-				||
-				( data.code && data.message )
-				) {
+			if (!data){
+				mw.log("mw.KCuePoints :: error retrieving data");
+				return false;
+			} else if ( data.code && data.message ) {
+				mw.log("mw.KCuePoints :: error code: " + data.code + ", error message: " + data.message);
 				return false;
 			}
 			return true;
