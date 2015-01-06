dee9a729e13a0e71c531e94e03383ab9daa44340
amirch1 <amir.chervinsky@kaltura.com>
2015-01-06 12:05:24 +0200

    FEC-2650 #time 1h #comment don't redraw the playlist on resize event on iOS8

--- modules/KalturaSupport/components/playlistAPI.js
+++ modules/KalturaSupport/components/playlistAPI.js
@@ -42,6 +42,7 @@
 		videoWidth: null,        // used to save the video width when exiting to full screen and returning
 		multiplePlayListsReady: false, //Indicate if multiplaylist selector is ready
 		playerIsReady: false,
+		redrawOnResize: true,
 
 		setup: function (embedPlayer) {
 			if (this.getConfig('includeInLayout') === false) { // support hidden playlists - force onPage and hide its div.
@@ -67,6 +68,12 @@
 					//Revert block player display after selecting playlist entry
 					_this.getPlayer()['data-blockPlayerDisplay'] = false;
 				}
+
+				// prevent iframe resize layout refresh  on iOS8
+				if ( mw.isIOS8() ){
+					_this.redrawOnResize = false;
+				}
+
 				_this.unbind('playerReady'); // we want to select the playlist only the first time the player loads
 			});
 
@@ -123,7 +130,7 @@
 
 			// set responsiveness
 			this.bind('updateLayout', function(){
-				if (!_this.getPlayer().layoutBuilder.isInFullScreen()) {
+				if (!_this.getPlayer().layoutBuilder.isInFullScreen() && _this.redrawOnResize) {
 					if ( $( ".playlistInterface" ).width() / 3 > _this.getConfig( 'mediaItemWidth' ) ) {
 						_this.setConfig( 'mediaItemWidth', $( ".playlistInterface" ).width() / 3 );
 
