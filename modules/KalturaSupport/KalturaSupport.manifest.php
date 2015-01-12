bbc121e2a60e92fbc914fa1cb3b83f7bca9aec22
amirch1 <amir.chervinsky@kaltura.com>
2015-01-12 15:04:55 +0200

    Updated the playlist onPage label and tooltip to indicate this is not a recommended option

--- modules/KalturaSupport/KalturaSupport.manifest.php
+++ modules/KalturaSupport/KalturaSupport.manifest.php
@@ -351,7 +351,8 @@ return array(
 				'initvalue' => true
 			),
 			'onPage' => array(
-				'doc' => "If the playlist should be rendered out of the IFrame (on page).",
+				'label' => "Render on publisher's page",
+				'doc' => "When unchecked, the playlist will be rendered inside the player's iframe (recommended).",
 				'type' => 'boolean'
 			),
 			'MinClips' => array(
