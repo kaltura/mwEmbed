a81828ae47835b40789b7b6d97a2aa8bb98dfc9f
Itay Kinnrot <itay.kinnrot@kaltura.com>
2015-01-18 17:21:39 +0200

    update version

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,7 +12,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.25.3';
+$wgMwEmbedVersion = '2.25.4';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
