b31941dccf80fffccd2f39fa4f30ee98c7e0df07
Itay Inbar <itayi@Itays-MacBook-Pro.local>
2015-02-17 18:54:49 +0200

    better

--- modules/EmbedPlayer/resources/mw.EmbedPlayerNativeComponent.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayerNativeComponent.js
@@ -363,7 +363,6 @@
 
 		_ondurationchange: function () {
 			mw.log( "EmbedPlayerNativeComponent:: onDurationChange::" + this.getPlayerElement().duration );
-			this.setDuration( this.getPlayerElement().attr('currentTime') );//itay
 		},
 
 		/**
