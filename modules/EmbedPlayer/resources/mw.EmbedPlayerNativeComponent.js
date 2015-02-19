ae6d2774beaf80943732c1246aa0a572dc7550d9
Itay Inbar <itayi@Itays-MacBook-Pro.local>
2015-02-19 12:45:25 +0200

    goLive

--- modules/EmbedPlayer/resources/mw.EmbedPlayerNativeComponent.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayerNativeComponent.js
@@ -340,7 +340,7 @@
 
 		backToLive: function () {
 			this.triggerHelper('movingBackToLive');
-			this.seek(this.getDuration());
+			this.getPlayerElement().attr('goLive', 'true');
 		},
 
 		doNativeAction: function (actionParams) {
@@ -483,6 +483,15 @@
 		_onprogress: function (event, progress) {
 			if (typeof progress !== 'undefined') {
 				this.updateBufferStatus(progress);
+				if(progress < 0.9){
+					if(!this.showProgressSpinner) {
+						this.addPlayerSpinner();
+						this.showProgressSpinner = true;
+					}
+				}else{
+					this.showProgressSpinner = false;
+					this.hideSpinner();
+				}
 			}
 		},
 
