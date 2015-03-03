bacce49ed6368ae66caa7f1e27ab65beba5cfdaf
einatr <einat.rolnik@kaltura.com>
2015-03-03 12:29:18 +0200

    bump 2.28.rc16

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -12,7 +12,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.28.rc15';
+$wgMwEmbedVersion = '2.28.rc16';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
