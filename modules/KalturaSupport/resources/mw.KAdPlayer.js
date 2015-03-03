3879181b8ed4e0a943ab97c6679b927eeecd940e
amirch1 <amir.chervinsky@kaltura.com>
2015-03-03 15:15:23 -0500

    FEC-3115 #time 2h #comment run VPAID flow regularly when the ad is an overlay on mobile device

--- modules/KalturaSupport/resources/mw.KAdPlayer.js
+++ modules/KalturaSupport/resources/mw.KAdPlayer.js
@@ -1595,7 +1595,7 @@ mw.KAdPlayer.prototype = {
 			}
 		};
 
-		if ( mw.isAndroid() || mw.isIpad() ) {
+		if ( (mw.isAndroid() || mw.isIpad()) && adSlot.type !== "overlay" ) {
 			var bindPostFix = ".vpaidSequenceCheck";
 			this.embedPlayer.bindHelper( 'playing' + bindPostFix, function () {
 				_this.embedPlayer.unbindHelper( 'playing' + bindPostFix );
