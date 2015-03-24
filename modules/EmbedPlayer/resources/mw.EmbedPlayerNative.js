d46e52d726e741aab2466d15c42371fbefa23cbf
Itay Kinnrot <itay.kinnrot@kaltura.com>
2015-03-24 17:43:25 +0200

    fix ios8 load issue

--- modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
@@ -333,7 +333,7 @@
 			if (!_this.loop &&
 				( mw.isIphone() || ( mw.isIpad() && mw.isIOS5() ) )) {
 				mw.log("EmbedPlayerNative::postEmbedActions: issue .load() call");
-				vid.load();
+//				vid.load();
 			}
 		},
 		/**
@@ -1071,7 +1071,7 @@
 						}
 						// issue a play request
 						if (!_this.playing) {
-							if (mw.isIOS8() && mw.isIphone()) {
+							if (0 && mw.isIOS8() && mw.isIphone()) {
 								setTimeout(function () {
 									vid.play();
 								}, 0);
