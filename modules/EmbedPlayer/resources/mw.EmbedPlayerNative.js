a0ccc2eaf38dd7716eb1651c40020a23167f9ab1
Itay Kinnrot <itay.kinnrot@kaltura.com>
2015-03-24 17:07:27 +0200

    fix ios8 load issue

--- modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
@@ -323,7 +323,7 @@ mw.EmbedPlayerNative = {
 		// other mobile devices ( android 4, break if we call load at play time )
 		if ( !_this.loop && mw.isIOS() ) {
 			mw.log("EmbedPlayerNative::postEmbedActions: issue .load() call");
-			vid.load();
+//			vid.load();
 		}
 	},
 	/**
@@ -993,10 +993,13 @@ mw.EmbedPlayerNative = {
 					if( _this.useNativePlayerControls() && $( _this ).find( 'video ').length == 0 ){
 						$( _this ).hide();
 					}
-					if( !( mw.isIphone() && mw.isIOS80() ) ) {
-						// update the preload attribute to auto
-							$( _this.getPlayerElement() ).attr('preload',"auto" );
-					}
+					// update the preload attribute to auto
+// if it's iOS8 the native iOS player won't be shown
+if( !( mw.isIphone() && mw.isIOS80() ) ) {
+
+					$( _this.getPlayerElement() ).attr('preload',"auto" );
+}
+
 					// issue a play request
 					_this.getPlayerElement().play();
 					// re-start the monitor:
