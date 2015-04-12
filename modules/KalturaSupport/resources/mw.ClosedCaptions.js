2c14c9f7b5f0cde7a5f425b70e207ece0f021e33
Ran Yefet <ran@yefet.net>
2015-04-12 13:50:18 +0300

    Support for forceLoadLanguage config

--- modules/KalturaSupport/resources/mw.ClosedCaptions.js
+++ modules/KalturaSupport/resources/mw.ClosedCaptions.js
@@ -19,7 +19,9 @@
 			"showOffButton": true,
 			"toggleActiveCaption": false,
 			"useExternalClosedCaptions": false,
-			"offButtonPosition": "first"
+			"offButtonPosition": "first",
+			// Can be used to force loading specific language and expose to other plugins
+			"forceLoadLanguage": false
 		},
 
 		textSources: [],
@@ -279,6 +281,11 @@
 					_this.textSources = textSources;
 				}]);
 
+				// Handle Force loading of captions
+				if( _this.getConfig('forceLoadLanguage') ) {
+					_this.forceLoadLanguage();
+				}
+
 				if( _this.getConfig('displayCaptions') !== false || ($.cookie( _this.cookieName ) !== 'None' && $.cookie( _this.cookieName )) ){
 					_this.autoSelectSource();
 					if( _this.selectedSource ){
@@ -386,6 +393,16 @@
 			);
 			// Return a "textSource" object:
 			return new mw.TextSource( embedSource );
+		},
+		forceLoadLanguage: function(){
+			var lang = this.getConfig('forceLoadLanguage');
+			var source = this.selectSourceByLangKey( lang );
+			// Found caption
+			if( source && !source.loaded ) {
+				source.load($.proxy(function(){
+					this.getPlayer().triggerHelper('newClosedCaptionsData', source);
+				},this));
+			}
 		},
 		autoSelectSource: function(){
 			var _this = this;
