4e690bb0e9236b956c169d1e1128273924f3a627
Itay Kinnrot <itay.kinnrot@kaltura.com>
2015-05-07 21:35:10 +0300

    work in progress

---
+++ modules/ComscoreStreamingTag/ComscoreStreamingTag.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+   "comscorestreamingtag" =>  array(
+      'scripts' => "resources/mw.ComscoreStreamingTag.js",
+      'dependencies' => array(
+         "mw.EmbedPlayer",
+         'mw.ajaxProxy'
+      ),
+      'kalturaPluginName' => 'comscorestreamingtag'
+   ),
+);
