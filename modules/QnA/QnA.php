a6a2b98d2ab11ccdc85435993296fce68544a53c
asafrobinovich <asaf.robinovich@kaltura.com>
2015-05-14 11:35:40 +0300

    Q&A plugin - WEBC-481 after session with the designer

--- modules/QnA/QnA.php
+++ modules/QnA/QnA.php
@@ -4,7 +4,7 @@ return array(
 		'scripts' => array( 'resources/knockout-3.3.0.js', 'resources/qnaKOExtensions.js', 'resources/qnaModule.js', 'resources/qnaService.js', 'resources/qna.js'),
 		'dependencies' => array( 'mw.KBaseScreen', 'nanoScroller' ),
 		'kalturaPluginName' => 'qna',
-		'styles' => 'resources/qna.css',
+		'styles' => array( 'resources/css/qna.css', 'resources/css/qna-announcements.css', 'resources/css/qna-fonts.css', 'resources/css/qna-nano.css', 'resources/css/qna-threads-list.css'),
 		'templates' => "../QnA/resources/qna.tmpl.html",
 		'messageFile' => 'QnA.i18n.php',
 	),
