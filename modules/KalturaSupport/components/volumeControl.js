6980d32f2a521b705a538196dfb68aa580648cc5
amirch1 <amir.chervinsky@kaltura.com>
2015-05-31 08:57:29 +0300

    FEC-677 #comment disregard pinVolumeBar on vertical layout

--- modules/KalturaSupport/components/volumeControl.js
+++ modules/KalturaSupport/components/volumeControl.js
@@ -88,7 +88,7 @@
 					if ( _this.getConfig( 'layout' ) === "horizontal" && !_this.getConfig( 'pinVolumeBar' ) ) {
 						_this.getComponent().removeClass( 'open' );
 					}
-					if ( _this.getConfig( 'layout' ) === "vertical" && !_this.getConfig( 'pinVolumeBar' ) ) {
+					if ( _this.getConfig( 'layout' ) === "vertical" ) {
 						setTimeout(function(){
 							if (!mouseOverSlider){
 								_this.getComponent().removeClass( 'open' );
