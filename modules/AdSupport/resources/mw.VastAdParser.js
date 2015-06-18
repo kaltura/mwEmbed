464e10d29809ee559453b7adc47c6d562a7c976e
Guy Livni <guy.livni@kaltura.com>
2015-06-18 15:40:05 +0300

    add check for "`"

--- modules/AdSupport/resources/mw.VastAdParser.js
+++ modules/AdSupport/resources/mw.VastAdParser.js
@@ -441,7 +441,7 @@ mw.VastAdParser = {
 		// check for empty impression, return empty text instead of trying to decode
 		var urlText = $.trim( $( node ).text() );
 		try {
-			if( ! /[<>#%{}\s|\\^\~\[\]]/.test(decodeURIComponent(urlText)) ){
+			if( ! /[<>`#%{}\s|\\^\~\[\]]/.test(decodeURIComponent(urlText)) ){
 				urlText = decodeURIComponent(urlText)
 			}
 		} catch( e ){
