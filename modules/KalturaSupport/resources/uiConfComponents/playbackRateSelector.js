c1d7795d4b601a7b1a6372978074aeafa5b5cfd0
amirch1 <amir.chervinsky@kaltura.com>
2015-06-29 17:03:01 +0300

    FEC-3331 #time 4h #comment add support for hds playback and added a flag to turn the feature on and off

--- modules/KalturaSupport/resources/uiConfComponents/playbackRateSelector.js
+++ modules/KalturaSupport/resources/uiConfComponents/playbackRateSelector.js
@@ -10,11 +10,13 @@
 		 	"showTooltip": true,
 		 	'defaultSpeed': '1',
 			'speeds': ".5,.75,1,1.5,2",
-			'enableKeyboardShortcuts': true
+			'enableKeyboardShortcuts': true,
+			'serverSpeedPlayback': true
 		},
 
 		isDisabled: false,
 		currentSpeed: 1,
+		manifestSource: null,
 
 		isSafeEnviornment: function(){
 			var _this = this,
@@ -49,6 +51,17 @@
 			this.bind( 'playerReady', function(){
 				_this.buildMenu();
 			});
+
+			this.bind( 'onChangeMediaDone', function(){
+				_this.manifestSource = null;
+			});
+
+			this.bind( 'SourceSelected', function(e, source){
+				if (source.src.indexOf("playManifest") !== -1 && (source.src.indexOf("/a.f4m") !== -1 || source.src.indexOf("/a.m3u8") !==-1)){
+					_this.manifestSource = source.src;
+				}
+			});
+
 			this.bind( 'onRemovePlayerSpinner', function(){
 				 if ( _this.getPlayer().getPlayerElement() ) {
 					 _this.getPlayer().getPlayerElement().playbackRate = _this.currentSpeed;
@@ -57,18 +70,7 @@
 			this.bind( 'playbackRateChangeSpeed', function(e, arg ){
 				_this.setSpeedFromApi( arg );
 			});
-			this.bind( 'SourceSelected', function(e, source ){
-				if ( source.src.length && _this.currentSpeed <= 2 ){
-					var src = source.src;
-					var fileName = src.substr(src.lastIndexOf("/"));
-					var base = src.substr(0,src.lastIndexOf("/"));
-					if (source.src.indexOf("playbackRate") !== -1){
-						base = base.substr(0,base.lastIndexOf("playbackRate")-1);
-					}
-					var newSrc = base + "/playbackRate/" + _this.currentSpeed + fileName;
-					source.src = newSrc;
-				}
-			});
+
 			if( this.getConfig('enableKeyboardShortcuts') ){
 				this.bind( 'addKeyBindCallback', function( e, addKeyCallback ){
 					_this.addKeyboardShortcuts( addKeyCallback );
@@ -158,46 +160,52 @@
 		handlePlayerInstanceUpdate: function( newSpeed ){
 			var _this = this;
 			var currentPlayTime = this.getPlayer().currentTime;
-
 			this.currentSpeed = newSpeed;
+			if (this.getConfig("serverSpeedPlayback") && this.currentSpeed <= 2 && this.getPlayer().instanceOf === 'Kplayer' && this.manifestSource){
+				var source = this.manifestSource;
 
-			if (this.currentSpeed <= 2){
-				var selectedSource = this.embedPlayer.mediaElement.autoSelectSource(this.embedPlayer.supportsURLTimeEncoding(), this.embedPlayer.startTime, this.embedPlayer.pauseTime);
-				$(this.embedPlayer).bind("mediaLoaded", function(){
-					alert("mediaLoaded");
-					$(this.embedPlayer).unbind("mediaLoaded");
-					setTimeout(function(){
-						_this.embedPlayer.bindHelper("seeked", function(){
-							_this.embedPlayer.unbindHelper("seeked");
-							_this.embedPlayer.play();
-						});
-						_this.embedPlayer.seek( currentPlayTime ); // issue a seek if given new seek time
-					}, 0);
-				});
-				this.embedPlayer.playerObject.sendNotification("changeMedia", { "entryUrl" : selectedSource.src});
-
-				return;
-			}
-
-
-
-			var source = this.getPlayer().mediaElement.autoSelectNativeSource();
-			var player = mw.EmbedTypes.getMediaPlayers().getNativePlayer( source.mimeType );
-			this.getPlayer().selectPlayer ( player );
-			this.getPlayer().updatePlaybackInterface( function(){
-				// update playback rate:
-				if( currentPlayTime == 0 ){
-					_this.updatePlaybackRate( newSpeed );
-				}else{
-					setTimeout(function(){
-						_this.bind("seeked", function(){
-							_this.updatePlaybackRate( newSpeed );
-							_this.unbind("seeked");
-						});
-						_this.getPlayer().seek( currentPlayTime ); // issue a seek if given new seek time
-					}, 0);
+				var fileName = source.substr(source.lastIndexOf("/"));
+				var base = source.substr(0,source.lastIndexOf("/"));
+				if (fileName.indexOf("/a.f4m") === 0){
+					base = base.substr(0,base.length-3);
 				}
-			});
+				if (source.indexOf("playbackRate") !== -1){
+					base = base.substr(0,base.lastIndexOf("playbackRate")-1);
+				}
+				var newSrc = base + "/playbackRate/" + this.currentSpeed + fileName;
+
+				if ( currentPlayTime > 0 ){
+					$(this.embedPlayer).bind("playing", function(){
+						$(_this.embedPlayer).unbind("playing");
+						setTimeout(function(){
+							_this.embedPlayer.seek( currentPlayTime / newSpeed );
+						},0);
+
+					});
+				}
+
+				this.updatePlaybackRate( newSpeed );
+				this.embedPlayer.playerObject.sendNotification("changeMedia", { "entryUrl" : newSrc});
+				this.embedPlayer.play();
+			}else{
+				var source = this.getPlayer().mediaElement.autoSelectNativeSource();
+				var player = mw.EmbedTypes.getMediaPlayers().getNativePlayer( source.mimeType );
+				this.getPlayer().selectPlayer ( player );
+				this.getPlayer().updatePlaybackInterface( function(){
+					// update playback rate:
+					if( currentPlayTime == 0 ){
+						_this.updatePlaybackRate( newSpeed );
+					}else{
+						setTimeout(function(){
+							_this.bind("seeked", function(){
+								_this.updatePlaybackRate( newSpeed );
+								_this.unbind("seeked");
+							});
+							_this.getPlayer().seek( currentPlayTime ); // issue a seek if given new seek time
+						}, 0);
+					}
+				});
+			}
 		},
 		/**
 		 * updatePlaybackRate issues a call to native player element to update playbackRate to target speed.
