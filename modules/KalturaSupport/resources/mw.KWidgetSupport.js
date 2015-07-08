2188f0d278c47effdd097eee393c22e6caaa6f0c
Itay Kinnrot <itay.kinnrot@kaltura.com>
2015-07-08 14:08:50 +0300

    resolve conflicts

--- modules/KalturaSupport/resources/mw.KWidgetSupport.js
+++ modules/KalturaSupport/resources/mw.KWidgetSupport.js
@@ -1492,6 +1492,21 @@ timeout: 7000,
 			deviceSources = this.removeAdaptiveFlavors( deviceSources );
 		}
 
+
+		// PRemove adaptive sources on Windows Phone
+		if( mw.isWindowsPhone() ) {
+			deviceSources = this.removeAdaptiveFlavors( deviceSources );
+		}
+
+		// if we have streamertype that is not hls and we support hls on the native player - we'll use kplayer + hls - we want to eliminate  this option
+		// for now the only usecase is microsoft edge browser
+		if ( mw.EmbedTypes.getMediaPlayers().isSupportedPlayer( 'kplayer' )  &&
+			this.originalStreamerType &&
+			this.originalStreamerType != "hls" &&
+			 mw.getConfig("LeadWithHLSOnFlash")== null 	){
+			    deviceSources = this.removeAdaptiveFlavors( deviceSources );
+		}
+
 		//TODO: Remove duplicate webm and h264 flavors
 		/*if (mw.EmbedTypes.getMediaPlayers().isSupportedPlayer( 'h264Native' ) && mw.EmbedTypes.getMediaPlayers().isSupportedPlayer( 'webmNative' )) {
 			//remove someone if duplicate
