92141ea290eaf39484c246f0e25b404ae599b254
igorshevach <igor.shevach@kaltura.com>
2015-07-09 10:52:13 +0300

    add networking stats

--- modules/DebugInfo/resources/DebugInfo.js
+++ modules/DebugInfo/resources/DebugInfo.js
@@ -167,6 +167,9 @@ mw.PluginManager.add( 'debugInfo', mw.KBaseComponent.extend({
                 this.$scope.mcRenderFps = data.RenderFps;
                 this.$scope.mcRenderDroppedFps = data.RenderDroppedFps;
                 this.$scope.multiastServerUrl=data.multiastServerUrl;
+                this.$scope.mcPacketLoss=data.PacketLoss;
+                this.$scope.mcPacketLossRate=data.PacketLossRate;
+                this.$scope.mcPacketsPerSec=data.PacketRate;
             }
         }
     }
