2372dc7ff4f2e4866708ed3aa2f7874e60f3314d
Michael Dale <mdale@wikimedia.org>
2015-10-14 07:14:16 -0700

    move escape into a default formater

--- modules/KalturaSupport/tests/externalResources/customFormater.js
+++ modules/KalturaSupport/tests/externalResources/customFormater.js
@@ -1,8 +1,5 @@
 mw.kalturaPluginWrapper(function(){
 	mw.util.formaters().register({
-		'escape': function( value ){
-			return escape( value );
-		},
 		'reverse': function( value){
 			return value.split("").reverse().join("");
 		}
