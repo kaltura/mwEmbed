36d9582f1789971e178d24aeb487486d8422a629
amirch1 <amir.chervinsky@kaltura.com>
2015-11-09 11:04:24 +0200

    SUP-6097 #comment revert unregistering to playerReady event

--- modules/KalturaSupport/resources/mw.ClosedCaptions.js
+++ modules/KalturaSupport/resources/mw.ClosedCaptions.js
@@ -115,7 +115,6 @@
 						_this.destory();
 						_this.setupTextSources( function () {
 							_this.buildMenu( _this.textSources );
-							_this.unbind('playerReady');
 						} );
 					} );
 					outOfBandCaptionEventHandlers.call(this);
