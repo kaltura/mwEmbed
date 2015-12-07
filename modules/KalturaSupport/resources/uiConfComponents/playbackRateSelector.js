ad88b235632a279e45e7766d58293d41d092d972
amirch1 <amir.chervinsky@kaltura.com>
2015-12-07 13:00:03 +0200

    FEC-2648 #time 1h #comment separate server side and client side speed change logic to separate functions

--- modules/KalturaSupport/resources/uiConfComponents/playbackRateSelector.js
+++ modules/KalturaSupport/resources/uiConfComponents/playbackRateSelector.js
@@ -170,78 +170,88 @@
 			var currentPlayTime = this.getPlayer().currentTime;
 			this.currentSpeed = newSpeed;
 			if (this.getConfig("serverSpeedPlayback") && this.currentSpeed <= 2 && (this.getPlayer().instanceOf === 'Kplayer' || mw.isMobileDevice())){
-				// for decimal numbers, make sure we have only one digit after the dot (server limitation)
-				if ( this.currentSpeed % 1 !== 0 ){
-					this.currentSpeed = this.currentSpeed.toFixed(1);
-				}
-				var source = this.manifestSource;
-				if (this.manifestSource){ // for HLS and HDS, since the movie duration changes, we need to recalculate the position
-					currentPlayTime = currentPlayTime * previousSpeed / this.currentSpeed;
-				}
-				var fileName = source.substr(source.lastIndexOf("/"));
-				var base = source.substr(0,source.lastIndexOf("/"));
-				if (fileName.indexOf("/a.f4m") === 0){
-					base = base.substr(0,base.length-3);
-				}
-				if (source.indexOf("playbackRate") !== -1){
-					base = base.substr(0,base.lastIndexOf("playbackRate")-1);
-				}
-				var newSrc = base + "/playbackRate/" + this.currentSpeed + fileName;
-				this.updatePlaybackRate( newSpeed );
+				this.switchServerSideSpeed(newSpeed, previousSpeed, currentPlayTime);
+			}else{
+				this.switchClientSideSpeed(newSpeed, previousSpeed, currentPlayTime);
+			}
+		},
+		switchServerSideSpeed: function(newSpeed, previousSpeed, currentPlayTime){
+			this.log('Set Speed on the server side to: ' + newSpeed);
+			var _this = this;
+			// for decimal numbers, make sure we have only one digit after the dot (server limitation)
+			if ( this.currentSpeed % 1 !== 0 ){
+				this.currentSpeed = this.currentSpeed.toFixed(1);
+			}
+			var source = this.manifestSource;
+			if (this.manifestSource){ // for HLS and HDS, since the movie duration changes, we need to recalculate the position
+				currentPlayTime = currentPlayTime * previousSpeed / this.currentSpeed;
+			}
+			var fileName = source.substr(source.lastIndexOf("/"));
+			var base = source.substr(0,source.lastIndexOf("/"));
+			if (fileName.indexOf("/a.f4m") === 0){
+				base = base.substr(0,base.length-3);
+			}
+			if (source.indexOf("playbackRate") !== -1){
+				base = base.substr(0,base.lastIndexOf("playbackRate")-1);
+			}
+			var newSrc = base + "/playbackRate/" + this.currentSpeed + fileName;
+			this.updatePlaybackRate( newSpeed );
 
-				if (mw.isMobileDevice()){
-					this.getPlayer().getPlayerElement().src = newSrc;
-					this.getPlayer().mediaElement.selectedSource.src = newSrc;
-					if (mw.isIOS()){
-						this.getPlayer().getPlayerElement().load();
-					}
-				}else{
-					this.embedPlayer.playerObject.sendNotification("changeMedia", { "entryUrl" : newSrc});
-					this.embedPlayer.play();
-				}
-
-				if ( currentPlayTime > 0 ){
-					this.embedPlayer.stopMonitor();
-					$(this.embedPlayer).bind("playing", function(){
-						$(_this.embedPlayer).unbind("playing");
-						setTimeout(function(){
-							if (mw.isIOS()){
-								_this.getPlayer().getPlayerElement().currentTime =  currentPlayTime ;
-							}else{
-								_this.embedPlayer.seek( currentPlayTime );
-							}
-							_this.embedPlayer.startMonitor();
-						},0);
-					});
+			if (mw.isMobileDevice()){
+				this.getPlayer().getPlayerElement().src = newSrc;
+				this.getPlayer().mediaElement.selectedSource.src = newSrc;
+				if (mw.isIOS()){
+					this.getPlayer().getPlayerElement().load();
 				}
 			}else{
-				// look for native sources and send them to the autoSelectSource function
-				var nativeSources = [];
-				var playableSources = this.getPlayer().mediaElement.getPlayableSources();
-				$.each( playableSources, function(i, source ){
-					if ( source.mimeType == 'video/mp4' || source.mimeType == 'video/h264' ) {
-						nativeSources.push( source );
-					}
-				});
-				var options = nativeSources.length ? { 'sources' : nativeSources } : {};
-				var source = this.getPlayer().mediaElement.autoSelectSource(options);
-				var player = mw.EmbedTypes.getMediaPlayers().getNativePlayer( source.mimeType );
-				this.getPlayer().selectPlayer ( player );
-				this.getPlayer().updatePlaybackInterface( function(){
-					// update playback rate:
-					if( currentPlayTime == 0 ){
-						_this.updatePlaybackRate( newSpeed );
-					}else{
-						setTimeout(function(){
-							_this.bind("seeked", function(){
-								_this.updatePlaybackRate( newSpeed );
-								_this.unbind("seeked");
-							});
-							_this.getPlayer().seek( currentPlayTime ); // issue a seek if given new seek time
-						}, 200);
-					}
+				this.embedPlayer.playerObject.sendNotification("changeMedia", { "entryUrl" : newSrc});
+				this.embedPlayer.play();
+			}
+
+			if ( currentPlayTime > 0 ){
+				this.embedPlayer.stopMonitor();
+				$(this.embedPlayer).bind("playing", function(){
+					$(_this.embedPlayer).unbind("playing");
+					setTimeout(function(){
+						if (mw.isIOS()){
+							_this.getPlayer().getPlayerElement().currentTime =  currentPlayTime ;
+						}else{
+							_this.embedPlayer.seek( currentPlayTime );
+						}
+						_this.embedPlayer.startMonitor();
+					},0);
 				});
 			}
+		},
+		switchClientSideSpeed: function(newSpeed, previousSpeed, currentPlayTime){
+			this.log('Set Speed on the client side to: ' + newSpeed);
+			var _this = this;
+			// look for native sources and send them to the autoSelectSource function
+			var nativeSources = [];
+			var playableSources = this.getPlayer().mediaElement.getPlayableSources();
+			$.each( playableSources, function(i, source ){
+				if ( source.mimeType == 'video/mp4' || source.mimeType == 'video/h264' ) {
+					nativeSources.push( source );
+				}
+			});
+			var options = nativeSources.length ? { 'sources' : nativeSources } : {};
+			var source = this.getPlayer().mediaElement.autoSelectSource(options);
+			var player = mw.EmbedTypes.getMediaPlayers().getNativePlayer( source.mimeType );
+			this.getPlayer().selectPlayer ( player );
+			this.getPlayer().updatePlaybackInterface( function(){
+				// update playback rate:
+				if( currentPlayTime == 0 ){
+					_this.updatePlaybackRate( newSpeed );
+				}else{
+					setTimeout(function(){
+						_this.bind("seeked", function(){
+							_this.updatePlaybackRate( newSpeed );
+							_this.unbind("seeked");
+						});
+						_this.getPlayer().seek( currentPlayTime ); // issue a seek if given new seek time
+					}, 200);
+				}
+			});
 		},
 		/**
 		 * updatePlaybackRate issues a call to native player element to update playbackRate to target speed.
