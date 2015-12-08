4e7c88138114dfa103f98eb768d95f1eda1c2018
Joan Puig Sanz <jpuig@comscore.com>
2015-12-08 18:17:07 +0100

    resolve conflicts

--- modules/ComscoreStreamingTag/resources/mw.ComscoreStreamingTag.js
+++ modules/ComscoreStreamingTag/resources/mw.ComscoreStreamingTag.js
@@ -154,7 +154,6 @@
 
 		onBuffering: function() {
 			this.callStreamSensePlugin("notify", this.playerEvents.BUFFER, {});
-			this.setPlayerPluginState(this.PlayerPluginState().BUFFERING);
 		},
 
 		onAdPlay: function(adId, type, index, duration) {
