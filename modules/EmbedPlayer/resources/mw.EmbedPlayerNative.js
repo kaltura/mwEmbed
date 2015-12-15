3fd311778a8fb5036782bc193cd23726078790fc
amirch1 <amir.chervinsky@kaltura.com>
2015-12-15 13:56:09 +0200

    start monitor on native onplay event instead of when clicking our play button. Fix for iPhone not restoring monitor after pause in native video player.

--- modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
@@ -866,8 +866,6 @@
 							vid.play();
 						}
 						_this.mobilePlayed = true;
-						// re-start the monitor:
-						_this.monitor();
 					}
 				} else {
 					_this.log(" parent play returned false, don't issue play on native element");
@@ -1173,6 +1171,8 @@
 			}
 			// Set firstEmbedPlay state to false to avoid initial play invocation :
 			this.ignoreNextNativeEvent = false;
+			// re-start the monitor:
+			this.monitor();
 		},
 
 		/**
