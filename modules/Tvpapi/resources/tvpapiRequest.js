f4db3dae38f2e4118b87d23c434a9aadd3488577
Oren Me <oren.me@gmail.com>
2016-01-31 15:45:13 +0200

    FEC-4703 #comment Support refresh token for TVPAPI

--- modules/Tvpapi/resources/tvpapiRequest.js
+++ modules/Tvpapi/resources/tvpapiRequest.js
@@ -8,6 +8,18 @@
             "restApiBaseUrl": ""
         },
 
+        restMethod: null,
+
+        init: function( embedPlayer, callback, pluginName ) {
+            // parent init return true / false based on isSafeEnviornment, default true
+            if( this._super( embedPlayer, callback, pluginName ) === false ) {
+                return ;
+            }
+            if( !this.restMethod ) {
+                this.restMethod = this.getConfig("restMethod");
+            }
+        },
+
         getProxyConfig: function( attr, raw ) {
             if( raw ){
                 return this.embedPlayer.getRawKalturaConfig( "proxyData", attr );
@@ -21,7 +33,7 @@
 
         getRequestUrl: function(){
             var baseUrl = this.getConfig( "restApiBaseUrl" ) || this.getPlayer().getKalturaConfig( null, 'TVPAPIBaseUrl' );
-            var restMethod = this.getConfig("restMethod");
+            var restMethod = this.restMethod;
             var url;
             if (baseUrl !== "" && restMethod !== "") {
                 url = baseUrl + restMethod;
@@ -37,6 +49,11 @@
                 type: "POST",
                 dataType: "json",
                 contentType: "application/json",
+                statusCode: {
+                    401: function() {
+                        _this.embedPlayer.embedPlayer("tvpapiTokenExpired");
+                    }
+                },
                 data: JSON.stringify( data ),
                 async: false
             } );
