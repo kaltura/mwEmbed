c2e816aa923b149abf50a1eb13a40998596b3448
Itay Kinnrot <itay.kinnrot@kaltura.com>
2016-02-03 14:36:57 +0200

    support change media,back to live,update play to not override source

--- modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
@@ -835,16 +835,18 @@
 					if (_this.getPlayerElement() && _this.getPlayerElement().play) {
 						_this.log(" issue native play call:");
 						// make sure the source is set:
-						if ( $(vid).attr('src') != _this.getSrc() || _this.resetSrc ) {
-							$(vid).attr('src', _this.getSrc());
-                            //trigger play again for iPad and El Capitan
-                            setTimeout( function () {
-                                if ( !_this.playing ) {
-                                    vid.play();
-                                    _this.parseTracks();
-                                }
-                            }, 300 );
-                            _this.resetSrc = false;
+						if (!_this.skipUpdateSource) {
+							if ( $( vid ).attr( 'src' ) != _this.getSrc() || _this.resetSrc ) {
+								$( vid ).attr( 'src' , _this.getSrc() );
+								//trigger play again for iPad and El Capitan
+								setTimeout( function () {
+									if ( !_this.playing ) {
+										vid.play();
+										_this.parseTracks();
+									}
+								} , 300 );
+								_this.resetSrc = false;
+							}
 						}
 						_this.hideSpinnerOncePlaying();
 						// make sure the video tag is displayed:
--- modules/HLSJS/resources/hlsjs.js
+++ modules/HLSJS/resources/hlsjs.js
@@ -31,70 +31,32 @@
 
 		addBindings: function() {
 			var $this = this;
+
 			this.bind("onSelectSource", function(event,sources){
+				$this.LoadHLS = false;
 				$.each(sources,function(index,item){
 					if (item.mimeType === "application/vnd.apple.mpegurl") {
 						$this.getPlayer().mediaElement.selectedSource = item;
+						$this.LoadHLS = true;
 
 					}
 				});
 			});
 			this.bind("playerReady",function(){
-				$this.getPlayer().skipUpdateSource = true;
-				$this.hls.attachMedia($this.getPlayer().getPlayerElement());
-				$this.hls.loadSource($this.getPlayer().getSrc());
-
-				$this.getPlayer().play = function(){
-					var vid = this.getPlayerElement();
-					var _this = this;
-
-					// if starting playback from stoped state and not in an ad or otherise blocked controls state:
-					// restore player:
-					if (this.isStopped() && this._playContorls) {
-						this.restorePlayerOnScreen();
-					}
-
-					var doPlay = function () {
-						// Run parent play:
-						if (_this.parent_play()) {
-							if (_this.getPlayerElement() && _this.getPlayerElement().play) {
-								_this.log(" issue native play call:");
-								// make sure the source is set:
-								_this.hideSpinnerOncePlaying();
-								// make sure the video tag is displayed:
-								$(_this.getPlayerElement()).show();
-								// Remove any poster div ( that would overlay the player )
-								if (!_this.isAudio()) {
-									_this.removePoster();
-								}
-								// if using native controls make sure the inteface does not block the native controls interface:
-								if (_this.useNativePlayerControls() && $(_this).find('video ').length == 0) {
-									$(_this).hide();
-								}
-								// issue a play request
-									var video = vid;
-
-								//	$this.hls.on(Hls.Events.MANIFEST_PARSED,function() {
-											vid.play();
-								//	});
-								_this.mobilePlayed = true;
-								// re-start the monitor:
-								_this.monitor();
-							}
-						} else {
-							_this.log(" parent play returned false, don't issue play on native element");
-						}
+				if ($this.LoadHLS) {
+					$this.getPlayer().skipUpdateSource = true;
+					$this.hls.attachMedia( $this.getPlayer().getPlayerElement() );
+					$this.hls.loadSource( $this.getPlayer().getSrc() );
+					$this.getPlayer().backToLive = function () {
+						var _this = this;
+						var vid = this.getPlayerElement();
+						vid.currentTime = vid.duration;
+						setTimeout( function () {
+							_this.triggerHelper( 'movingBackToLive' ); //for some reason on Mac the isLive client response is a little bit delayed, so in order to get update liveUI properly, we need to delay "movingBackToLive" helper
+						} , 1000 );
 					};
-					doPlay();
-				};
-
-
+				}
 			});
-
-
 		}
-
-
-
 	}));
 } )( window.mw, window.jQuery ,window.Hls);
