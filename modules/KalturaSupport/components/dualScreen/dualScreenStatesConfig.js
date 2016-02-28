d23c0967ac8e2b5ef57daeae014cf4d2333fe6f0
Eran Sakal <eran.sakal@kaltura.com>
2016-02-28 15:05:51 +0200

    updated state machine to support view type as main states with 'invoke' function that knows how to setup that state. (previously the state machine logic was dependent also on the state you have been before the change)

--- modules/KalturaSupport/components/dualScreen/dualScreenStatesConfig.js
+++ modules/KalturaSupport/components/dualScreen/dualScreenStatesConfig.js
@@ -1,102 +1,96 @@
-(function ( mw ) {
-	"use strict";
-	mw.dualScreen = mw.dualScreen || {};
-	mw.dualScreen.states = [
-		{
-			'name': 'PiP',
-			'initial': true,
-			'invoke' : function(context)
-			{
-				if (context.previousState !== 'PiP')
-				{
-					this.enableUserActions();
-					this.showDisplay();
-					this.disableSideBySideView();
-				}
+(function (mw) {
+        "use strict";
+        mw.dualScreen = mw.dualScreen || {};
+        mw.dualScreen.states = [
+            {
+                'name': 'PiP',
+                'initial': true,
+                'invoke': function (context) {
 
-				if (context.targetMainDisplayType && context.currentMainDisplayType !== context.targetMainDisplayType)
-				{
-					this.toggleMainDisplay();
-				}
+                    if (context.previousState !== 'PiP') {
+                        // make sure the player is setup to PiP mode
+                        this.enableUserActions();
+                        this.showDisplay();
+                        this.disableSideBySideView();
+                    }
 
-			}
-		},
-		{
-			'name': 'SbS',
-			'invoke' : function(context)
-			{
-				if (context.previousState !== 'SbS')
-				{
-					this.disableUserActions( );
-					this.showDisplay();
-					this.enableSideBySideView();
-				}
+                    if (context.targetMainDisplayType && context.currentMainDisplayType !== context.targetMainDisplayType) {
+                        // make sure the right display type is setup
+                        this.toggleMainDisplay();
+                    }
 
-				if (context.targetMainDisplayType && context.currentMainDisplayType !== context.targetMainDisplayType)
-				{
-					this.toggleSideBySideView();
-					this.toggleMainDisplay();
-				}
-			}
-		},
-		{
-			'name': 'hide',
-			'invoke' : function(context)
-			{
-				if (context.previousState !== 'hide')
-				{
-					this.disableUserActions( );
-					this.disableSideBySideView();
-					this.hideDisplay( );
-				}
+                }
+            },
+            {
+                'name': 'SbS',
+                'invoke': function (context) {
+                    if (context.previousState !== 'SbS') {
+                        // make sure the player is setup to SbS mode
+                        this.disableUserActions();
+                        this.showDisplay();
+                        this.enableSideBySideView();
+                    }
 
-				if (context.targetMainDisplayType && context.currentMainDisplayType !== context.targetMainDisplayType)
-				{
-					this.showDisplay( );
-					this.toggleMainDisplay();
-					this.hideDisplay( );
-				}
-			}
-		}
-	];
+                    if (context.targetMainDisplayType && context.currentMainDisplayType !== context.targetMainDisplayType) {
+                        // make sure the right display type is setup
+                        this.toggleSideBySideView();
+                        this.toggleMainDisplay();
+                    }
+                }
+            },
+            {
+                'name': 'hide',
+                'invoke': function (context) {
+                    if (context.previousState !== 'hide') {
+                        // make sure the player is setup to single mode
+                        this.disableUserActions();
+                        this.disableSideBySideView();
+                        this.hideDisplay();
+                    }
 
-	mw.dualScreen.nativeAppStates = [
-		{
-			'name': 'PiP',
-			'initial': true,
-			'invoke' : function(context)
-			{
-				if (context.previousState !== 'PiP')
-				{
-					this.enableUserActions();
-					this.showDisplay();
+                    if (context.targetMainDisplayType && context.currentMainDisplayType !== context.targetMainDisplayType) {
+                        // make sure the right display type is setup
+                        this.showDisplay();
+                        this.toggleMainDisplay();
+                        this.hideDisplay();
+                    }
+                }
+            }
+        ];
 
-					if (context.currentMainDisplayType !== 'video')
-					{
-						this.toggleMainDisplay();
-					}
-				}
-			}
-		},
-		{
-			'name': 'hide',
-			'invoke' : function(context)
-			{
-				if (context.previousState !== 'hide')
-				{
-					this.disableUserActions();
-					this.hideDisplay( );
-				}
+        mw.dualScreen.nativeAppStates = [
+            {
+                'name': 'PiP',
+                'initial': true,
+                'invoke': function (context) {
+                    if (context.previousState !== 'PiP') {
+                        // make sure the player is setup to PiP mode
+                        this.enableUserActions();
+                        this.showDisplay();
 
-				if (context.targetMainDisplayType && context.currentMainDisplayType !== context.targetMainDisplayType)
-				{
-					this.showDisplay( );
-					this.toggleMainDisplay();
-					this.hideDisplay( );
-				}
-			}
-		}
-	];
-}
+                        if (context.currentMainDisplayType !== 'video') {
+                            // make sure PiP show only video stream (in native app only video is supported in PiP)
+                            this.toggleMainDisplay();
+                        }
+                    }
+                }
+            },
+            {
+                'name': 'hide',
+                'invoke': function (context) {
+                    if (context.previousState !== 'hide') {
+                        // make sure the player is setup to single mode
+                        this.disableUserActions();
+                        this.hideDisplay();
+                    }
 
-)( window.mw );
+                    if (context.targetMainDisplayType && context.currentMainDisplayType !== context.targetMainDisplayType) {
+                        // make sure the right display type is setup
+                        this.showDisplay();
+                        this.toggleMainDisplay();
+                        this.hideDisplay();
+                    }
+                }
+            }
+        ];
+    })(window.mw);
