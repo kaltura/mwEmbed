f7eb8d82c8a565e4f8769f2ee2d49b7a388f6694
eitan <eitanavgil@gmail.com>
2016-03-10 17:03:04 -0500

    Moderation enhancement

--- modules/KalturaSupport/resources/uiConfComponents/moderationPlugin.js
+++ modules/KalturaSupport/resources/uiConfComponents/moderationPlugin.js
@@ -6,6 +6,7 @@
 			"parent": mw.isMobileDevice() ? 'topBarContainer' : 'controlsContainer',
 		 	"order": 62,
 		 	"displayImportance": "low",
+		 	"sendLabelWithComments": false ,
 		 	"align": "right",
 		 	"showTooltip": true,
 			"smartContainer": 'morePlugins',
@@ -18,7 +19,8 @@
 		 	"reasonSex": gM("ks-MODERATION-REASON-SEX"),
 		 	"reasonViolence": gM("ks-MODERATION-REASON-VIOLENCE"),
 		 	"reasonHarmful": gM("ks-MODERATION-REASON-HARMFUL"),
-		 	"reasonSpam": gM("ks-MODERATION-REASON-SPAM")
+		 	"reasonSpam": gM("ks-MODERATION-REASON-SPAM"),
+			"additionalReasons" : null
 		},
 
 		setup: function () {
@@ -68,14 +70,24 @@
 					.addClass( 'reportButton right' )
 					.text( gM("ks-MODERATION-SUBMIT") )
 					.click(function() {
+						var selectedIndex = $( '#flagType' ).val()-1;
 						_this.submitFlag({
+							'flagLabel': $("#flagType option:eq("+selectedIndex+")").text(),
 							'flagType': $( '#flagType' ).val(),
 							'flagComments': $( '#flagComments' ).val()
 						});
 					}) )
 			);
 
+
 			var $moderationScreen = $( '<div />' ).append($header, $moderationMessage );
+
+			if(_this.getConfig('additionalReasons') != null){
+				var reasons = _this.getConfig('additionalReasons').split(",");
+				for (var i=0; i< reasons.length ; i++) {
+					$moderationScreen.find("#flagType").append($( '<option />' ).attr( 'value', 5+i ).text(reasons[i]));
+				}
+			}
 
 			var closeCallback = function() {
 				// Enable space key binding
@@ -106,13 +118,17 @@
 			var _this = this;
 			this.getPlayer().triggerHelper( 'moderationSubmit', flagObj.flagType );
 			this.getPlayer().addPlayerSpinner();
+			var comments = flagObj.flagComments;
+			if(_this.getConfig("sendLabelWithComments") == true){
+				comments = flagObj.flagLabel+" - " + comments;
+			}
 			this.getKalturaClient().doRequest( {
 				'service' : 'baseentry',
 				'action' : 'flag',
 				'moderationFlag:objectType' : 'KalturaModerationFlag',
 				'moderationFlag:flaggedEntryId' : _this.getPlayer().kentryid,
 				'moderationFlag:flagType' : flagObj.flagType,
-				'moderationFlag:comments' : flagObj.flagComments
+				'moderationFlag:comments' : comments
 			}, function( data ) {
 				_this.getPlayer().hideSpinner();
 				var $flagScreen = $( '<div />' )
