d67040d5bc767e9727a2fd4f35bf5aea37db0892
amirch1 <amir.chervinsky@kaltura.com>
2016-03-13 15:09:31 +0200

    work against local library

--- modules/Chromecast/resources/receiver/mpl.js
+++ modules/Chromecast/resources/receiver/mpl.js
@@ -106,10 +106,6 @@ onload = function () {
 			customData = payload['value'];
 			setDebugMessage('customData', customData);
 		} else if (payload['type'] === 'load') {
-			mediaElement = document.getElementById('receiverVideoElement');
-			mediaElement.autoplay = false;
-			setMediaElementEvents(mediaElement);
-			mediaManager.setMediaElement(mediaElement);
 			setMediaManagerEvents();
 		} else if (payload['type'] === 'embed' && !playerInitialized) {
 			var publisherID = payload['publisherID'];
