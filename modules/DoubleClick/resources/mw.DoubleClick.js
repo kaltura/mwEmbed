3ca920f8a443fc050365398ed85954823a598871
amirch1 <amir.chervinsky@kaltura.com>
2016-04-07 10:01:44 +0300

    revert cache fix for ima3 loading

--- modules/DoubleClick/resources/mw.DoubleClick.js
+++ modules/DoubleClick/resources/mw.DoubleClick.js
@@ -363,9 +363,9 @@
 		loadIma:function( successCB, failureCB ){
 			var _this = this;
 			var isLoaded = false;
-			var timeoutVal = _this.getConfig("adsManagerLoadedTimeout") || 15000;
+			var timeoutVal = _this.getConfig("adsManagerLoadedTimeout") || 5000;
 			mw.log( "DoubleClick::loadIma: start timer for adsManager loading check: " + timeoutVal + "ms");
-			var imaLoaderTimeoutID = setTimeout(function(){
+			setTimeout(function(){
 				if ( !isLoaded ){
 					mw.log( "DoubleClick::loadIma: adsManager failed loading after " + timeoutVal + "ms");
 					failureCB();
@@ -376,23 +376,14 @@
 			if ( this.getConfig( 'debugMode' ) === true ){
 				imaURL =  '//s0.2mdn.net/instream/html5/ima3_debug.js';
 			}
-
-			$.ajax({
-				url: imaURL,
-				dataType: "script",
-				cache: true
-			})
-			.success(function (data) {
+			$.getScript( imaURL , function() {
 				isLoaded = true;
 				successCB();
-			})
-			.error(function( jqxhr, textStatus, errorCode ) {
-				failureCB( errorCode );
-			})
-			.always(function() {
-				clearTimeout(imaLoaderTimeoutID);
-			})
-
+			} )
+				.fail( function( jqxhr, settings, errorCode ) {
+					isLoaded = true;
+					failureCB( errorCode );
+				} );
 		},
 		startAdsManager: function(){
 			// Initialize the ads manager. In case of ad playlist with a preroll, the preroll will start playing immediately.
