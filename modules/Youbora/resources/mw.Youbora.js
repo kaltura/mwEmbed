866dd5e805832d0effc12a95f278df0976005385
Oren Me <oren.me@gmail.com>
2016-04-11 16:38:44 +0300

    OPF-4120 #comment Report play manifest URLs always
    MSE based playback holds blob urls in src so they aren’t useful anyhow

--- modules/Youbora/resources/mw.Youbora.js
+++ modules/Youbora/resources/mw.Youbora.js
@@ -419,11 +419,6 @@
 			return this.viewCode + "_" + this.viewIndex;
 		},
 		getCurrentVideoSrc: function(){
-			var vid = this.embedPlayer.getPlayerElement();
-			if( vid && vid.src ){
-				return vid.src;
-			}
-			// else just return the normal content source:
 			return this.embedPlayer.getSrc();
 		}
 
