6ef8dc27ac2a2f46fd125dd385159017340587be
Nissim Pardo <nissopa@gmail.com>
2016-04-13 17:01:27 +0300

    Revert "Revert "handle disconnect""
    This reverts commit 2fb94c74df808998dade97450bde84adeb72e17e.

--- modules/Chromecast/resources/chromecast.js
+++ modules/Chromecast/resources/chromecast.js
@@ -83,9 +83,7 @@
 				_this.onRequestSessionSuccess();
 			});
 			$( this.embedPlayer).bind('chromecastDeviceDisConnected', function(){
-				_this.getComponent().css("color","white");
-				_this.embedPlayer.disablePlayer();
-				_this.embedPlayer.updatePlaybackInterface()
+				_this.stopApp();
 			});
 
 			$( this.embedPlayer).bind('hideConnectingMessage', function(){
@@ -517,13 +515,16 @@
 			clearInterval(this.monitorInterval);
 			var _this = this;
 			var seekTime = this.getCurrentTime();
-			// stop casting
-			this.session.stop(this.onStopAppSuccess, this.onError);
 			this.getComponent().css("color","white");
 			this.updateTooltip(this.startCastTitle);
 			this.casting = false;
 			this.embedPlayer.casting = false;
 			this.embedPlayer.getInterface().find(".chromecastScreen").remove();
+			if (this.isNativeSDK){
+				return;
+			}
+			// stop casting
+			this.session.stop(this.onStopAppSuccess, this.onError);
 			// restore native player
 			this.embedPlayer.selectPlayer(this.savedPlayer);
 			this.savedPlayer = null;
