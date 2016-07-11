6ab29012442c26ad13ef1ae25800050ac73446e4
amirch1 <amir.chervinsky@kaltura.com>
2016-07-11 08:17:46 +0300

    FEC-5790 #comment if custom spinner is define make it override the mobile class settings

--- modules/MwEmbedSupport/jquery.loadingSpinner/jquery.loadingSpinner.js
+++ modules/MwEmbedSupport/jquery.loadingSpinner/jquery.loadingSpinner.js
@@ -72,7 +72,7 @@
 		// add color and shadow:
 		opts = $.extend({}, spinnerConfig, opts);
 
-		if (mw.getConfig("EmbedPlayer.EnableMobileSkin") && kWidget.isMobileDevice()){
+		if (mw.getConfig("EmbedPlayer.EnableMobileSkin") && kWidget.isMobileDevice() && !options){
 			opts["customSpinner"] = true;
 			opts.className = "customSpinner";
 		}
