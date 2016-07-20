0bf52b16aefb7fd424d3c8ea45a287c6170d4a28
Oren Me <oren.me@gmail.com>
2016-07-20 18:36:06 +0300

    Fix playhead update when seeking in paused state
    When seeking in paused state and the used player is flash-osmf HLS we
    don’t update the playhead correctly.
    Reason is that we are not getting the total bytes from player when
    entry first loads and then all percentage calculations are NaN.
    The bug was always there probably, but only when we fixed
    https://github.com/kaltura/mwEmbed/pull/2890 the issue started to make
    problems visible.
    FEC-5827

--- modules/EmbedPlayer/resources/mw.EmbedPlayer.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayer.js
@@ -1166,9 +1166,9 @@
 				this.hideSpinner();
 				// pause in a non-blocking call to avoid synchronous playing event
 				setTimeout(function () {
-					_this.updatePlayheadStatus();
 					_this.pause();
 					_this.stopMonitor();
+					_this.updatePlayheadStatus();
 				}, 0);
 			} else {
 				// continue to playback ( in a non-blocking call to avoid synchronous pause event )
