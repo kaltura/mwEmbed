1a28fd931a9830dc9c2337aafeb7c1f70ed1b3b0
Yair Ansbacher <yair.ansbacher@kaltura.com>
2016-08-03 12:33:46 +0300

    FEC-5861 #comment Do not generate abr source for smoothStream (#2924)

--- modules/KalturaSupport/resources/mw.KWidgetSupport.js
+++ modules/KalturaSupport/resources/mw.KWidgetSupport.js
@@ -1690,7 +1690,6 @@ mw.KWidgetSupport.prototype = {
 
 		}
 
-		//Only support ABR on-the-fly for DRM protected entries
 		if( mw.getConfig('Kaltura.UseFlavorIdsUrls') ) {
 			var validClipAspect = this.getValidAspect(deviceSources);
 			//Only add mpeg dash CENC on the fly if dash sources exist
@@ -1708,25 +1707,6 @@ mw.KWidgetSupport.prototype = {
 				});
 				this.attachFlavorAssetDrmData(dashSource, dashAdaptiveFlavors[0], flavorDrmData);
 				deviceSources.push(dashSource);
-			}
-			//Only add playready on the fly if pre-encrypted doesn't exist
-			if ((iphoneAdaptiveFlavors.length || ipadAdaptiveFlavors.length) &&
-				this.getSourcesByAttribute(deviceSources, "type", "video/playreadySmooth").length === 0) {
-				var targetFlavors = ipadAdaptiveFlavors.length ? ipadAdaptiveFlavors : iphoneAdaptiveFlavors;
-				var assetId = targetFlavors[0];
-				var ismSource = this.generateAbrSource({
-					entryId: asset.entryId,
-					flavorUrl: flavorUrl,
-					flavorId: "ism",
-					type: 'video/playreadySmooth',
-					flavors: targetFlavors,
-					format: "sl",
-					ext: "ism",
-					protocol: protocol,
-					clipAspect: validClipAspect
-				});
-				this.attachFlavorAssetDrmData(ismSource, assetId, flavorDrmData);
-				deviceSources.push(ismSource);
 			}
 		}
 
