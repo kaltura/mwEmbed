a7fce3a770ed7fb008d8afb5388884c08567a82f
yair.ansbacher <Yair.Ansbacher@kaltura.com>
2016-08-10 17:17:06 +0300

    FEC-5898 #comment Set hls.js as default plugin and allow disabling it using "disableHLSOnJs" parameter

--- modules/Hlsjs/resources/hlsjs.js
+++ modules/Hlsjs/resources/hlsjs.js
@@ -1,7 +1,7 @@
 ( function( mw, $ , Hls ) {"use strict";
 
 	//Currently use native support when available, e.g. Safari desktop and Edge
-	if (Hls.isSupported() && !mw.isDesktopSafari() && !mw.isEdge() && mw.getConfig("LeadWithHLSOnJs")) {
+	if (Hls.isSupported() && !mw.isDesktopSafari() && !mw.isEdge() && !mw.getConfig("disableHLSOnJs")) {
 		// Add HLS Logic player:
 		//Force HLS streamer type
 		mw.setConfig("streamerType", "hls");
