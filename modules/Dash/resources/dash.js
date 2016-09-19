e1dfdd84650ef16d00ff400378d3b405b8c31c3f
yair.ansbacher <Yair.Ansbacher@kaltura.com>
2016-09-19 15:14:48 +0300

    FEC-6102 Prevent loading manifest many times - register to  firstPlay in setup (one time) instead of in initShaka (many times in changeMedia scenario)

--- modules/Dash/resources/dash.js
+++ modules/Dash/resources/dash.js
@@ -3,10 +3,7 @@
 	if (!window.Promise) {
 		shaka.polyfill.installAll();
 	}
-	if (shaka.Player.isBrowserSupported() &&
-		!mw.getConfig( "EmbedPlayer.ForceNativeComponent" ) &&
-		!mw.isDesktopSafari() &&
-		!mw.isAndroid()) {
+	if (shaka.Player.isBrowserSupported() && !mw.getConfig("EmbedPlayer.ForceNativeComponent") && !mw.isDesktopSafari() && !mw.isAndroid()) {
 		$(mw).bind('EmbedPlayerUpdateMediaPlayers', function (event, mediaPlayers) {
 			mw.log("Dash::Register shaka player for application/dash+xml mime type");
 			var shakaPlayer = new mw.MediaPlayer('shakaPlayer', ['application/dash+xml'], 'Native');
@@ -20,9 +17,6 @@
 
 			/** type {boolean} */
 			LoadShaka: false,
-
-			/** type {boolean} */
-			manifestLoadingStart: false,
 
 			currentBitrate: null,
 			/**
@@ -44,6 +38,7 @@
 			addBindings: function () {
 				this.bind("SourceChange", this.isNeeded.bind(this));
 				this.bind("playerReady", this.initShaka.bind(this));
+				this.bind("firstPlay", this.onFirstPlay.bind(this));
 				this.bind("seeking", this.onSeekBeforePlay.bind(this));
 				this.bind("switchAudioTrack", this.onSwitchAudioTrack.bind(this));
 				this.bind("selectClosedCaptions", this.onSwitchTextTrack.bind(this));
@@ -90,11 +85,6 @@
 					window.VTTCue = this.getPlayer().getOriginalVTTCue();
 
 					this.registerShakaEvents();
-
-					this.bind("firstPlay", function(){
-						this.unbind("seeking");
-						this.loadManifest();
-					}.bind(this));
 				}
 			},
 
@@ -137,31 +127,33 @@
 				return drmConfig;
 			},
 
-			registerShakaEvents: function(){
+			registerShakaEvents: function () {
 				player.addEventListener('error', this.onErrorEvent.bind(this));
 				player.addEventListener('adaptation', this.onAdaptation.bind(this));
+			},
+
+			onFirstPlay: function () {
+				this.unbind("seeking");
+				this.loadManifest();
 			},
 
 			loadManifest: function () {
 				var _this = this;
 				var selectedSource = this.getPlayer().getSrc();
-				if(!this.manifestLoadingStart){
-					this.manifestLoadingStart = true;
-					this.log('Loading manifest started');
-					this.getPlayer().resolveSrcURL(selectedSource)
-						.done(function (manifestSrc) {  // success
-							selectedSource = manifestSrc;
-						})
-						.always(function () {  // both success or error
-								// Try to load a manifest.
-								player.load(selectedSource).then(function () {
-									// This runs if the asynchronous load is successful.
-									_this.log('Loading manifest ended');
-									_this.addTracks();
-								}).catch(_this.onError.bind(_this));  // onError is executed if the asynchronous load fails.
-							}
-						);
-				}
+				this.log('Loading manifest started');
+				this.getPlayer().resolveSrcURL(selectedSource)
+					.done(function (manifestSrc) {  // success
+						selectedSource = manifestSrc;
+					})
+					.always(function () {  // both success or error
+							// Try to load a manifest.
+							player.load(selectedSource).then(function () {
+								// This runs if the asynchronous load is successful.
+								_this.log('Loading manifest ended');
+								_this.addTracks();
+							}).catch(_this.onError.bind(_this));  // onError is executed if the asynchronous load fails.
+						}
+					);
 			},
 
 			addTracks: function () {
@@ -258,9 +250,9 @@
 						player.selectTrack(selectedAbrTrack, false);
 						this.getPlayer().triggerHelper("sourceSwitchingStarted", this.currentBitrate);
 						var _this = this;
-						setTimeout(function(){
+						setTimeout(function () {
 							_this.getPlayer().triggerHelper("sourceSwitchingEnd", Math.round(source.getBitrate()));
-						},1000);
+						}, 1000);
 						mw.log("Dash::switchSrc to ", selectedAbrTrack);
 					}
 				} else { // "Auto" option is selected
@@ -321,7 +313,7 @@
 				}
 			},
 
-			onSeekBeforePlay: function(){
+			onSeekBeforePlay: function () {
 				this.unbind("seeking");
 				this.unbind("firstPlay");
 				this.loadManifest();
@@ -340,7 +332,7 @@
 			 */
 			onError: function (event, data) {
 				var errorData = data ? data.type + ", " + data.details : event;
-				mw.log("Dash::Error: " , errorData);
+				mw.log("Dash::Error: ", errorData);
 			},
 
 			/**
@@ -352,19 +344,18 @@
 					this.LoadShaka = false;
 					this.loaded = false;
 					this.currentBitrate = null;
-					this.manifestLoadingStart = false;
 					player.destroy();
 					this.restorePlayerMethods();
 				}
 			},
 
-			onAdaptation: function(){
+			onAdaptation: function () {
 				var selectedAbrTrack = this.getTracksByType("video").filter(function (abrTrack) {
 					return abrTrack.active;
 				})[0];
-				if(selectedAbrTrack){
+				if (selectedAbrTrack) {
 					var currentBitrate = Math.round(selectedAbrTrack.bandwidth / 1024);
-					if(this.currentBitrate !== currentBitrate){
+					if (this.currentBitrate !== currentBitrate) {
 						this.currentBitrate = currentBitrate;
 						this.embedPlayer.triggerHelper('bitrateChange', currentBitrate);
 						this.log('The bitrate has changed to ' + currentBitrate);
@@ -406,7 +397,7 @@
 		var playerConfig = window.kalturaIframePackageData.playerConfig;
 		if (playerConfig && playerConfig.plugins && !playerConfig.plugins["dash"]) {
 			playerConfig.plugins["dash"] = {
-				plugin : true
+				plugin: true
 			};
 			mw.setConfig('KalturaSupport.PlayerConfig', playerConfig);
 		}
