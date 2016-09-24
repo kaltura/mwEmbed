3508caebccc45fe3405f0f3d0cf1a7829a9b9085
Oren Me <oren.me@gmail.com>
2016-09-24 20:25:35 +0300

    Bump version v2.48.1

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -20,7 +20,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.48';
+$wgMwEmbedVersion = '2.48.1';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
