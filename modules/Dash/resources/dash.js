f55e19a0dc6cf331be3d57225d98741f68ac88e9
yair.ansbacher <Yair.Ansbacher@kaltura.com>
2016-09-25 12:06:46 +0300

    FEC-6129 #comment Re enabling shaka on android This reverts commit 0c75d514d133bab86dfb08c4aef998cc026c754a.

--- modules/Dash/resources/dash.js
+++ modules/Dash/resources/dash.js
@@ -5,8 +5,7 @@
 	}
 	if (shaka.Player.isBrowserSupported() &&
 		!mw.getConfig( "EmbedPlayer.ForceNativeComponent" ) &&
-		!mw.isDesktopSafari() &&
-		!mw.isAndroid()) {
+		!mw.isDesktopSafari()) {
 		$(mw).bind('EmbedPlayerUpdateMediaPlayers', function (event, mediaPlayers) {
 			mw.log("Dash::Register shaka player for application/dash+xml mime type");
 			var shakaPlayer = new mw.MediaPlayer('shakaPlayer', ['application/dash+xml'], 'Native');
