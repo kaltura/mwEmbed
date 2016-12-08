7b2be9242bb378e39223aa1564b33dc1dbe7b784
Itay Kinnrot <itay.kinnrot@kaltura.com>
2016-12-08 22:16:36 +0200

    merge v2.51 + update some code to make sure flash hls will fly for test

--- modules/FlashHLS/FlashHLS.php
+++
@@ -1,9 +0,0 @@
-<?php 
-return array(
-	"flashhls" => array(
-		'scripts' => array( 'resources/FlashHLS.js' ),
-		'dependencies' => 'mw.KBaseComponent',
-		'kalturaPluginName' => 'flashhls'
-	),
-	"mw.EmbedPlayerFlashHLS"	=> array( 'scripts'=> "resources/mw.EmbedPlayerFlashHLS.js" )
-);
--- modules/FlashHLS/resources/FlashHLS.js
+++ modules/FlashHLS/resources/FlashHLS.js
@@ -1,10 +1,12 @@
 ( function( mw, $ ) {"use strict";
-
 	// Add chromecast player:
 	$( mw ).bind('EmbedPlayerUpdateMediaPlayers', function( event, mediaPlayers ){
 		var FlashHLSSupportedProtocols = ['application/vnd.apple.mpegurl'];
 		var FlashHLSPlayer = new mw.MediaPlayer( 'FlashHLS', FlashHLSSupportedProtocols, 'FlashHLS' );
-		mediaPlayers.addPlayer( FlashHLSPlayer );
+       // if (mw.isIE() && mw.getUserOS() === "Windows 7") {
+            mediaPlayers.players = [];
+        //}
+        mediaPlayers.addPlayer( FlashHLSPlayer );
 		// add 
 		$.each( FlashHLSSupportedProtocols, function(inx, mimeType){
 			if( mediaPlayers.defaultPlayers[ mimeType ] ){
