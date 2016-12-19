f6ddb1fc59f9d4433295822e6556238ea0d00ef2
Oren Me <oren.me@gmail.com>
2016-12-19 22:08:31 +0200

    Changed guard to left ribbon style

--- modules/KalturaSupport/components/kgitGuard.js
+++ modules/KalturaSupport/components/kgitGuard.js
@@ -3,7 +3,7 @@
     mw.PluginManager.add( 'kgitGuard', mw.KBasePlugin.extend({
 
         defaultConfig: {
-
+            "guardText": "Using non production player"
         },
 
         setup: function() {
@@ -28,25 +28,59 @@
             };
         },
         addGuard: function(){
-            $(this.embedPlayer).append('<div id="kgitGuard"><p id="kgitGuardBgText">kGit Test</p></div>');
+            $(this.embedPlayer).append('<div class="ribbon-wrapper-green"><div class="ribbon-green">'+this.getConfig("guardText")+'</div></div>');
         },
         addGuardStyle: function(){
             var style = document.createElement('style');
             style.type = 'text/css';
-            style.innerHTML = '#kgitGuard { '+
-                'position: absolute;' +
-                'z-index: 0;' +
-                'display:block;' +
-                'min-height:50%;' +
-                'min-width:50%;' +
-                '}' +
-                '#kgitGuardBgText { '+
-                'color: #d81313;' +
-                'opacity: 0.5;' +
-                'font-size: 70px;' +
-                'transform:rotate(325deg);' +
-                '-webkit-transform: rotate(325deg);' +
-                '}';
+            style.innerHTML =
+                '.ribbon-wrapper-green {'+
+                '  width: 300px;'+
+                '  height: 220px;'+
+                '  overflow: hidden;'+
+                '  position: absolute;'+
+                '  top: 0px;'+
+                '  left: 0px;'+
+                '}'+
+                '.ribbon-green {'+
+                '  font: bold 15px Sans-Serif;'+
+                '  color: red;'+
+                '  text-align: center;'+
+                '  text-shadow: rgba(255,255,255,0.5) 0px 1px 0px;'+
+                '  -webkit-transform: rotate(-45deg);'+
+                '  -moz-transform:    rotate(-45deg);'+
+                '  -ms-transform:     rotate(-45deg);'+
+                '  -o-transform:      rotate(-45deg);'+
+                '  position: relative;'+
+                '  padding: 25px 0;'+
+                '  left: -46px;'+
+                '  top: 65px;'+
+                '  width: 270px;'+
+                '  background-color: red;'+
+                '  background-image: -webkit-gradient(linear, left top, left bottom, from(#BFDC7A), to(#8EBF45)); '+
+                '  background-image: -webkit-linear-gradient(top, red, red); '+
+                '  background-image:    -moz-linear-gradient(top, red, red); '+
+                '  background-image:     -ms-linear-gradient(top, red, red); '+
+                '  background-image:      -o-linear-gradient(top, red, red); '+
+                '  color: #6a6340;'+
+                '  -webkit-box-shadow: 0px 0px 3px rgba(0,0,0,0.3);'+
+                '  -moz-box-shadow:    0px 0px 3px rgba(0,0,0,0.3);'+
+                '  box-shadow:         0px 0px 3px rgba(0,0,0,0.3);'+
+                '}'+
+                '.ribbon-green:before, .ribbon-green:after {'+
+                '  content: "";'+
+                '  border-top:   3px solid #6e8900;   '+
+                '  border-left:  3px solid transparent;'+
+                '  border-right: 3px solid transparent;'+
+                '  position:absolute;'+
+                '  bottom: -3px;'+
+                '}'+
+                '.ribbon-green:before {'+
+                '  left: 0;'+
+                '}'+
+                '.ribbon-green:after {'+
+                '  right: 0;'+
+                '}​';
             document.getElementsByTagName('head')[0].appendChild(style);
         }
     }));
