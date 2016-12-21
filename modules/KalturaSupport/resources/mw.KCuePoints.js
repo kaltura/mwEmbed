a0eca1393d5b1f72d9c05d0ffbf4258b6de3138f
Oren Me <oren.me@gmail.com>
2016-12-21 10:21:04 +0200

    Set correct thumbnailUrl from server response

--- modules/KalturaSupport/resources/mw.KCuePoints.js
+++ modules/KalturaSupport/resources/mw.KCuePoints.js
@@ -120,8 +120,9 @@
 							'id': item.assetId
 						}
 					);
+					responseArray.push(item);
 				}
-				responseArray[index] = item;
+
 			});
 
 			if (requestArray.length) {
@@ -131,14 +132,18 @@
 					if (requestArray.length === 1){
 						data = [data];
 					}
-					$.each(data, function (index, res) {
-						if (!_this.isValidResult(res)) {
-							data[index] = null;
+					$.each(data, function (index, thumbnailUrl) {
+						if (_this.isValidResult(thumbnailUrl)) {
+							var resItem = responseArray[index];
+							if (resItem){
+								resItem.thumbnailUrl = thumbnailUrl;
+								if (loadThumbnailWithReferrer){
+									resItem.thumbnailUrl += '?options:referrer=' + referrer;
+								}
+							}
 						}
 					});
-					$.each(thumbCuePoint, function (index, item) {
-						item.thumbnailUrl = loadThumbnailWithReferrer ? data[index] + '?options:referrer=' + referrer : data[index];
-					});
+
 					if (callback) {
 						callback();
 					}
