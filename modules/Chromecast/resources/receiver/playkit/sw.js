0faecb7d0ae2b5b8b69e1ae6746073c2bd5ee259
Dan Ziv <dan.ziv@kaltura.com>
2017-02-01 17:50:18 +0200

    ttl for responses

--- modules/Chromecast/resources/receiver/playkit/sw.js
+++ modules/Chromecast/resources/receiver/playkit/sw.js
@@ -1,16 +1,20 @@
 var CACHE_NAME = 'kaltura-chromecast-receiver-cache';
-
+var TTL_GAP = 10;
 
 self.addEventListener( 'fetch', function ( event ) {
-    console.log( '[ServiceWorker] fetch' );
+    console.log( 'ServiceWorker:: fetch' );
 
     event.respondWith(
         caches.match( event.request )
             .then( function ( response ) {
                 // Cache hit - return response
                 if ( response ) {
-                    console.log( "[ServiceWorker] Cache hit - return response", event.request );
-                    return response;
+                    var now = new Date();
+                    if ( (response.TTL - now) > 0 ) {
+                        console.log( "ServiceWorker:: Cache hit - return response", response );
+                        return response;
+                    }
+                    console.log( "ServiceWorker:: Response in cache buy ttl is past", response );
                 }
 
                 // IMPORTANT: Clone the request. A request is a stream and
@@ -23,6 +27,7 @@ self.addEventListener( 'fetch', function ( event ) {
                     function ( response ) {
                         // Check if we received a valid response
                         if ( !response || response.status !== 200 || response.type !== 'basic' ) {
+                            console.log( "ServiceWorker:: Response isn't valid", response );
                             return response;
                         }
 
@@ -31,12 +36,16 @@ self.addEventListener( 'fetch', function ( event ) {
                         // as well as the cache consuming the response, we need
                         // to clone it so we have two streams.
                         var responseToCache = response.clone();
+                        var ttlValue = new Date();
+                        ttlValue.setDate( ttlValue.getDate() + TTL_GAP );
+                        responseToCache.TTL = ttlValue;
+                        console.log( "ServiceWorker:: Caching response...", responseToCache );
 
                         caches.open( CACHE_NAME )
                             .then( function ( cache ) {
                                 cache.put( event.request, responseToCache );
+                                console.log( "ServiceWorker:: Response cached!", responseToCache );
                             } );
-
                         return response;
                     }
                 );
--- modules/Chromecast/resources/receiver/playkit/swLoader.js
+++ modules/Chromecast/resources/receiver/playkit/swLoader.js
@@ -1,11 +1,12 @@
 if ( 'serviceWorker' in navigator ) {
     window.addEventListener( 'load', function () {
-        navigator.serviceWorker.register( 'sw.js' ).then( function ( registration ) {
-            // Registration was successful
-            console.log( 'ServiceWorker registration successful with scope: ', registration.scope );
-        } ).catch( function ( err ) {
-            // registration failed :(
-            console.log( 'ServiceWorker registration failed: ', err );
+        navigator.serviceWorker.register( 'sw.js' )
+            .then( function ( registration ) {
+                // Registration was successful
+                console.log( 'ReceiverServiceWorker:: registration successful with scope: ', registration.scope );
+            } ).catch( function ( err ) {
+            // registration failed
+            console.log( 'ServiceWorker:: registration failed: ', err );
         } );
     } );
 }
