781aebcde2c853e49ec2f88dd08300d906f55217
Dan Ziv <dan.ziv@kaltura.com>
2017-02-02 17:00:01 +0200

    test with sw from root

--- modules/Chromecast/resources/receiver/playkit/swLoader.js
+++ modules/Chromecast/resources/receiver/playkit/swLoader.js
@@ -1,6 +1,6 @@
 if ( 'serviceWorker' in navigator ) {
     window.addEventListener( 'load', function () {
-        navigator.serviceWorker.register( 'sw.js' )
+        navigator.serviceWorker.register( '../../../../../sw.js' )
             .then( function ( registration ) {
                 // Registration was successful
                 console.log( 'ReceiverServiceWorker:: registration successful with scope: ', registration.scope );
