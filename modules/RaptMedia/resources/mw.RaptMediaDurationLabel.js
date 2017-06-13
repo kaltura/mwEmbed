b5e5c21bc8fb75dd6515f66754c077e03aaf765a
Hargobind S. Khalsa <hargo@raptmedia.com>
2017-06-13 17:03:14 +0000

    [Rapt Media] Integrate Rapt API
    - Expose the Rapt Player API via the Kaltura Player API
    - Refactor Rapt Media Plugin to support faster initialization and
    prevent double initialization
    - Misc refactoring and bugfixes

--- modules/RaptMedia/resources/mw.RaptMediaDurationLabel.js
+++ modules/RaptMedia/resources/mw.RaptMediaDurationLabel.js
@@ -3,55 +3,40 @@
  * With RaptMediaDurationLabel plugin the time label can interact within the context of a single RaptMedia clip instead of just the entire stitched playlist.
  * This plugin is only activated when the entryId provided is a Playlist Entry with partnerData == "raptmedia;projectId".
  *
- * See the RaptMedia plugin for more information. 
+ * See the RaptMedia plugin for more information.
  */
 (function ( mw, $ ) {
 	"use strict";
-	mw.PluginManager.add('raptMediaDurationLabel', mw.PluginManager.getClass('durationLabel').extend({	
-		
+	mw.PluginManager.add('raptMediaDurationLabel', mw.PluginManager.getClass('durationLabel').extend({
+
 		setup: function () {
 			this._super();
-			
+			this.addBindings();
+		},
+
+		addBindings: function() {
 			var _this = this;
-			this.bind('mediaLoaded', function(event) { 
-				_this.setupRapt();
+
+			this.bind('raptMedia_newSegment', function(e, segment) {
+				_this.segmentDuration = segment.duration;
+				_this.updateUI();
 			});
 
-			this.setupRapt();
+			this.bind('raptMedia_cleanup', function(e) {
+				_this.segmentDuration = null;
+				_this.updateUI();
+			});
 		},
 
-		setupRapt: function (){
+		updateUI: function(duration) {
+			this._super(
+				Math.floor(
+					this.segmentDuration ||
+					duration ||
+					this.currentDuration
+				)
+			);
+		}
 
-			var partnerData = this.getPlayer().evaluate('{mediaProxy.entry.partnerData}');
-			this.raptMediaPlaylistEntry = (partnerData != null && partnerData.indexOf("raptmedia") > -1);
-			
-			var _this = this;
-
-			if (this.raptMediaPlaylistEntry == true) {
-				this.raptCurrentSegment = null;
-				this.unbind('durationChange');
-				this.bind('raptMedia_newSegment', function(e, raptEngineCurrentSegment) {
-					_this.raptCurrentSegment = raptEngineCurrentSegment;
-					_this.updateUI( _this.raptDuration() );
-				});
-			} else {
-				this.raptCurrentSegment = null;
-				this.unbind('raptMedia_newSegment');
-				this.bind( 'durationChange', function(event, duration){
-					if( !_this.getPlayer().isInSequence() ) {
-						_this.contentDuration = duration;
-						_this.updateUI( Math.floor(duration) );
-					}
-				});
-			}
-		},
-
-		raptDuration: function () {
-			var currentDuration = this.getPlayer().getDuration();
-			if (this.raptCurrentSegment != null) 
-				currentDuration = this.raptCurrentSegment.msDuration / 1000;
-			return currentDuration;
-		},
-		
 	} ) );
-} ) ( window.mw, window.jQuery );	
+} ) ( window.mw, window.jQuery );
