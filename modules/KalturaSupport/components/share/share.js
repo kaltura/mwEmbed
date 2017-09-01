35fb94fc6e9f22ad16e6f4d5f74376abaa6a3bf0
Maksym Korniiov <m.korniiov@synergetica.net>
2017-09-01 16:48:07 +0300

    FEV-60 Ensure keyboard focus returns properly from dialogs
    also fixed close button by press enter in share screen

--- modules/KalturaSupport/components/share/share.js
+++ modules/KalturaSupport/components/share/share.js
@@ -400,6 +400,10 @@
 			// close button override
 			$(".share .icon-close").on("mousedown", function(e){
 				_this.closeScreen();
+            }).keyup(function (e) {
+                if(e.keyCode === 13){
+                    _this.closeScreen();
+                }
 			});
 
 		},
