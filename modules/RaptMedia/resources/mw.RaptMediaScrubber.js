5897d96c2a8b264f8ae8b2b546f9d5ba74fac5c8
Hargobind S. Khalsa <khalsah@gmail.com>
2017-09-10 07:20:39 -0600

    feat: Rapt Media revisions and API features (#3513)
    * [Rapt Media] Integrate Rapt API
    
    - Expose the Rapt Player API via the Kaltura Player API
    - Refactor Rapt Media Plugin to support faster initialization and
    prevent double initialization
    - Misc refactoring and bugfixes
    
    * [Rapt Media] Fix plugin dependencies in manifest
    
    * [Rapt Media] Cleanup player config in test file
    
    * [Rapt Media] Customize uncertainty for MPEG-DASH
    
    MPEG-DASH handles timing in stitched playlists better, allowing us to
    reduce the amount of "uncertainty" applied to segment start and end
    times when it is being used for stitched playlist streaming.
    
    * [Rapt Media] Mitigate safari seek weirdness
    
    * Override scrubber thumbnail logic consider rapt segments
    
    * [Rapt Media] Replace const with var
    
    * [Rapt Media] Cleanup from PR feedback
    
    * [Rapt Media] Use `checkPlayerSourcesEvent` for async initalization
    
    * [Rapt Media] Mitigate segment looping bug on slow/throttled CPU
    
    This doesn't completely solve the issue, but instead of looping the
    segment the player usually stutters/replays the last small bit of the
    segment before eventually managing to pause.
    
    * [Rapt Media] Fix an initialization event race condition
    
    Handle the situation where the `checkPlayerSourcesEvent` handler is
    called _after_ the `KalturaSupport_EntryDataReady` handler.
    
    * [Rapt Media] Use pessimistic default for timing uncertainy
    
    * [Rapt Media] Override player.seek() to seek within current segment

--- modules/RaptMedia/resources/mw.RaptMediaScrubber.js
+++ modules/RaptMedia/resources/mw.RaptMediaScrubber.js
@@ -3,188 +3,91 @@
  * With raptMediaScrubberPlugin the scrubber can interact within the context of a single RaptMedia clip instead of just the entire stitched playlist.
  * This plugin is only activated when the entryId provided is a Playlist Entry with partnerData == "raptmedia;projectId".
  *
- * See the RaptMedia plugin for more information. 
+ * See the RaptMedia plugin for more information.
  */
 (function ( mw, $ ) {
 	"use strict";
 	mw.PluginManager.add('raptMediaScrubber', mw.PluginManager.getClass('scrubber').extend({
-		
-		setup: function (){
-			this._super();
-		},
-
-		addBindings: function() {
-			this._super();
-
-			var _this = this;
-
-			this.bind('mediaLoaded', function(event) { 
-				_this.raptCleanup();
-				var partnerData = _this.getPlayer().evaluate('{mediaProxy.entry.partnerData}');
-				_this.raptMediaPlaylistEntry = (partnerData != null && partnerData.indexOf("raptmedia") > -1);
-
-				if (_this.raptMediaPlaylistEntry == true) {
-					//rapt entry
-					_this.addBindingsRapt();
-				} else {
-					//regular entry
-				}
-			});
-		},
-
-		raptCleanup: function () {
-			this.unbind('durationChange');
-			this.unbind('monitorEvent');
-			this.unbind('raptMedia_newSegment');
-			this.unbind('raptMedia_pausedDecisionPoint');
-			this.raptCurrentSegment = null;
-			this.raptMediaPlaylistEntry = false;
-		},
-
-		addBindingsRapt: function () {
-			var _this = this;
-			
-			this.bind('raptMedia_newSegment', function(e, raptEngineCurrentSegment) {
-				_this.raptCurrentSegment = raptEngineCurrentSegment;
-				_this.duration = _this.raptDuration();
-			});
-			this.bind('durationChange', function (event, duration) {
-                if (_this.getPlayer().userSlide) return;
-                _this.duration = _this.raptDuration();
-			});
-			this.bind('monitorEvent', function () {
-				if (_this.raptMediaPlaylistEntry && _this.raptCurrentSegment && !_this.getPlayer().userSlide && !_this.sliderUpdating) {
-					var currentTime = _this.getPlayer().currentTime - (_this.raptCurrentSegment.msStartTime / 1000);
-	                _this.updatePlayheadUI(currentTime / _this.raptDuration() * 1000);
-	            }
-			});
-			this.bind('raptMedia_pausedDecisionPoint', function () {
-				_this.updatePlayheadUI(1000);
-			});
-		},
-
-		updatePlayheadPercentUI: function (perc) {
-			if (this.raptMediaPlaylistEntry) 
-				return;
-			this._super(perc);
-		},
-
-		raptDuration: function () {
-			if (!this.raptMediaPlaylistEntry) 
-				return this.getPlayer().getDuration();
-			else if (this.raptCurrentSegment != null) 
-				return parseFloat((this.raptCurrentSegment.msDuration / 1000).toFixed(2));
-			else
-				return 0;
-		},
-
-		getSliderConfig: function () {
+		getSliderConfig: function() {
 			var _this = this;
 			var embedPlayer = this.getPlayer();
-			
-			return {
-				range: "min",
-				value: 0,
-				min: 0,
-				max: 1000,
-				// we want less than monitor rate for smoth animation
-				animate: 10, //mw.getConfig('EmbedPlayer.MonitorRate') - ( mw.getConfig('EmbedPlayer.MonitorRate') / 5 ),
-				start: function (event, ui) {
-					embedPlayer.userSlide = true;
-					// Release the mouse when player is not focused
-					$(embedPlayer).one('hidePlayerControls onFocusOutOfIframe', function () {
-						$(document).trigger('mouseup');
-					});
-				},
-				slide: function (event, ui) {
-					_this.updateAttr(ui);
-				},
-				change: function (event, ui) {
-					if (!_this.raptMediaPlaylistEntry) { // regular entry:
-						var seekTime = (ui.value / 1000) * embedPlayer.getDuration();
-						// always update the title
-						_this.updateAttr(ui);
-						// Only run the onChange event if done by a user slide
-						// (otherwise it runs times it should not)
-						if (embedPlayer.userSlide) {
-							embedPlayer.userSlide = false;
-							embedPlayer.seeking = true;
-							embedPlayer.triggerHelper("userInitiatedSeek", seekTime);
-							embedPlayer.seek(seekTime);
-						}
-					} else { // raptmedia entry:
-						// Only run the onChange event if done by a user slide
-						// (otherwise it runs times it should not)
-						if (!embedPlayer.userSlide || _this.sliderUpdating || !_this.raptCurrentSegment) return;
 
-						var segmentSeekTime = (ui.value / 1000) * _this.raptDuration();
-						var endOfSegment = _this.isEndOfCurrentSegment(segmentSeekTime);
-						
-						var seekDuringPlay = embedPlayer.isPlaying() && !endOfSegment;
-						if (seekDuringPlay) embedPlayer.stopEventPropagation();
+			var config = this._super();
 
-						_this.sliderUpdating = true;
-						var segmentStartOffset = (_this.raptCurrentSegment != null) ? parseFloat((_this.raptCurrentSegment.msStartTime / 1000).toFixed(2)) : 0;
-						var seekTime = segmentSeekTime + segmentStartOffset;
-						//use 350ms from end of segment as saftey buffer to avoid missing the end due to misaligned frame rates or slow event propagations
-						var seekTimeEndOfSeg = segmentStartOffset + parseFloat(((_this.raptCurrentSegment.msDuration-50)/1000).toFixed(2));
+			var originalChange = config.change;
+			config.change = function(event, ui) {
+				var status = embedPlayer.evaluate('{raptMedia.status}');
+				var segment = embedPlayer.evaluate('{raptMedia.currentSegment}');
 
-						_this.updateAttr(ui);
+				if (status !== 'enabled') {
+					return originalChange(event, ui);
+				}
 
-						if (endOfSegment) {
-							embedPlayer.triggerHelper("userInitiatedSeek", seekTimeEndOfSeg);
-							embedPlayer.stopPlayAfterSeek = true;
-							embedPlayer.seek(seekTimeEndOfSeg, true);
-							_this.log('user seeked to end of segment');
-						} else {
-							embedPlayer.triggerHelper("userInitiatedSeek", seekTime);
-							embedPlayer.stopPlayAfterSeek = !seekDuringPlay;
-							embedPlayer.seek(seekTime, !seekDuringPlay);
-						}
-						embedPlayer.userSlide = false;
-						_this.sliderUpdating = false;
+				if (segment == null) {
+					_this.log('Warning: missing segment data');
+					return;
+				}
 
-						var _embedPlayer = embedPlayer;
-						if (!seekDuringPlay) {
-							setTimeout(function () {
-								_embedPlayer.triggerHelper('doPause');
-							}, 0);
-						} else {
-							setTimeout(function () {
-								_embedPlayer.restoreEventPropagation();
-							}, 0);
-						}
-					}
+				_this.updateAttr(ui);
+
+				var seekTime = (ui.value / 1000) * segment.duration;
+				if (embedPlayer.userSlide) {
+					embedPlayer.userSlide = false;
+					embedPlayer.seeking = true;
+					embedPlayer.triggerHelper("userInitiatedSeek", seekTime);
+					embedPlayer.triggerHelper("raptMedia_doSeek", seekTime);
 				}
 			};
+
+			return config;
 		},
 
-		isEndOfCurrentSegment: function (segTime) {
-			var seekTimeMillis = parseFloat(segTime.toFixed(3)) * 1000;
-			if (seekTimeMillis < 0) seekTimeMillis = 0;
-			var segmentDurationMillis = this.raptCurrentSegment.msDuration - 550;
-			var isSegmentEnd = (seekTimeMillis >= segmentDurationMillis);
-			return isSegmentEnd;
+		showThumbnailPreview: function(data) {
+			var embedPlayer = this.getPlayer();
+			var status = embedPlayer.evaluate('{raptMedia.status}');
+			var segment = embedPlayer.evaluate('{raptMedia.currentSegment}');
+
+			if (status !== 'enabled') {
+				this._super(data);
+			}
+
+			if (segment == null) {
+				this.log('Warning: missing segment data');
+				return;
+			}
+
+			var original = data.val;
+
+			// Update thumbnail
+			data.val = data.val * (segment.duration / this.duration) + 1000 * (segment.startTime / this.duration);
+			this._super(data);
+
+			// Overwrite timestamp
+			var $sliderPreviewTime = this.getComponent().find(".sliderPreview .sliderPreviewTime");
+			$sliderPreviewTime.text(kWidget.seconds2npt(original / 1000 * this.getDuration()));
 		},
 
-		updatePlayheadUI: function (val) {
-			this._super(val);
-		},
-
-		updateAttr: function (ui) {
+		updateAttr: function(ui) {
 			var perc = ui.value / 1000;
-			var $slider = this.$el.find('.ui-slider-handle');
-			var title = mw.seconds2npt(perc * this.raptDuration());
+			var $slider = this.$el;
+			var title = mw.seconds2npt(perc * this.getDuration());
+			var totalDuration = mw.seconds2npt(this.getDuration());
 			var attributes = {
 				'data-title': title,
-				'aria-valuetext': mw.seconds2npt(perc * this.raptDuration()),
+				'aria-valuetext': title + " of " + totalDuration,
 				'aria-valuenow': parseInt(perc * 100) + '%'
 			};
 			$slider.attr(attributes);
-			if (this.getConfig('accessibilityLabels')) {
-				$slider.html('<span class="accessibilityLabel">' + title + '</span>');
-			}
 		},
-		
+
+		getDuration: function(ui) {
+			var player = this.getPlayer();
+
+			if (player.evaluate('{raptMedia.status}') === 'enabled') {
+				return player.evaluate('{raptMedia.currentSegment.duration}');
+			} else {
+				return player.getDuration();
+			}
+		}
 	} ) );
-} ) ( window.mw, window.jQuery );	
+} ) ( window.mw, window.jQuery );
