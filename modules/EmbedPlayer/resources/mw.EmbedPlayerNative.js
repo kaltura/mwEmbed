bc36659ef48d6ed80a383f2f02eeaad555d81e18
Oren Me <oren.me@gmail.com>
2017-09-26 14:28:59 +0300

    feat: add play promise to handle play rejection (#3552)
    (cherry picked from commit 9ebdf91)

--- modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
@@ -960,7 +960,16 @@
 
 						// issue a play request
 						if (!_this.playing) {
-							vid.play();
+							var playPromise = vid.play();
+                            if (playPromise !== undefined) {
+                                playPromise.then(function() {
+                                    mw.log("play promise resolved");
+                                }).catch(function(error) {
+                                    mw.log("play promise rejected");
+                                    //If play is rejected then return UI state to pause so user can take action
+                                    _this.pause();
+                                });
+                            }
 						}
 
 						_this.mobilePlayed = true;
