28feb302e4c3e0e95dfcb9b6c87216a6385d1247
Joe Zeitouny <joezeitouny@users.noreply.github.com>
2017-10-21 19:49:31 +0200

    feat: repackage comscore streaming plugin with an updated generic plugin version v2.6.0.170905 (#3593)

--- modules/ComscoreStreamingTag/resources/mw.KalturaComScoreSTAPlugin.js
+++ modules/ComscoreStreamingTag/resources/mw.KalturaComScoreSTAPlugin.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var PLUGIN_PLATFORM_NAME = 'kaltura',
-        PLUGIN_VERSION = '1.2.3.170522',
+        PLUGIN_VERSION = '1.3.0.170908',
         PLAYER_VERSION = mw.getConfig('version');
 
     function KalturaComScoreSTAPlugin(playerPluginInstance) {
