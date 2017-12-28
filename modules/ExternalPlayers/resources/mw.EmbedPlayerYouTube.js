31492db67dbdcc27fefad055a3af1cbbb21cee12
Oren Me <oren.me@gmail.com>
2017-12-28 16:32:58 +0200

    fix(FEC-7689): autoplay not working on youtube entries
    fix incorrect `_this` ref

--- modules/ExternalPlayers/resources/mw.EmbedPlayerYouTube.js
+++ modules/ExternalPlayers/resources/mw.EmbedPlayerYouTube.js
@@ -100,7 +100,7 @@
                 this.setVolume(0);
             }
             //autoplay
-            if(mw.getConfig('autoPlay') && _this.canAutoPlay()){
+            if(mw.getConfig('autoPlay') && this.canAutoPlay()){
                 this.unbindHelper("preSeek");
                 this.play();
             }else{
