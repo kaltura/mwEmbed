444c3da76b2fa02979831bc1d5a748bae5d24b83
Oren Me <oren.me@gmail.com>
2018-01-01 14:36:19 +0200

    fix(SUP-12992) - fix incorrect texttrack kind string

--- modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
@@ -1685,7 +1685,7 @@
 			if (textTracks) {
 				for (var i = 0; i < textTracks.length; i++) {
 					var textTrack = textTracks[i];
-					if (textTrack.mode === 'showing' && (textTrack.kind === 'subtitles' || textTrack.kind === 'caption')) {
+					if (textTrack.mode === 'showing' && (textTrack.kind === 'subtitles' || textTrack.kind === 'captions')) {
 						return textTrack;
 					}
 				}
