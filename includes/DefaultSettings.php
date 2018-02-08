ce2c3b6a49ba03f1ecf5174adf42e1c16fd21b1d
Dan Ziv <dan.ziv@kaltura.com>
2018-02-08 17:40:26 +0200

    Bump version to 2.66

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -20,7 +20,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.66.rc6';
+$wgMwEmbedVersion = '2.66';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
