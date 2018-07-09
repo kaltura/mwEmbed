c884c64bcaa7fdea6fa1ddcbf24cb8dfa30a4132
eitan <eitanavgil@kaltura.com>
2018-07-09 12:48:19 +0300

    bump version to 2.70.acc4

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -20,7 +20,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.70.acc3';
+$wgMwEmbedVersion = '2.70.acc4';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
