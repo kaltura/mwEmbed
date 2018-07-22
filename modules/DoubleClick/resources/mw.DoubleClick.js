3640c752aded18be9a7378c2830f675fb7d96ddc
Dan Ziv <dan.ziv@kaltura.com>
2018-07-22 10:43:29 +0300

    fix(FEC-8187, FEC-8188): seek before play with ads on mobile (#3859)

--- modules/DoubleClick/resources/mw.DoubleClick.js
+++ modules/DoubleClick/resources/mw.DoubleClick.js
@@ -499,7 +499,7 @@
                         // Set the content element to player element:
                         var playerElement = _this.embedPlayer.getPlayerElement();
                         //Load the video tag to enable setting the source by doubleClick library
-                        if ((mw.isDesktopSafari() || mw.isMobileDevice()) && !_this.playerElementLoaded) {
+                        if (_this.adsManager.isCustomPlaybackUsed() && !_this.playerElementLoaded) {
                             _this.playerElementLoaded = true;
                             playerElement.load();
                         }
