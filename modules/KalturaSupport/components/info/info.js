e667d18f51107074fbcdef0c6e5b5e7b543d242a
Eitan <eitanavgil@gmail.com>
2018-08-06 15:10:54 +0300

    Accessibility to 2.71 (#3842)
    * dualScreen-icons-accessible
    add focus by tab to 3 icons on dualScreen
    
    * 1.fix from PR comments
    2.add keyboard navigate for contentSelection box
    
    * add tab focus on hamburger
    
    * fix from PR comments
    
    * FEV-135 (#3690)
    
    [IVQ-Access.] Q text not readable by SR
    
    * FEV-136 (#3691)
    
    [IVQ-Access.] continue button keyboard trap
    
    * FEV-144 (#3692)
    
    [IVQ-Access.] submit and review buttons keyboard trap
    
    * Dual screen icons accessible (#3693)
    
    * dualScreen-icons-accessible
    add focus by tab to 3 icons on dualScreen
    
    * 1.fix from PR comments
    2.add keyboard navigate for contentSelection box
    
    * add tab focus on hamburger
    
    * fix from PR comments
    
    * fix tabs-ordering of streamsSelector
    
    * fix text for SR
    
    * FEV-152 (#3694)
    
    Accessibility [player] - Focus moves out of dialog screen
    
    * fix text for SR
    
    * SR fixes
    
    * FEV-154 (#3695)
    
    * FEV-154
    [Player-Access.] moderation/report plugin - no support for SR
    
    * Update moderationPlugin.js
    
    * fix info focus
    
    * fix info z-order
    
    * add access to slides drop-down
    
    * add access to slides drop-down
    
    * add access to slides drop-down
    
    * cycle quiz question
    
    * fix mouse trap for quiz opening screen
    
    * reorder tab index of quiz cue-points markers on timeline
    
    * reorder tab index of quiz cue-points markers on timeline
    
    * fix focus color and tab index
    
    * whitespace
    
    * z-index to 999 (1000 is reserved to screens)
    
    * switch z-order
    
    * FEV-152-focus-out-infoScreen (#3766)
    
    fix focus-out of info screen
    
    * Fev 152 focus out info screen (#3770)
    
    * FEV-152-focus-out-infoScreen
    fix focus-out of info screen
    
    * FEV-152-focus-out-dialog-screen
    1. fix focus-out of related screen
    2. add tabindex to imgs on related screen
    
    * fix(KavaTimer): replace es3 reserved word (#3783)
    
    * add tabIndex to quiz
    
    * add tabIndex to quiz
    
    * FEV-181-skip-buttons add aria-label and fix access by keyboard (#3816)
    
    * fix: Send mediaHits (location=0) for linear without DVR (#3799)
    
    * FEV-181-skip-buttons add aria-label and fix access by keyboard
    
    * dualScreen-icons-accessible: add aria-label
    
    * FEV-184-chapter-side-menu-buttons-new:
    
    * FEV-182-submit-button-new:
    1. fix keyboard tab order of cp and submit button
    2. add aria-label
    3. fix click issue with continue button on "almost done" screen
    
    * WEBC-1185-allow-anonymous-qa-new: add setting to change moderator name
    
    * FEV-182-change-tabindex: set static tabindex
    
    * bump version
    
    * add tab fic for chapters
    
    * add tab index to navigation in quiz
    
    *  improve test page
    
    * change z order
    
    * add enter behaviour to chapters
    
    * whitespace
    
    * handle escape and tab browsing
    
    * bump version
    
    * add escape logic to items in chapters list
    
    * add escape logic to items in chapters list
    
    * FEV-189 add aria-lable to quiz bubbles (#3858)
    
    * FEV-197 - remove focus from dualScreen control bar
    
    * FEV-193
    
    * FEV-194 add focus to Volume control
    
    * FEV-194 improve border for tab index
    
    * bomp version
    
    * FEV-196
    
    * FEV-190
    
    * bump acc version
    
    * FEV-192 - search placeholder contrast
    
    * FEV-191 - chapters button to close menu with keyboard
    
    * FEV-198 - selection highlight
    
    * bump version to 2.70.acc4
    
    * fix FF focus rect
    
    * Update DefaultSettings.php

--- modules/KalturaSupport/components/info/info.js
+++ modules/KalturaSupport/components/info/info.js
@@ -44,6 +44,16 @@ mw.PluginManager.add( 'infoScreen', mw.KBaseScreen.extend({
 					$(embedPlayer.getPlayerElement()).addClass("blur");
 					embedPlayer.getPlayerPoster().addClass("blur");
 					embedPlayer.getInterface().find(".infoScreen .icon-close").focus();
+					screen.keydown( $.proxy( _this.tabKeyBind, _this )  );
+					embedPlayer.getInterface().find(".infoScreen").keydown(function(e){
+						if(e.keyCode === 9){// keyCode = 9 - tab button
+							setTimeout(function () {
+								if(!$(':focus').parents('.infoScreen').hasClass('infoScreen')){
+									embedPlayer.getInterface().find(".infoScreen .icon-close").focus();
+								}
+							}, 0);
+						}
+					});
 				});
 			}
 		});
@@ -67,6 +77,17 @@ mw.PluginManager.add( 'infoScreen', mw.KBaseScreen.extend({
 		this.bind('onCloseFullScreen', function () {
 			embedPlayer.getVideoHolder().removeClass("fullscreen-video-size-small");
 		});
+		this.tabKeyBind = function(e){
+			if(e.keyCode === 9){// keyCode = 9 - tab button
+				setTimeout(function () {
+					var currentFocusedElement = $(':focus');//when timeout will done - new element will be in focus
+					if(!currentFocusedElement.parents('.infoScreen').length){
+						_this.getPlayer().getInterface().find(".infoScreen .icon-close").focus();
+						return;
+					}
+				}, 0);
+			}
+		};
 	},
 	addScreenBindings: function(){
 		if (mw.isNativeApp()) {
