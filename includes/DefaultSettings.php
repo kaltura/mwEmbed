17a35393eccdbb7abf4e5a2bf2561fc84f29df15
Oren Me <oren.me@gmail.com>
2018-10-03 19:15:14 +0300

    Bump version to 2.72.rc1

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -20,7 +20,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.71.6';
+$wgMwEmbedVersion = '2.72.rc1';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
