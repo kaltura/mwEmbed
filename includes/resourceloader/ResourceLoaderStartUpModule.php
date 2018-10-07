e1370597aa0bfac4c87ee5ef47bea52c969afe6c
yossipapi <yossi.papiashvili@kaltura.com>
2018-10-07 11:45:09 +0300

    PHP7: remove unused methods + fix count warnings

--- includes/resourceloader/ResourceLoaderStartUpModule.php
+++ includes/resourceloader/ResourceLoaderStartUpModule.php
@@ -141,7 +141,7 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 				$mtime = $moduleMtime;
 				// Modules without dependencies, a group or a foreign source pass two arguments (name, timestamp) to
 				// mw.loader.register()
-				if ( !count( $module->getDependencies() && $module->getGroup() === null && $module->getSource() === 'local' ) ) {
+				if (!count($module->getDependencies()) && $module->getGroup() === null && $module->getSource() === 'local') {
 					$registrations[] = array( $name, $mtime );
 				}
 				// Modules with dependencies but no group or foreign source pass three arguments
