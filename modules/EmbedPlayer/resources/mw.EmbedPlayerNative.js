520fe7a6b0fb965714c3b286a5ff9534f405e97d
Shalom Meoded <shalommeoded@gmail.com>
2018-11-20 11:52:37 +0200

    Fix(FEC-8545): Fixed the Seek issue in the IOS Inline player (#3937)

--- modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
+++ modules/EmbedPlayer/resources/mw.EmbedPlayerNative.js
@@ -482,7 +482,7 @@
 			// some initial calls to prime the seek:
 			if ( ( vid.currentTime === 0 && callbackCount === 0 ) && vid.readyState === 0 ) { //load video again if not loaded yet (vid.readyState === 0)
 				// when seeking turn off preload none and issue a load call.
-				if(mw.isIpad()){
+				if(mw.isIpad()|| mw.isIOSAbove7()){
 					$(vid).attr('preload', 'auto')[0].load();
 				} else {
 					$(vid).attr('preload', 'auto');
