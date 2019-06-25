95732094bcb5eb4ee9c57b1ff2878ad13a1ba5c9
eitan.avgil <eitan.avgil@kaltura.com>
2019-06-25 14:55:06 +0300

    bump

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -20,7 +20,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.76.1.an4';
+$wgMwEmbedVersion = '2.76.1';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
