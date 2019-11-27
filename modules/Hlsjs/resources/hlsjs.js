87003024048c8f682af99a184fe99b4da5721df2
Shalom Meoded <shalom.meoded@kaltura.com>
2019-11-27 13:18:09 +0200

    fix(FEC-9507): Live stream audio is not initiated on Edge

--- modules/Hlsjs/resources/hlsjs.js
+++ modules/Hlsjs/resources/hlsjs.js
@@ -128,7 +128,7 @@
 					$(this.getPlayer().getPlayerElement()).one("canplay", function(){
 						// The initial seeking to the live edge has finished.
 						this.afterInitialSeeking = true;
-						if (this.embedPlayer.isLive() && mw.isIE11()) {
+						if (this.embedPlayer.isLive() && (mw.isIE11() || mw.isEdge())) {
 							var player = this.embedPlayer.getPlayerElement();
 							//nudge time on IE11 live streams due to audio bug https://github.com/video-dev/hls.js/issues/2323
 							player.currentTime = player.currentTime + 0.1;
