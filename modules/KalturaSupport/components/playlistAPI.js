c7bbae43f6c4fa763d0596753fb2b0f1528cf58c
Shalom Meoded <shalom.meoded@kaltura.com>
2020-06-03 16:18:00 +0300

    Added a KS adding condition

--- modules/KalturaSupport/components/playlistAPI.js
+++ modules/KalturaSupport/components/playlistAPI.js
@@ -397,7 +397,7 @@
 				title = kWidget.sanitize( title );
 				description = kWidget.sanitize( description );
 
-				var thumbnailUrl = this.getThumbUrl(item) || item.thumbnailUrl || customData.thumbUrl;
+				var thumbnailUrl = item.thumbnailUrl || customData.thumbUrl || this.getThumbUrl(item);
 				var thumbnailRotatorUrl = this.getConfig('thumbnailRotator') ? this.getThumRotatorUrl() : '';
 
 				item.order = i;
@@ -405,7 +405,7 @@
 				item.description = description;
 				item.width = this.getConfig('mediaItemWidth');
 				item.thumbnail = {
-					url: thumbnailUrl,
+					url: mw.getConfig('loadThumbnailWithKs') === true && thumbnailUrl.indexOf("ks") < 1 ? thumbnailUrl += '/ks/' + mw.getConfig('ks') : thumbnailUrl,
 					thumbAssetId: item.assetId,
 					rotatorUrl: thumbnailRotatorUrl,
 					width: this.getThumbWidth(),
