272941bf999c95a00733eae610bed5326815be35
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2021-03-10 10:42:55 +0200

    fix(FEC-11027): thumbnail API scaling problem (#4209)
    checking ShowOriginalPoster only when thumbnails are posters, and not when thumbnail is related to scrubber

--- modules/KalturaSupport/resources/mw.KWidgetSupport.js
+++ modules/KalturaSupport/resources/mw.KWidgetSupport.js
@@ -2118,7 +2118,7 @@ mw.KWidgetSupport.prototype = {
 		// Only append width/height params if thumbnail from kaltura service ( could be external thumbnail )
 		if( thumbUrl.indexOf( "thumbnail/entry_id" ) != -1 ){
 
-			if( mw.getConfig('EmbedPlayer.ShowOriginalPoster') ){
+			if(!thumb.vid_slices && mw.getConfig('EmbedPlayer.ShowOriginalPoster') ){
 				thumbUrl += '/width/0/height/0';
 			} else {
 				thumbUrl += '/width/' + thumb.width + '/height/' + thumb.height;
