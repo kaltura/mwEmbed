a39a41de254bd8a1dde5b6a3828f138904e59820
Yair Ansbacher <yair.ansbacher@kaltura.com>
2021-05-26 13:40:13 +0300

    Bump version to 2.89.rc1

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -20,7 +20,7 @@ $wgScriptCacheDirectory = realpath( dirname( __FILE__ ) ) . '/cache';
 $wgBaseMwEmbedPath = realpath( dirname( __FILE__ ) . '/../' );
 
 // The version of the library:
-$wgMwEmbedVersion = '2.88.5';
+$wgMwEmbedVersion = '2.89.rc1';
 
 // Default HTTP protocol from GET or SERVER parameters
 if( isset($_GET['protocol']) ) {
