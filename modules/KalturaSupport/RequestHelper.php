a2abaa77ffeee6f1ecce9c15e2a496e80e0be1f9
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2022-06-12 17:05:27 +0300

    Test php - DO NOT MERGER

--- modules/KalturaSupport/RequestHelper.php
+++ modules/KalturaSupport/RequestHelper.php
@@ -210,20 +210,20 @@ class RequestHelper {
 			return $wgKalturaForceReferer;
 		}
 		if( isset( $_SERVER['HTTP_REFERER'] ) ){
-		    var_dump('##### HTTP REFERER ######');
-		    var_dump($_SERVER['HTTP_REFERER']);
+		    echo('##### HTTP REFERER ######');
+		    echo($_SERVER['HTTP_REFERER']);
 			$urlParts = parse_url( $_SERVER['HTTP_REFERER'] );
-			var_dump('##### URL PARTS ######');
+			echo('##### URL PARTS ######');
 			var_dump($urlParts);
 			if (isset( $urlParts['scheme'] ) &&  isset( $urlParts['host']) ) {
-			    var_dump('##### SCHEME ######');
-			    var_dump($urlParts['scheme']);
-			    var_dump('##### HOST ######');
-			    var_dump($urlParts['host']);
+			    echo('##### SCHEME ######');
+			    echo($urlParts['scheme']);
+			    echo('##### HOST ######');
+			    echo($urlParts['host']);
 				return $urlParts['scheme'] . "://" . $urlParts['host'] . "/";
 			}
 		}
-		var_dump('##### RETURN KALTURA.COM ######');
+		echo('##### RETURN KALTURA.COM ######');
 		return 'http://www.kaltura.com/';
 	}
 
