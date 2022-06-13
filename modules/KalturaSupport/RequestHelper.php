43db43d797b38898fff3030d0ff59c3c0c3fcf56
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2022-06-13 15:59:48 +0300

    add logic to getReferer

--- modules/KalturaSupport/RequestHelper.php
+++ modules/KalturaSupport/RequestHelper.php
@@ -209,13 +209,30 @@ class RequestHelper {
 		if( $wgKalturaForceReferer !== false ){
 			return $wgKalturaForceReferer;
 		}
-		if( isset( $_SERVER['HTTP_REFERER'] ) ){
-			$urlParts = parse_url( $_SERVER['HTTP_REFERER'] );
-			if (isset( $urlParts['scheme'] ) &&  isset( $urlParts['host']) ) {
-				return $urlParts['scheme'] . "://" . $urlParts['host'] . "/";
-			}
+		$requestHeaders = getallheaders();
+		if( isset( $requestHeaders ) && isset( $requestHeaders['Referer'] ) ){
+		    $refererUrl = $this->buildReferer($requestHeaders['Referer']);
+		    echo('referer header ->');
+            echo(print_r($refererUrl);
+        } else if (isset( $_SERVER['HTTP_REFERER'] )){
+            $refererUrl = $this->buildReferer($_SERVER['HTTP_REFERER']);
+            echo(print_r('HTTP_REFERER header ->');
+            echo(print_r($refererUrl);
+		}
+		if (isset ($refererUrl) && $refererUrl !== null) {
+		    echo(print_r('referer url ->');
+		    echo(print_r($refererUrl);
+		    return $refererUrl;
 		}
 		return 'http://www.kaltura.com/';
+	}
+
+	private function buildReferer($refererUrl) {
+	    $urlParts = parse_url( $refererUrl );
+        if (isset( $urlParts['scheme'] ) &&  isset( $urlParts['host']) ) {
+            return $urlParts['scheme'] . "://" . $urlParts['host'] . "/";
+        }
+        return null;
 	}
 
 	// Check if private IP
