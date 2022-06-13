30bff6d0d9e3b50b553e3ef7bf4fa8b0286b3046
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2022-06-13 16:40:42 +0300

    add logic to getReferer

--- modules/KalturaSupport/RequestHelper.php
+++ modules/KalturaSupport/RequestHelper.php
@@ -209,13 +209,42 @@ class RequestHelper {
 		if( $wgKalturaForceReferer !== false ){
 			return $wgKalturaForceReferer;
 		}
-		if( isset( $_SERVER['HTTP_REFERER'] ) ){
-			$urlParts = parse_url( $_SERVER['HTTP_REFERER'] );
-			if (isset( $urlParts['scheme'] ) &&  isset( $urlParts['host']) ) {
-				return $urlParts['scheme'] . "://" . $urlParts['host'] . "/";
-			}
+
+		if (isset( $_SERVER['HTTP_REFERER'] )){
+            $refererUrl = $this->buildReferer($_SERVER['HTTP_REFERER']);
+            echo('   HTTP_REFERER header ->   ');
+            echo($refererUrl);
+        } else if (isset( $_SERVER ) && isset($_SERVER['HTTP_HOST'])) {
+            if ( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+                $refererUrl = "https://";
+            } else {
+                $refererUrl = "http://";
+            }
+            $refererUrl.= $_SERVER['HTTP_HOST'];
+            echo('   HTTP_HOST header ->   ');
+            echo($refererUrl);
+        } else {
+            $requestHeaders = getallheaders();
+            if( isset( $requestHeaders ) && isset( $requestHeaders['Referer'] ) ){
+                $refererUrl = $this->buildReferer($requestHeaders['Referer']);
+                echo('   referer header ->   ');
+                echo($refererUrl);
+            }
+        }
+		if (isset ($refererUrl) && $refererUrl !== null) {
+		    echo('   referer url ->   ');
+		    echo($refererUrl);
+		    return $refererUrl;
 		}
 		return 'http://www.kaltura.com/';
+	}
+
+	private function buildReferer($refererUrl) {
+	    $urlParts = parse_url( $refererUrl );
+        if (isset( $urlParts['scheme'] ) &&  isset( $urlParts['host']) ) {
+            return $urlParts['scheme'] . "://" . $urlParts['host'] . "/";
+        }
+        return null;
 	}
 
 	// Check if private IP
